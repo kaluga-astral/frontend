70f86b0c4f7f9f9a156f297e7c4cb634b5ab4cba
Perevezentsev Pavel <perevezenzev@gmail.com>
2021-10-27 13:51:00 +0300

    Инициализация

---
+++ packages/ui/src/Button/Button.test.tsx
@@ -0,0 +1,7 @@
+import { Button } from './Button'
+
+describe('Button', () => {
+  it('is truthy', () => {
+    expect(Button).toBeTruthy()
+  })
+})
---
+++ packages/ui/src/Button/Button.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import MuiButton from '@mui/material/Button'
+
+export const Button = () => {
+  return <MuiButton>Button</MuiButton>
+}
+
+export default Button
