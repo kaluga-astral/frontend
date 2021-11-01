116d099df6aa4f65596f04941baa4778c7be1c3b
Albert Merkel <merkel.albert1997@gmail.com>
2021-11-01 09:46:44 +0300

    add storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,32 @@
+// emotion-theming нужен для: https://github.com/storybookjs/storybook/issues/10231#issuecomment-613394048
+
+import { ThemeProvider } from 'emotion-theming'
+import { ThemeProvider as MuiThemeProvider } from '@mui/material'
+import { getTheme, themes } from './themes'
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' }
+}
+
+export const decorators = [
+  (Story, context) => (
+    <MuiThemeProvider theme={getTheme(context.globals.theme)}>
+      <ThemeProvider theme={getTheme(context.globals.theme)}>
+        <Story {...context} />
+      </ThemeProvider>
+    </MuiThemeProvider>
+  )
+]
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: themes[0].name,
+    toolbar: {
+      icon: 'circlehollow',
+      items: themes.map((theme) => theme.name),
+      showName: true
+    }
+  }
+}
