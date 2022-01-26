0e43025363df451d6c942c07aff5bca4b7264f16
Кулешов Олег Игоревич <kuleshov_oi@astralnalog.ru>
2022-01-26 17:27:19 +0300

    UIKIT-49 - Создал Тэг компонент и начал работу над стилями Сменил #EBECF0 на #EEF1F4 ( background=>elementHover)

---
+++ packages/ui/src/Tag/constants.ts
@@ -0,0 +1,26 @@
+export enum TagColors {
+  DEFAULT = 'default',
+  PRIMARY = 'primary',
+  SECONDARY = 'secondary',
+  ERROR = 'error',
+  INFO = 'info',
+  SUCCESS = 'success',
+  WARNING = 'warning',
+}
+
+export enum TagVariants {
+  REGULAR = 'regular',
+  ROUNDED = 'rounded',
+}
+
+export enum TagSizes {
+  MEDIUM = 'medium',
+  SMALL = 'small',
+}
+
+export enum TagStates {
+  DEFAULT = 'default',
+  HOVER = 'hover',
+  ACTIVE = 'active',
+  SELECTED = 'selected',
+}
