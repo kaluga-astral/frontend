d40a31647d3537ac6a92b39a504d7480f3aa4912
Artem Laschenkov <laschenkov59@yandex.ru>
2022-01-26 15:15:35 +0300

    FIX | UIKIT-47 |  Исправляет Loader на СircularProgress часть 2

--- packages/ui/src/CircularProgress/styled.ts
+++ packages/ui/src/CircularProgress/styled.ts
@@ -4,9 +4,16 @@ import styled from '@emotion/styled';
 import { Theme } from '../theme';
 
 import { СircularProgressColors, СircularProgressSizes } from './constants';
-import { СircularProgressColor, СircularProgressProps, СircularProgressSize } from './types';
+import {
+  СircularProgressColor,
+  СircularProgressProps,
+  СircularProgressSize,
+} from './types';
 
-type StyledСircularProgressProps = Omit<СircularProgressProps, 'color' | 'size'> & {
+type StyledСircularProgressProps = Omit<
+  СircularProgressProps,
+  'color' | 'size'
+> & {
   customColor?: СircularProgressColor;
   customSize?: СircularProgressSize;
 };
@@ -15,13 +22,19 @@ type StyledСircularProgressThemeProps = StyledСircularProgressProps & {
   theme: Theme;
 };
 
-const getColor = ({ theme, customColor }: StyledСircularProgressThemeProps): string => {
-  if (customColor === СircularProgressColors.PRIMARY) return theme.palette.primary.dark;
+const getColor = ({
+  theme,
+  customColor,
+}: StyledСircularProgressThemeProps): string => {
+  if (customColor === СircularProgressColors.PRIMARY)
+    return theme.palette.primary.dark;
 
   return theme.palette.primary.contrastText;
 };
 
-const getСircularProgressSize = ({ customSize }: StyledСircularProgressThemeProps): string => {
+const getСircularProgressSize = ({
+  customSize,
+}: StyledСircularProgressThemeProps): string => {
   if (customSize === СircularProgressSizes.SMALL) return '16px';
 
   return '24px';
