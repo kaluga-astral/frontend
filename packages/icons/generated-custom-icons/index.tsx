ca5b2fc22e6909cbadc8b4a18f593317d7d6482b
Albert Merkel <merkel.albert1997@gmail.com>
2022-01-31 12:24:24 +0300

    fix gitignore

--- packages/icons/generated-custom-icons/index.tsx
+++
@@ -1,1 +0,0 @@
-export { default as GoogleFillMd } from './GoogleFillMd';
--- packages/icons/generated-themed-icons/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as EyeFillMd } from './EyeFillMd';
-export { default as EyeOutlineMd } from './EyeOutlineMd';
-export { default as VkFillMd } from './VkFillMd';
