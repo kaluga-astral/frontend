6ff4130e8c5f834ab03d795a6a83f78941d2ef4f
Кулешов Олег Игоревич <kuleshov_oi@astralnalog.ru>
2022-02-02 16:33:21 +0300

    UIKIT-49 - Добавил Кейс с аватарами и иконками удаления

--- packages/ui/src/Tag/Tag.tsx
+++ packages/ui/src/Tag/Tag.tsx
@@ -1,8 +1,17 @@
 // import { TagProps } from './types';
+import { IconButton } from '../IconButton';
+
 import { StyledTag } from './styled';
 
-const Tag = ({ children, color, variant, ...props }: any) => {
-  return <StyledTag customcolor={color} customvariant={variant} {...props} />;
+const Tag = ({ children, color, variant, deleteIcon, ...props }: any) => {
+  return (
+    <StyledTag
+      customcolor={color}
+      customvariant={variant}
+      deleteIcon={<IconButton variant="light">{deleteIcon}</IconButton>}
+      {...props}
+    />
+  );
 };
 
 export default Tag;
