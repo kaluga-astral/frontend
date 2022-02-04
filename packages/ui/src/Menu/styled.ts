22d1883cf0aba1c3facafe1f5612bb6ed5da67ab
Nikita <nikitasmirnov90@gmail.com>
2022-02-04 10:50:13 +0300

    UIKIT-46 Поправил box-shadow

--- packages/ui/src/Menu/styled.ts
+++ packages/ui/src/Menu/styled.ts
@@ -6,9 +6,9 @@ import { MenuProps } from './types';
 
 export const StyledMenu = styled(Menu)<MenuProps>`
   .MuiMenu-paper {
-    ${({ theme }) => theme.elevation[200]}
-    border-radius: ${({ theme }) => theme.shape.small};
     margin-top: ${({ theme }) => theme.spacing(2)};
+    border-radius: ${({ theme }) => theme.shape.small};
+    box-shadow: ${({ theme }) => theme.elevation[200]};
   }
 
   .MuiMenu-list {
--- packages/ui/src/MenuList/styled.ts
+++ packages/ui/src/MenuList/styled.ts
@@ -6,8 +6,8 @@ import { MenuListProps } from './types';
 
 export const StyledMenuList = styled(MenuList)<MenuListProps>`
   &.MuiList-root {
-    ${({ theme }) => theme.elevation[200]}
-    border-radius: ${({ theme }) => theme.shape.small};
     padding: ${({ theme }) => theme.spacing(1, 0)};
+    border-radius: ${({ theme }) => theme.shape.small};
+    box-shadow: ${({ theme }) => theme.elevation[200]};
   }
 `;
