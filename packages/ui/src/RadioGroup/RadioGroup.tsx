85daec37441ad5ac8412f42eb5d831a896db0dee
Artem Laschenkov <laschenkov59@yandex.ru>
2022-02-05 01:22:55 +0300

    Это пока лишнее

--- packages/ui/src/RadioGroup/RadioGroup.tsx
+++
@@ -1,9 +0,0 @@
-import { RadioGroupProps } from '@mui/material';
-
-import { StyledRadioGroup } from './styled';
-
-export const RadioGroup = ({ ...props }: RadioGroupProps) => {
-  return <StyledRadioGroup {...props} />;
-};
-
-export default RadioGroup;
