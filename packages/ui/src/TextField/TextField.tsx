bcdd5cda6f301049c608414beeaffd3d969548fc
StanislavDa <flugegeheimenbolsen@gmail.com>
2022-02-16 18:12:15 +0500

    UIKIT-91 Пробросил ref в TextField

--- packages/ui/src/TextField/TextField.tsx
+++ packages/ui/src/TextField/TextField.tsx
@@ -1,48 +1,51 @@
-import { useMemo } from 'react';
+import { forwardRef, useMemo } from 'react';
 import MuiTextField from '@mui/material/TextField';
 
 import { FormHelperText } from '../FormHelperText';
 
 import { TextFieldProps } from './types';
 
-export const TextField = (props: TextFieldProps) => {
-  const {
-    variant,
-    success,
-    error,
-    color: colorProp,
-    helperText: helperTextProp,
-    ...restProps
-  } = props;
+export const TextField = forwardRef<HTMLDivElement>(
+  (props: TextFieldProps, ref) => {
+    const {
+      variant,
+      success,
+      error,
+      color: colorProp,
+      helperText: helperTextProp,
+      ...restProps
+    } = props;
 
-  const color = useMemo(() => {
-    if (success) {
-      return 'success';
-    }
+    const color = useMemo(() => {
+      if (success) {
+        return 'success';
+      }
 
-    return undefined;
-  }, [success]);
+      return undefined;
+    }, [success]);
 
-  const helperText = useMemo(() => {
-    if (success) {
-      return <FormHelperText success>{helperTextProp}</FormHelperText>;
-    }
-    if (error) {
-      return <FormHelperText error>{helperTextProp}</FormHelperText>;
-    }
+    const helperText = useMemo(() => {
+      if (success) {
+        return <FormHelperText success>{helperTextProp}</FormHelperText>;
+      }
+      if (error) {
+        return <FormHelperText error>{helperTextProp}</FormHelperText>;
+      }
 
-    return helperTextProp;
-  }, [helperTextProp, success, error]);
+      return helperTextProp;
+    }, [helperTextProp, success, error]);
 
-  return (
-    <MuiTextField
-      variant="outlined"
-      error={error}
-      color={color}
-      helperText={helperText}
-      {...restProps}
-    />
-  );
-};
+    return (
+      <MuiTextField
+        ref={ref}
+        variant="outlined"
+        error={error}
+        color={color}
+        helperText={helperText}
+        {...restProps}
+      />
+    );
+  }
+);
 
 export default TextField;
