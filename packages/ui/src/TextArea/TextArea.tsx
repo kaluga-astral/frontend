926989b872144ecb601ad03b37af63046d8d57b0
Cryzer <feoktistov.ilya@yandex.ru>
2022-02-17 10:17:51 +0300

    UIKIT-52 - Добавлен forwardRef для компонента

--- packages/ui/src/TextArea/TextArea.tsx
+++ packages/ui/src/TextArea/TextArea.tsx
@@ -1,7 +1,11 @@
+import { forwardRef } from 'react';
+
 import { TextField } from '../TextField';
 
 import { TextAreaProps } from './types';
 
-export const TextArea = ({ rows = 7, ...props }: TextAreaProps) => {
-  return <TextField multiline rows={rows} {...props} />;
-};
+export const TextArea = forwardRef<HTMLDivElement, TextAreaProps>(
+  ({ rows = 7, ...props }, ref) => {
+    return <TextField ref={ref} multiline rows={rows} {...props} />;
+  }
+);
