a7a4feb56a15f773d3312f046d0d006687c9428c
Andrey Potyomkin <and_tem@icloud.com>
2022-02-25 17:52:38 +0300

    build: добавлены логи для команд commander

--- commander/tasks/modifyPackageJSON.js
+++ commander/tasks/modifyPackageJSON.js
@@ -1,13 +1,42 @@
 const fs = require('fs');
 
-const updatePackageVersion = (packageData, rootPackageVersion) => {
+const { PACKAGES_NAMES } = require('../constants');
+
+// обновляет до последней версии пакеты, которые есть в репозитории
+const updateDepsVersions = (packageDeps, rootPackageVersion) =>
+  PACKAGES_NAMES.reduce((newPackageDeps, packageName) => {
+    console.log(
+      'packageName',
+      packageName,
+      newPackageDeps,
+      newPackageDeps[packageName]
+    );
+    if (!newPackageDeps[packageName]) return newPackageDeps;
+
+    return { ...newPackageDeps, [packageName]: `^${rootPackageVersion}` };
+  }, packageDeps);
+
+const updatePackagesVersions = (packageData, rootPackageVersion) => {
   fs.writeFileSync(
     './package.json',
-    JSON.stringify({ ...packageData, version: rootPackageVersion }, null, 2)
+    JSON.stringify(
+      {
+        ...packageData,
+        dependencies: updateDepsVersions(
+          packageData.dependencies,
+          rootPackageVersion
+        ),
+        version: rootPackageVersion,
+      },
+      null,
+      2
+    )
   );
 };
 
 const modifyPackageJSON = () => {
+  console.log('Starting modifyPackageJSON...');
+
   const packageData = JSON.parse(fs.readFileSync('./package.json'));
 
   const {
@@ -21,7 +50,11 @@ const modifyPackageJSON = () => {
     fs.readFileSync('../../package.json')
   );
 
-  updatePackageVersion(packageData, rootPackageVersion);
+  console.log('Update packages versions and deps');
+
+  updatePackagesVersions(packageData, rootPackageVersion);
+
+  console.log('Write data to lib package.json');
 
   fs.writeFileSync(
     './lib/package.json',
@@ -48,6 +81,8 @@ const modifyPackageJSON = () => {
       2
     )
   );
+
+  console.log('Finish modifyPackageJSON');
 };
 
 module.exports = { modifyPackageJSON };
