afd956e0ec653034bf12c8fe351ed2d79b518ad6
Andrey Potyomkin <and_tem@icloud.com>
2022-02-25 17:54:49 +0300

    build: добавлены логи для publish команды

--- commander/tasks/publish.js
+++ commander/tasks/publish.js
@@ -3,6 +3,8 @@ const shell = require('shelljs');
 const { DIST_DIR_NAME } = require('../constants');
 
 const publish = () => {
+  console.log('Starting publish...');
+
   const { NPM_PUBLISH_TOKEN } = process.env;
 
   const { code } = shell.exec(
@@ -12,6 +14,8 @@ const publish = () => {
   if (code !== 0) {
     shell.exit(code);
   }
+
+  console.log('Finish publish');
 };
 
 module.exports = { publish };
