2f42e3cce76bc406d32c42ad86facdc8a9c8c4b5
Artem Laschenkov <laschenkov59@yandex.ru>
2022-03-09 22:23:49 +0300

    wip(ui, DatePicker): Стилизация Paper

--- packages/ui/src/DatePicker/DatePicker.tsx
+++ packages/ui/src/DatePicker/DatePicker.tsx
@@ -3,17 +3,27 @@ import { DatePicker as MuiDatePicker } from '@mui/lab';
 import { CalendarOutlineMd } from '@astral/icons';
 import { TextField } from '@mui/material';
 
+import { useTheme } from '../theme';
+import type { Theme } from '../theme/baseTheme';
+
 import { DatePickerProps } from './types';
 
 export const DatePicker = forwardRef<HTMLDivElement, DatePickerProps>(
   ({ ...props }, ref) => {
     const [value] = useState<Date | null>(new Date());
+    const theme: Theme = useTheme();
 
     return (
       <MuiDatePicker
         {...props}
         ref={ref}
         value={value}
+        PaperProps={{
+          style: {
+            marginTop: theme.spacing(2),
+            boxShadow: theme.elevation[200],
+          },
+        }}
         renderInput={(params) => <TextField {...params} />}
         components={{ OpenPickerIcon: CalendarOutlineMd }}
       />
