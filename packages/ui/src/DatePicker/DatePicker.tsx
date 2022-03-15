4fb2e33a578c0fa7065d3d7be5bc642545f4fc22
Artem Laschenkov <laschenkov59@yandex.ru>
2022-03-15 15:22:38 +0300

    wip(ui, DatePicker): Исправления по результатам ревью. Убрал useState

--- packages/ui/src/DatePicker/DatePicker.tsx
+++ packages/ui/src/DatePicker/DatePicker.tsx
@@ -1,23 +1,22 @@
-import { forwardRef, useState } from 'react';
+import { forwardRef } from 'react';
 import { DatePicker as MuiDatePicker } from '@mui/lab';
 import { CalendarOutlineMd } from '@astral/icons';
 import { TextField } from '@mui/material';
 
 import { useTheme } from '../theme';
-import type { Theme } from '../theme/baseTheme';
+import type { Theme } from '../theme';
 
 import { DatePickerProps } from './types';
 
 export const DatePicker = forwardRef<HTMLDivElement, DatePickerProps>(
   ({ ...props }, ref) => {
-    const [value] = useState<Date | null>(new Date());
     const theme: Theme = useTheme();
 
     return (
       <MuiDatePicker
         {...props}
         ref={ref}
-        value={value}
+        value={new Date()}
         PaperProps={{
           style: {
             marginTop: theme.spacing(2),
