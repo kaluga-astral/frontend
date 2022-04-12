2e98303c54b75568bcf1f4d2c5bdd57a2a63cdb3
Nikita <nikitasmirnov90@gmail.com>
2022-04-12 13:16:46 +0300

    feat(ui,List): Реализован функционал сворачивания List

--- packages/ui/src/List/styled.ts
+++ packages/ui/src/List/styled.ts
@@ -2,13 +2,23 @@ import { List as MuiList } from '@mui/material';
 
 import { styled } from '../styles';
 
-export const StyledList = styled(MuiList)`
+import { ListProps } from './types';
+
+export const StyledList = styled(MuiList)<ListProps>`
   a {
     display: inline-block;
     width: 100%;
     height: 100%;
 
     text-decoration: none;
+  }
+
+  .MuiCollapse-root {
+    transition: ${({ theme }) =>
+      `${theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      })} !important`};
   }
 
   .MuiCollapse-wrapperInner {
