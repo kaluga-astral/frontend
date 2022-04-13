d6896946c0dd09abea06793df28ac276a8fd101e
Alexey Malyshev <lordniken@mail.ru>
2022-04-13 12:02:32 +0300

    feat(ui,DatePicker): Изменены типы

--- packages/ui/src/DatePicker/types.ts
+++ packages/ui/src/DatePicker/types.ts
@@ -2,5 +2,14 @@ import { DatePickerProps as MuiDatePickerProps } from '@mui/lab';
 
 export type DatePickerProps = Omit<
   MuiDatePickerProps,
-  'components' | 'renderInput'
+  | 'components'
+  | 'renderInput'
+  | 'showDaysOutsideCurrentMonth'
+  | 'components'
+  | 'componentsProps'
+  | 'OpenPickerButtonProps'
+  | 'PaperProps'
+  | 'PopperProps'
 >;
+
+export type DatePickerValue = Date;
