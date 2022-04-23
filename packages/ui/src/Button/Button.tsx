9f9567415b423069d46b124cf0e0302278a8077e
AnreyZlobin <zlobin.andy@gmail.com>
2022-04-23 09:48:50 +0300

    feat(ui,Button,IconButton): Добавил forwardRef для кнопок

--- packages/ui/src/Button/Button.tsx
+++ packages/ui/src/Button/Button.tsx
@@ -1,21 +1,20 @@
+import { forwardRef } from 'react';
+
 import { CircularProgress } from '../CircularProgress';
 
 import { StyledButton } from './styled';
 import { ButtonProps } from './types';
 
-export const Button = ({
-  children,
-  startIcon,
-  endIcon,
-  loading,
-  ...props
-}: ButtonProps) => {
-  return (
-    <StyledButton loading={loading} {...props}>
-      <span>{startIcon}</span>
-      {children}
-      {loading && <CircularProgress />}
-      <span>{endIcon}</span>
-    </StyledButton>
-  );
-};
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, ref) => {
+    const { children, startIcon, endIcon, loading, ...restProps } = props;
+    return (
+      <StyledButton loading={loading} {...restProps} ref={ref}>
+        <span>{startIcon}</span>
+        {children}
+        {loading && <CircularProgress />}
+        <span>{endIcon}</span>
+      </StyledButton>
+    );
+  }
+);
--- packages/ui/src/IconButton/IconButton.tsx
+++ packages/ui/src/IconButton/IconButton.tsx
@@ -1,7 +1,16 @@
+import { forwardRef } from 'react';
+
 import { BaseButtonProps } from '../ButtonBase';
 
 import { StyledIconButton } from './styled';
 
-export const IconButton = ({ children, ...props }: BaseButtonProps) => {
-  return <StyledIconButton {...props}>{children}</StyledIconButton>;
-};
+export const IconButton = forwardRef<HTMLButtonElement, BaseButtonProps>(
+  (props, ref) => {
+    const { children, ...restProps } = props;
+    return (
+      <StyledIconButton {...restProps} ref={ref}>
+        {children}
+      </StyledIconButton>
+    );
+  }
+);
