894a5169517e499409c11ce8a40cc5cef8c24772
Perevezentsev Pavel <pavel@perevezencev.pw>
2022-04-25 17:10:27 +0300

    chore: Исправление деклараций

--- packages/ui/declarations/emotion.d.ts
+++ packages/ui/declarations/emotion.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="@emotion/react/types/css-prop" />
 
 import '@emotion/react';
-import { Theme as BaseTheme } from '../../../src/theme/baseTheme';
+import { Theme as BaseTheme } from '../src/theme/baseTheme';
 
 declare module '@emotion/react' {
   export interface Theme extends BaseTheme {}
