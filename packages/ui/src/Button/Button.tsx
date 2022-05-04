4d0b3fb617125ae6ee904057018ac1174c6a95ee
Alexey Malyshev <lordniken@mail.ru>
2022-05-04 14:54:05 +0300

    bug(ui,Button): установлен variant contained как дефоулт проп

--- packages/ui/src/Button/Button.tsx
+++ packages/ui/src/Button/Button.tsx
@@ -2,15 +2,19 @@ import { forwardRef } from 'react';
 import { LoadingButton } from '@mui/lab';
 
 import { CircularProgress } from '../CircularProgress';
+import { ButtonVariants } from '../ButtonBase/constants';
 
 import { ButtonProps } from './types';
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   (props, ref) => {
+    const { variant = ButtonVariants.CONTAINED, ...restProps } = props;
+
     return (
       <LoadingButton
         ref={ref}
-        {...props}
+        {...restProps}
+        variant={variant}
         loadingIndicator={<CircularProgress size="small" />}
       />
     );
