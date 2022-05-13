98c24056775d1e304b18c128a826b6cf5b211c18
Andrey Potyomkin <and_tem@icloud.com>
2022-05-13 09:55:59 +0300

    UI-KIT-0000: feat: test commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,46 +1,44 @@
-const path = require('path');
-
-const getDirNames = require('read-dir-names');
-
-const packagesNames = getDirNames(path.resolve(__dirname, 'packages'));
-const componentsNames = getDirNames(
-  path.resolve(__dirname, 'packages', 'ui', 'src')
-);
-
 module.exports = {
-  parserOpts: {
-    issuePrefixes: ["UI-KIT-"],
+  extends: [
+    "@commitlint/config-conventional"
+  ],
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(?:UI-KIT-[0-9]{4}:\s)?(\w*)(?:\((.*)\))?: (.*)$/,
+      headerCorrespondence: ["type", "scope", "subject"],
+      issuePrefixes: ["^UI-KIT-[0-9]{4}"],
+    },
   },
-
   rules: {
+    // Тело коммита должно начинаться с пустой строки
+    "body-leading-blank": [2, "always"],
+
+    // Нижний колонтитул коммита должен начинаться с пустой строки
+    "footer-leading-blank": [2, "always"],
+
+    // Максимальная длина заголовка 72 символа
+    "header-max-length": [2, "always", 72],
+
+    // Область всегда только в верхнем регистре
+    "scope-case": [2, "always", "upper-case"],
+
     // Описание не может быть пустым
-    'subject-empty': [2, 'never'],
+    "subject-empty": [2, "never"],
 
     // Описание не должно заканчиваться '.'
-    'subject-full-stop': [2, 'never', '.'],
+    "subject-full-stop": [2, "never", "."],
+
+    // Тип всегда только в нижнем регистре
+    "type-case": [2, "always", "lower-case"],
 
     // Тип не может быть пустым
-    'type-empty': [2, 'never'],
+    "type-empty": [2, "never"],
 
     // Перечислим все возможные варианты коммитов
-    'type-enum': [
+    "type-enum": [
       2,
-      'always',
-      [
-        'feat',
-        'bug',
-        'wip',
-        'refactor',
-        'doc',
-        'build',
-        'chore',
-        'revert',
-        'style',
-        'wip',
-      ],
+      "always",
+      ["build", "ci", "docs", "feat", "fix", "bug", "refactor", "test", "wip"]
     ],
-
-    // Перечисления доступных scope
-    'scope-enum': [2, 'always', [...packagesNames, ...componentsNames]],
-  },
+  }
 };
