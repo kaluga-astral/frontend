cc4f053c60a7f967f6c16328f6aff1752d34146e
Andrey Potyomkin <and_tem@icloud.com>
2022-05-13 13:21:10 +0300

    chore(ui): Test commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -31,7 +31,7 @@ module.exports = {
     "header-max-length": [2, "always", 72],
 
     // Область всегда только в нижнем регистре
-    "scope-case": [2, "always", "lower-case"],
+    "scope-case": [2, "always", ["camel-case", "pascal-case", "kebab-case", "lower-case"]],
 
     // Описание не может быть пустым
     "subject-empty": [2, "never"],
