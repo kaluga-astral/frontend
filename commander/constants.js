b9e5958515e87a14016c5fbf408045f08f3533e1
AlexandraLitvinenko <litvinenko_aa@astralnalog.ru>
2022-05-17 16:00:25 +0300

    fix(form): исправлены ошибки eslint

--- commander/constants.js
+++ commander/constants.js
@@ -1,11 +1,11 @@
-const DIST_DIR_NAME = "lib";
+const DIST_DIR_NAME = 'lib';
 
 const PACKAGES_NAMES = [
-  "@astral/ui",
-  "@astral/icons",
-  "@astral/fonts",
-  "@astral/illustrations",
-  "@astral/form",
+  '@astral/ui',
+  '@astral/icons',
+  '@astral/fonts',
+  '@astral/illustrations',
+  '@astral/form',
 ];
 
 module.exports = {
