b4293bdab5dc11b45dc7c305487200f7bd5e5a4e
Andrey Potyomkin <and_tem@icloud.com>
2022-05-23 18:29:41 +0300

    chore: удалено правило для scope-case

--- commitlint.config.js
+++ commitlint.config.js
@@ -29,13 +29,6 @@ module.exports = {
     // Максимальная длина заголовка 90 символов
     "header-max-length": [2, "always", 90],
 
-    // Для scope разрешается использовать только: lower-case, camel-case, kebab-case, pascal-case
-    // "scope-case": [
-    //   2,
-    //   "never",
-    //   ["upper-case", "sentence-case", "snake-case", "start-case"]
-    // ],
-
     // Описание не может быть пустым
     "subject-empty": [2, "never"],
 
