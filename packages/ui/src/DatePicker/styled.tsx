83961c5976a31224f1b75d082b92d9f56831e84e
Alexey Malyshev <lordniken@mail.ru>
2022-05-23 12:47:17 +0300

    UIKIT-188: feat(ui,DatePicker): исправлены стили по stylelint

--- packages/ui/src/DatePicker/styled.tsx
+++ packages/ui/src/DatePicker/styled.tsx
@@ -2,85 +2,89 @@ import { styled } from '../styles';
 
 export const DatePickerWrapper = styled.div`
   .react-datepicker {
-    background-color: ${({ theme }) => theme.palette.background.paper};
-    box-shadow: ${({ theme }) => theme.elevation[200]};
-    border-radius: ${({ theme }) => theme.spacing(1)};
-    margin-top: ${({ theme }) => theme.spacing(2)};
-    padding: ${({ theme }) => theme.spacing(4)};
     min-width: 320px;
     min-height: 300px;
+    margin-top: ${({ theme }) => theme.spacing(2)};
+    padding: ${({ theme }) => theme.spacing(4)};
+
+    background-color: ${({ theme }) => theme.palette.background.paper};
+    border-radius: ${({ theme }) => theme.spacing(1)};
+    box-shadow: ${({ theme }) => theme.elevation[200]};
 
     & .react-datepicker__day-names,
     .react-datepicker__week {
-      width: 287px;
       display: flex;
       justify-content: space-between;
+      width: 287px;
     }
 
     & .react-datepicker__day {
       & > button {
         font-weight: ${({ theme }) => theme.typography.fontWeightRegular};
       }
-      &-name {
-        width: 41px;
-        text-align: center;
-        text-transform: uppercase;
-        font-size: ${({ theme }) => theme.typography.h7.fontSize};
-        font-weight: ${({ theme }) => theme.typography.fontWeightMedium};
-        color: ${({ theme }) => theme.palette.grey[600]};
-        margin-top: ${({ theme }) => theme.spacing(3)};
-        margin-bottom: ${({ theme }) => theme.spacing(2)};
-      }
+
       &--outside-month > button {
         color: ${({ theme }) => theme.palette.grey[600]};
       }
+
+      &--disabled > button {
+        color: ${({ theme }) => theme.palette.grey[300]};
+        font-weight: ${({ theme }) => theme.typography.fontWeightRegular};
+
+        cursor: default;
+      }
+
       &--selected > button {
         color: ${({ theme }) => theme.palette.common.white};
+
         background-color: ${({ theme }) => theme.palette.primary.main};
       }
-      &--today {
-        & > button {
-          position: relative;
-          font-weight: ${({ theme }) => theme.typography.fontWeightMedium};
 
-          &:before {
-            content: '';
-            position: absolute;
-            height: 2px;
-            width: 33px;
-            background-color: ${({ theme }) => theme.palette.primary[900]};
-            bottom: ${({ theme }) => theme.spacing(1)};
-          }
-        }
-        &.react-datepicker__day--selected {
-          & > button {
-            &:before {
-              background-color: ${({ theme }) => theme.palette.primary[200]};
-            }
-          }
-        }
+      &--today > button {
+        position: relative;
+
+        font-weight: ${({ theme }) => theme.typography.fontWeightMedium};
       }
-      &--disabled {
-        &:not(.react-datepicker__day--selected) > button {
-          &:hover,
-          :active {
-            background-color: transparent;
-          }
-        }
 
-        & > button {
-          cursor: default;
-          color: ${({ theme }) => theme.palette.grey[300]};
-          font-weight: ${({ theme }) => theme.typography.fontWeightRegular};
+      &--disabled > button:focus {
+        outline: none;
+      }
 
-          &:focus {
-            outline: none;
-          }
-        }
+      &--disabled:not(.react-datepicker__day--selected) > button:hover,
+      &--disabled:not(.react-datepicker__day--selected) > button:active {
+        background-color: transparent;
+      }
 
-        &:focus-visible {
-          outline: none;
-        }
+      &--disabled:focus-visible {
+        outline: none;
+      }
+
+      &--today > button::before {
+        position: absolute;
+        bottom: ${({ theme }) => theme.spacing(1)};
+
+        width: 33px;
+        height: 2px;
+
+        background-color: ${({ theme }) => theme.palette.primary[900]};
+
+        content: '';
+      }
+
+      &--today > .react-datepicker__day--selected > button::before {
+        background-color: ${({ theme }) => theme.palette.primary[200]};
+      }
+
+      &-name {
+        width: 41px;
+        margin-top: ${({ theme }) => theme.spacing(3)};
+        margin-bottom: ${({ theme }) => theme.spacing(2)};
+
+        color: ${({ theme }) => theme.palette.grey[600]};
+        font-weight: ${({ theme }) => theme.typography.fontWeightMedium};
+        font-size: ${({ theme }) => theme.typography.h7.fontSize};
+        text-align: center;
+        text-transform: uppercase;
       }
     }
   }
