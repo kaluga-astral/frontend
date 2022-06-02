805b42a8515090e5f3365b04ac2be92600837f44
StanislavDa <flugegeheimenbolsen@gmail.com>
2022-06-02 17:57:10 +0500

    UIKIT-210: feat(ui,Button): Добавлен флаг selected. Исправлен цвет в состоянии loading

--- packages/ui/src/Button/Button.tsx
+++ packages/ui/src/Button/Button.tsx
@@ -1,20 +1,31 @@
-import { forwardRef } from 'react';
 import { LoadingButton } from '@mui/lab';
+import { forwardRef, useMemo } from 'react';
 
+import { ButtonVariants } from '../ButtonBase';
 import { CircularProgress } from '../CircularProgress';
+import { CircularProgressColors } from '../CircularProgress/constants';
 
 import { ButtonProps } from './types';
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   (props, ref) => {
-    const { variant = 'contained', ...restProps } = props;
+    const { variant = ButtonVariants.CONTAINED, ...restProps } = props;
+
+    const loadingIndicatorColor = useMemo(() => {
+      if (variant !== ButtonVariants.CONTAINED)
+        return CircularProgressColors.PRIMARY;
+
+      return CircularProgressColors.INVERTED;
+    }, [variant]);
 
     return (
       <LoadingButton
         ref={ref}
         {...restProps}
         variant={variant}
-        loadingIndicator={<CircularProgress size="small" />}
+        loadingIndicator={
+          <CircularProgress color={loadingIndicatorColor} size="small" />
+        }
       />
     );
   }
