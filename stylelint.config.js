e5d184ebb051ed1ef0cb1c0981537b88eb73ac65
Andrey Potyomkin <and_tem@icloud.com>
2022-06-10 08:38:59 +0300

    chore: Интегрирован общий stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,33 +1,3 @@
 module.exports = {
-  overrides: [
-    {
-      files: ['./**/*.ts', './**/*.tsx'],
-      customSyntax: '@stylelint/postcss-css-in-js',
-      extends: [
-        'stylelint-config-standard',
-        // группирует правила по блокам
-        'stylelint-config-rational-order',
-        'stylelint-prettier/recommended',
-      ],
-      plugins: ['stylelint-prettier', 'stylelint-order'],
-      rules: {
-        // отключено из-за несовместимости со styled
-        'function-no-unknown': null,
-        // отключено из-за несовместимости со styled
-        'function-name-case': null,
-        // отключено из-за несовместимости со styled
-        'value-keyword-case': null,
-        // разрешен kebab case (используется в mui)
-        'selector-class-pattern': '(([A-Z][a-z0-9]+)*)+(\_[a-z0-9]+([A-Z][a-z0-9]+)*)?(\-[a-z0-9]+([A-Z][a-z0-9]+)*)?',
-        'plugin/rational-order': [
-          true,
-          {
-            'empty-line-between-groups': true,
-          },
-        ],
-        // отключено из-за конфликта с plugin/rational-order
-        'declaration-empty-line-before': null,
-      },
-    },
-  ],
+  extends: ['@astral/stylelint-config-styled'],
 };
