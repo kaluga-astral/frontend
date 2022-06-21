44ee38224ce97e455037788de9fd3abb65c50cc4
Andrey Potyomkin <and_tem@icloud.com>
2022-06-21 17:37:12 +0300

    chore(UIKIT-284): Интегрирован общий commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 
+const { createConfig } = require('@astral/commitlint-config');
 const getDirNames = require('read-dir-names');
 
 const packagesNames = getDirNames(path.resolve(__dirname, 'packages'));
@@ -7,62 +8,7 @@ const componentsNames = getDirNames(
   path.resolve(__dirname, 'packages', 'ui', 'src')
 );
 
-module.exports = {
-  extends: [
-    "@commitlint/config-conventional"
-  ],
-
-  parserPreset: {
-    parserOpts: {
-      headerPattern: /^(?:UIKIT-[0-9]{2,4}:\s)?(\w*)(?:\((.*)\))?: (.*)$/,
-      headerCorrespondence: ["type", "scope", "subject"],
-      issuePrefixes: ["^UIKIT-[0-9]{2,4}"],
-    },
-  },
-  rules: {
-    // Тело коммита должно начинаться с пустой строки
-    "body-leading-blank": [2, "always"],
-
-    // Нижний колонтитул коммита должен начинаться с пустой строки
-    "footer-leading-blank": [2, "always"],
-
-    // Максимальная длина заголовка 90 символов
-    "header-max-length": [2, "always", 90],
-
-    // Описание не может быть пустым
-    "subject-empty": [2, "never"],
-
-    // Описание не должно заканчиваться '.'
-    "subject-full-stop": [2, "never", "."],
-
-    // Описание должно начинаться с большой буквы
-    "subject-case": [2, "always", "sentence-case"],
-
-    // Тип всегда только в нижнем регистре
-    "type-case": [2, "always", "lower-case"],
-
-    // Тип не может быть пустым
-    "type-empty": [2, "never"],
-
-    // Перечислим все возможные варианты коммитов
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'bug',
-        'wip',
-        'refactor',
-        'doc',
-        'build',
-        'chore',
-        'revert',
-        'style',
-        'wip',
-      ],
-    ],
-
-    // Перечисления доступных scope
-    'scope-enum': [2, 'always', [...packagesNames, ...componentsNames]],
-  },
-};
+module.exports = createConfig({
+  scopes: [...packagesNames, ...componentsNames],
+  ticketPrefix: 'UIKIT',
+});
