4c0f61834b3e9cdcd1bcf2fb781f5827de2d3fd9
Andrey Potyomkin <and_tem@icloud.com>
2022-06-22 18:42:34 +0300

    chore(UIKIT-284): Интегрирован общий commitlint config (#327)
    * chore(UIKIT-284): Интегрирован общий commitlint config
    
    * chore(UIKIT-284): Merge main
    
    * chore(UIKIT-284): Обновлены версии code style пакетов

--- packages/ui/src/DataGrid/DataGridHeadColumn/styled.tsx
+++ packages/ui/src/DataGrid/DataGridHeadColumn/styled.tsx
@@ -17,9 +17,11 @@ export const StyledTableCell = styled(TableCell, {
   shouldForwardProp: (prop) => prop !== 'width',
 })<StyledTableCellProps>`
   width: ${({ width = 'auto' }) => width};
+  padding: ${({ theme }) => theme.spacing(3, 4)};
+
+  color: ${({ theme }) => theme.palette.grey[700]};
 
   cursor: ${({ sortable }) => (sortable ? 'pointer' : 'initial')};
-  padding: ${({ theme }) => theme.spacing(3, 4)};
-  color: ${({ theme }) => theme.palette.grey[700]};
+
   user-select: none;
 `;
