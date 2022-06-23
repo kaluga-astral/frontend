a9e64f82b3a9cb62eea65537a1bf7e8e53114d64
vinokurov_and@mail.ru <vinokurov_and@mail.ru>
2022-06-23 14:41:03 +0300

    bug(UIKIT-310, ui): Исправлена ошибка типизации для Tooltip и CircularProgress

--- packages/ui/src/CircularProgress/CircularProgress.tsx
+++ packages/ui/src/CircularProgress/CircularProgress.tsx
@@ -1,25 +1,28 @@
 import { forwardRef, useMemo } from 'react';
 import MuiCircularProgress from '@mui/material/CircularProgress';
 
+import { WithoutEmotionSpecific } from '../types';
+
 import { CircularProgressProps } from './types';
 
-export const CircularProgress = forwardRef<HTMLElement, CircularProgressProps>(
-  (props, ref) => {
-    const { size: sizeProp, color, ...restProps } = props;
-    const size = useMemo(() => {
-      return sizeProp === 'small' ? 16 : 24;
-    }, [sizeProp]);
+export const CircularProgress = forwardRef<
+  HTMLElement,
+  WithoutEmotionSpecific<CircularProgressProps>
+>((props, ref) => {
+  const { size: sizeProp, color, ...restProps } = props;
+  const size = useMemo(() => {
+    return sizeProp === 'small' ? 16 : 24;
+  }, [sizeProp]);
 
-    return (
-      <MuiCircularProgress
-        ref={ref}
-        {...restProps}
-        size={size}
-        color="inherit"
-        data-color={color}
-      />
-    );
-  },
-);
+  return (
+    <MuiCircularProgress
+      ref={ref}
+      {...restProps}
+      size={size}
+      color="inherit"
+      data-color={color}
+    />
+  );
+});
 
 export default CircularProgress;
--- packages/ui/src/Tooltip/Tooltip.tsx
+++ packages/ui/src/Tooltip/Tooltip.tsx
@@ -1,10 +1,15 @@
 import { forwardRef } from 'react';
 
+import { WithoutEmotionSpecific } from '../types';
+
 import { TooltipProps } from './types';
 import { StyledTooltip } from './styled';
 import { TooltipSizes } from './constants';
 
-export const Tooltip = forwardRef<HTMLDivElement, TooltipProps>(
+export const Tooltip = forwardRef<
+  HTMLDivElement,
+  WithoutEmotionSpecific<TooltipProps>
+>(
   (
     { children, placement = 'top', size = TooltipSizes.MEDIUM, ...props },
     ref,
