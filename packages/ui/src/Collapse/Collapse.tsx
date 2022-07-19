f3e9d69137cce23673bb1583099c795484bf505f
SergeyShevtsov <sergeyprostoas@gmail.com>
2022-07-19 10:38:43 +0300

    bug(UIKIT-329,ui,Collapse): Файлы "styled.ts" переименованны в "styles.ts"

--- packages/ui/src/Collapse/Collapse.tsx
+++ packages/ui/src/Collapse/Collapse.tsx
@@ -1,6 +1,6 @@
 import { CollapseProps } from '@mui/material';
 
-import { StyledCollapse } from './styled';
+import { StyledCollapse } from './styles';
 
 export const Collapse = (props: CollapseProps) => {
   return <StyledCollapse {...props} />;
