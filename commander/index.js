469d7e455180ba1cedb1747a5a6247d1eed16193
Andrey Potyomkin <and_tem@icloud.com>
2022-07-26 16:17:10 +0300

    chore(UIKIT-335,commander): Добавлена команда для линтинга title PR

--- commander/index.js
+++ commander/index.js
@@ -1,42 +1,12 @@
 #!/usr/bin/env node
 
-const { copyCommonFiles } = require('./tasks/copyCommonFiles');
-const { modifyPackageJSON } = require('./tasks/modifyPackageJSON');
-const { build } = require('./tasks/build');
-const { copyFonts } = require('./tasks/copyFonts');
-const { publish } = require('./tasks/publish');
-const { copyImages } = require('./tasks/copyImages');
+const { hideBin } = require('yargs/helpers');
+const yargs = require('yargs/yargs');
+
+const { runTask } = require('./main');
 
 const [, , task] = process.argv;
 
-switch (task) {
-  case 'build':
-    build();
+const { argv } = yargs(hideBin(process.argv));
 
-    break;
-  case 'copyFonts':
-    copyFonts();
-
-    break;
-  case 'copyImages':
-    copyImages();
-
-    break;
-  case 'publish':
-    publish();
-
-    break;
-  case 'copyCommonFiles':
-    copyCommonFiles();
-
-    break;
-  case 'modifyPackageJSON':
-    modifyPackageJSON();
-
-    break;
-  default:
-    console.error('Task not found');
-    process.exit(1);
-
-    break;
-}
+runTask({ task, args: argv });
