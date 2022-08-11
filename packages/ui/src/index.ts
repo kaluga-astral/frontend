2ca84cd27011df793119371fe2a03a74ab750f7b
SergeyShevtsov <sergeyprostoas@gmail.com>
2022-08-11 11:46:36 +0300

    chore(UIKIT-281,ui): В корневой index.ts добавлены недостающие компоненты

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -34,6 +34,8 @@ export * from './DialogTitle';
 
 export * from './DropdownButton';
 
+export * from './Fade';
+
 export * from './FormControlLabel';
 
 export * from './FormHelperText';
@@ -41,6 +43,8 @@ export * from './FormHelperText';
 export * from './GlobalStyles';
 
 export * from './Grid';
+
+export * from './Grow';
 
 export * from './IconButton';
 
@@ -73,6 +77,8 @@ export * from './MenuItem';
 export * from './MenuList';
 
 export * from './MenuGroup';
+
+export * from './OverflowTypography';
 
 export * from './Placeholder';
 
@@ -115,6 +121,10 @@ export * from './SideDialogActions';
 export * from './SideDialogContent';
 
 export * from './SideDialogContentText';
+
+export * from './Slide';
+
+export * from './Zoom';
 
 export * from './Notification';
 
