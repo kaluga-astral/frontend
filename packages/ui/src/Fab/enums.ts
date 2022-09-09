4bcab7836b197048b24534af9fd5cf47790ce638
Олег <35649223+olewka40@users.noreply.github.com>
2022-09-09 10:57:13 +0300

    feat(UIKIT-399): Реализован компонент Fab и storybook под него (#388)

---
+++ packages/ui/src/Fab/enums.ts
@@ -0,0 +1,19 @@
+export enum FabSizes {
+  Small = 'small',
+  Medium = 'medium',
+  Large = 'large',
+}
+
+export enum FabStates {
+  Default = 'default',
+  Hover = 'hover',
+  Active = 'active',
+}
+
+export enum FabColors {
+  Default = 'default',
+  Primary = 'primary',
+  Error = 'error',
+  Success = 'success',
+  Warning = 'warning',
+}
