f4bbaf2abd8a83593848b8a377fec67d034ef934
SergeyShevtsov <sergeyprostoas@gmail.com>
2022-09-12 14:18:47 +0300

    chore(UIKIT-423,ui,Alert): Добавлены стили компоненту Alert

--- packages/ui/src/Alert/styles.ts
+++ packages/ui/src/Alert/styles.ts
@@ -2,6 +2,11 @@ import { Alert, AlertProps } from '@mui/material';
 
 import { styled } from '../styles';
 
+/**
+ * @description Выбор брейкпоинта для медиа-запроса.
+ */
+const getBreakpointSM = (): string => '600px';
+
 export const StyledAlert = styled(Alert)<AlertProps>`
   padding: ${({ theme }) => theme.spacing(3)};
 
@@ -9,7 +14,7 @@ export const StyledAlert = styled(Alert)<AlertProps>`
     padding: ${({ theme }) => theme.spacing(3, 0)};
   }
 
-  @media screen and (min-width: 600px) {
+  @media screen and (min-width: ${getBreakpointSM()}) {
     padding: ${({ theme }) => theme.spacing(2, 4)};
 
     .MuiAlert-message > *:last-child:not(:empty):not(.MuiTypography-root) {
