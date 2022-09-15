068a4cfb386aa93808ea7469d6081c5b295be33f
StanislavDa <flugegeheimenbolsen@gmail.com>
2022-09-15 11:05:33 +0500

    bug(UIKIT-420,ui,Typography): Перенесены типы Typography в компонент

--- packages/ui/src/Typography/Typography.tsx
+++ packages/ui/src/Typography/Typography.tsx
@@ -1,8 +1,40 @@
-import { Typography as MuiTypography } from '@mui/material';
+import {
+  Typography as MuiTypography,
+  TypographyPropsVariantOverrides,
+  TypographyTypeMap,
+} from '@mui/material';
+import { OverrideProps } from '@mui/material/OverridableComponent';
+import { Variant } from '@mui/material/styles/createTypography';
 import React, { forwardRef, useMemo } from 'react';
 
+import { Theme } from '../theme';
+
 import { TypographyColors } from './enums';
-import { TypographyProps } from './types';
+
+export type TypographyColor = keyof typeof TypographyColors;
+
+export type TypographyProps = Omit<
+  OverrideProps<TypographyTypeMap, React.ElementType>,
+  'variant' | 'color'
+> & {
+  variant?: Variant | keyof TypographyPropsVariantOverrides;
+  component?: React.ElementType;
+  color?: TypographyColor | ((theme: Theme) => string);
+};
+
+declare module '@mui/material/Typography' {
+  interface TypographyPropsVariantOverrides {
+    h7: true;
+    h8: true;
+    h9: true;
+    ui: true;
+    link: true;
+    pointer: true;
+    small: true;
+    code: true;
+    div: true;
+  }
+}
 
 export const Typography: React.FC<TypographyProps> = forwardRef<
   HTMLElement,
