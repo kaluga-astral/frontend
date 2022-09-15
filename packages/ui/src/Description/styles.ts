6b7a00e0b82a6dfcc04d226f0d3507c54082c608
Олег <35649223+olewka40@users.noreply.github.com>
2022-09-15 14:27:16 +0300

    feat(UIKIT-428,ui,Description): Изменён размер шрифта для mobile и default цвет для Name (#402)

--- packages/ui/src/Description/styles.ts
+++ packages/ui/src/Description/styles.ts
@@ -4,7 +4,12 @@ export const DescriptionWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   align-items: baseline;
+
   ${({ theme }) => theme.breakpoints.down('sm')} {
     flex-direction: column;
+
+    .MuiTypography-root {
+      font-size: ${({ theme }) => theme.typography.fontSize};
+    }
   }
 `;
