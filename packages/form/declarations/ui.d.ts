aedb83c7eec1697c2d80ac8b058b8fe59d5cebdd
Andrey Potyomkin <and_tem@icloud.com>
2022-09-20 13:49:08 +0300

    chore(UIKIT-140,form): Убран ui.d.ts файл, поправлены команды билда

--- packages/form/declarations/ui.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="@astral/ui/src/declaration/emotion" />
-/// <reference types="@astral/ui/src/declaration/mui-material" />
-/// <reference types="@emotion/react/types/css-prop" />
