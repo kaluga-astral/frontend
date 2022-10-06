016e319ce1bd2f6b9309a71d99805e778cae1a47
StanislavDa <flugegeheimenbolsen@gmail.com>
2022-10-06 14:36:39 +0500

    feat(UIKIT-142,form,FormMaskField): Реализован адаптер для MaskField

---
+++ packages/form/src/FormMaskField/FormMaskField.tsx
@@ -0,0 +1,27 @@
+import { MaskField, MaskFieldProps } from '@astral/ui';
+import { useController } from 'react-hook-form';
+
+import { useFieldErrorProps } from '../hooks';
+import { WithFormFieldProps } from '../types';
+
+/**
+ * @description Тип значения, которое сетится в state формы
+ */
+export type FormMaskFieldValue = string;
+
+export type FormMaskFieldProps<FieldValues extends object> = WithFormFieldProps<
+  MaskFieldProps,
+  FieldValues
+>;
+
+/**
+ * @description Тестовый инпут с маской для формы
+ */
+export function FormMaskField<FieldValues extends object>(
+  props: FormMaskFieldProps<FieldValues>,
+) {
+  const { field, fieldState } = useController(props);
+  const errorProps = useFieldErrorProps(fieldState);
+
+  return <MaskField {...field} {...props} {...errorProps} />;
+}
