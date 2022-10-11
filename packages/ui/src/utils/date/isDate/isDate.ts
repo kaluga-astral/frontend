26f56423c8b6c3be6619131ee0fafb3762ae56c9
ofigelov <konungivan@gmail.com>
2022-10-11 12:48:33 +0500

    feat(UIKIT-419,ui,DatePicker): использование date-fns для простых методов

--- packages/ui/src/utils/date/isDate/isDate.ts
+++ packages/ui/src/utils/date/isDate/isDate.ts
@@ -1,9 +1,7 @@
+import { isDate as fnsIsDate } from 'date-fns';
+
 /**
  * @description функция проверки значения на дату
  */
 export const isDate = (value: unknown): value is Date =>
-  value !== null &&
-  (value instanceof Date ||
-    (typeof value === 'object' &&
-      Object.prototype.toString.call(value) === '[object Date]')) &&
-  !isNaN(+value);
+  fnsIsDate(value) && !isNaN(Number(value));
