14f320011e5f5727ce05d23af49ada4ddbc593b3
StanislavDa <flugegeheimenbolsen@gmail.com>
2022-10-11 11:08:30 +0500

    feat(UIKIT-462,form,FormMobilePhoneField): Изменены декларации

---
+++ packages/ui/declarations/declarations.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="@astral/ui/declaration/emotion" />
+/// <reference types="@astral/ui/declaration/mui-material" />
+/// <reference types="@emotion/react/types/css-prop" />
+
+declare module '*.svg' {
+  const image: string;
+
+  export default image;
+}
