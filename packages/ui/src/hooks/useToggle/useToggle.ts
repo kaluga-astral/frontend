2fe48325d0f4b8b64a87fd4a0f2e36aca7b9723e
Sedykh Ivan <konungivan@gmail.com>
2022-10-21 14:34:10 +0500

    bug(UIKIT-485,ui,Datepicker): дженерики для useToggle

--- packages/ui/src/hooks/useToggle/useToggle.ts
+++ packages/ui/src/hooks/useToggle/useToggle.ts
@@ -1,39 +1,41 @@
-import { SyntheticEvent, useState } from 'react';
+import { useState } from 'react';
 
-import { Reason } from '../../types';
+type Arguments = unknown[];
 
-type OpenHandler = () => void;
-type CloseHandler = (
-  event?: SyntheticEvent<Element, Event> | Event,
-  reason?: Reason,
-) => void;
+type Handler<T extends Arguments> = (...a: T) => void;
+type OpenHandler<T extends Arguments> = Handler<T>;
+type CloseHandler<T extends Arguments> = Handler<T>;
 
-type UseToggleResultTuple = [boolean, OpenHandler, CloseHandler];
+type UseToggleResultTuple<C extends Arguments, O extends Arguments> = [
+  boolean,
+  OpenHandler<O>,
+  CloseHandler<C>,
+];
 
-type UseToggleOptions =
-  | {
-      initialState?: boolean;
-      onActive?: () => void;
-      onInactive?: CloseHandler;
-    }
-  | undefined;
+type UseToggleOptions<C extends Arguments, O extends Arguments> = {
+  initialState?: boolean;
+  onActive?: OpenHandler<O>;
+  onInactive?: CloseHandler<C>;
+};
 
 /**
  * @description хук хранящий стейт активности, и предоставляющий методы переключения, позволяет уменьшить бойлерплейт код
  */
 
-export const useToggle = (params: UseToggleOptions): UseToggleResultTuple => {
+export const useToggle = <C extends Arguments, O extends Arguments>(
+  params?: UseToggleOptions<C, O>,
+): UseToggleResultTuple<C, O> => {
   const { initialState = false, onInactive, onActive } = params || {};
   const [isActive, setActive] = useState(initialState);
 
-  const handleActive = () => {
+  const handleActive: OpenHandler<O> = (...args) => {
     setActive(true);
-    onActive?.();
+    onActive?.(...args);
   };
 
-  const handleInactive: CloseHandler = (_, reason) => {
+  const handleInactive: CloseHandler<C> = (...args) => {
     setActive(false);
-    onInactive?.(_, reason);
+    onInactive?.(...args);
   };
 
   return [isActive, handleActive, handleInactive];
