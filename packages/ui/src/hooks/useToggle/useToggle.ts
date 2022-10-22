fd18b6bf99cfac61c53579a40d744e5e588b7bd8
Sedykh Ivan <konungivan@gmail.com>
2022-10-22 09:46:25 +0500

    bug(UIKIT-485,ui,Datepicker): комментарии для useToggle

--- packages/ui/src/hooks/useToggle/useToggle.ts
+++ packages/ui/src/hooks/useToggle/useToggle.ts
@@ -1,39 +1,73 @@
 import { useState } from 'react';
 
+/**
+ * @description тип обозначающий какой угодно рест аргументов
+ */
 type Arguments = unknown[];
 
-type Handler<T extends Arguments> = (...a: T) => void;
-type OpenHandler<T extends Arguments> = Handler<T>;
-type CloseHandler<T extends Arguments> = Handler<T>;
+/**
+ * @description хэндлер принимающий на вход неизвестное колличество аргументов, неизвестного вида
+ */
+type Handler<Args extends Arguments> = (...a: Args) => void;
+/**
+ * @description хэндлер на открытие с неизвестным колличеством аргументов, неизвестного вида
+ */
+type OpenHandler<Args extends Arguments> = Handler<Args>;
+/**
+ * @description хэндлер на закрытие с неизвестным колличеством аргументов, неизвестного вида
+ */
+type CloseHandler<Args extends Arguments> = Handler<Args>;
 
-type UseToggleResultTuple<C extends Arguments, O extends Arguments> = [
+type UseToggleResultTuple<
+  CloseArgs extends Arguments,
+  OpenArgs extends Arguments,
+> = [
+  /**
+   * @description флаг активности состояния
+   */
   boolean,
-  OpenHandler<O>,
-  CloseHandler<C>,
+  OpenHandler<OpenArgs>,
+  CloseHandler<CloseArgs>,
 ];
 
-type UseToggleOptions<C extends Arguments, O extends Arguments> = {
+type UseToggleOptions<
+  CloseArgs extends Arguments,
+  OpenArgs extends Arguments,
+> = {
+  /**
+   * @description изначальное состояние активности
+   */
   initialState?: boolean;
-  onActive?: OpenHandler<O>;
-  onInactive?: CloseHandler<C>;
+  /**
+   * @description коллбэк на открытие
+   */
+  onActive?: OpenHandler<OpenArgs>;
+  /**
+   * @description коллбэк на закрытие
+   */
+  onInactive?: CloseHandler<CloseArgs>;
 };
 
 /**
- * @description хук хранящий стейт активности, и предоставляющий методы переключения, позволяет уменьшить бойлерплейт код
+ * @description хук хранящий стейт активности, и предоставляющий методы переключения c проксируемыми аргументами, позволяет уменьшить бойлерплейт код
  */
-
-export const useToggle = <C extends Arguments, O extends Arguments>(
-  params?: UseToggleOptions<C, O>,
-): UseToggleResultTuple<C, O> => {
+export const useToggle = <
+  CloseArgs extends Arguments,
+  OpenArgs extends Arguments,
+>(
+  params?: UseToggleOptions<CloseArgs, OpenArgs>,
+): UseToggleResultTuple<CloseArgs, OpenArgs> => {
   const { initialState = false, onInactive, onActive } = params || {};
   const [isActive, setActive] = useState(initialState);
 
-  const handleActive: OpenHandler<O> = (...args) => {
+  // выставляем флаг активности в true, и проксируем все принятые аргументы
+  const handleActive: OpenHandler<OpenArgs> = (...args) => {
     setActive(true);
     onActive?.(...args);
   };
 
-  const handleInactive: CloseHandler<C> = (...args) => {
+  // выставляем флаг активности в false, и проксируем все принятые аргументы
+  const handleInactive: CloseHandler<CloseArgs> = (...args) => {
     setActive(false);
     onInactive?.(...args);
   };
