236b5bf9ebfb54c59d350a50bc8186c73a19c575
Sedykh Ivan <konungivan@gmail.com>
2022-10-31 18:08:34 +0500

    feat(UIKIT-354,ui,Types): пустой

--- packages/ui/src/Badge/Badge.tsx
+++ packages/ui/src/Badge/Badge.tsx
@@ -1,10 +1,8 @@
 import { BadgeProps } from './types';
 import { StyledBadge } from './styled';
 
-export const Badge = ({ children, color, ...props }: BadgeProps) => {
-  return (
-    <StyledBadge customColor={color} {...props} max={99}>
-      {children}
-    </StyledBadge>
-  );
-};
+export const Badge = ({ children, color, ...props }: BadgeProps) => (
+  <StyledBadge customColor={color} {...props} max={99}>
+    {children}
+  </StyledBadge>
+);
