0c6e787acf93354656e6b425d2add3bf60b46fee
vinokurov_and@mail.ru <vinokurov_and@mail.ru>
2022-11-08 13:35:29 +0300

    feat(UIKIT-512,ui,Stepper): Деструктуризирован объект с классами

--- packages/ui/src/theme/components/MuiStepLabel.ts
+++ packages/ui/src/theme/components/MuiStepLabel.ts
@@ -5,18 +5,19 @@ import type { Theme } from '../baseTheme';
 export const MuiStepLabel: Components<Theme>['MuiStepLabel'] = {
   styleOverrides: {
     label({ theme }) {
+      const { active, completed, error } = stepLabelClasses;
+
       return {
         fontSize: theme.typography.small.fontSize,
         lineHeight: theme.typography.small.lineHeight,
         fontWeight: theme.typography.small.fontWeight,
         color: theme.palette.grey[700],
-        [`&.${stepLabelClasses.active}, &.${stepLabelClasses.completed} , &.${stepLabelClasses.error}`]:
-          {
-            color: theme.palette.grey[900],
-            fontSize: theme.typography.small.fontSize,
-            lineHeight: theme.typography.small.lineHeight,
-            fontWeight: theme.typography.small.fontWeight,
-          },
+        [`&.${active}, &.${completed}, &.${error}`]: {
+          color: theme.palette.grey[900],
+          fontSize: theme.typography.small.fontSize,
+          lineHeight: theme.typography.small.lineHeight,
+          fontWeight: theme.typography.small.fontWeight,
+        },
       };
     },
     iconContainer({ theme }) {
