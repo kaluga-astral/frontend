e6200c4356ef5457cb2ffef17f711c35edbb7ef5
aesarantsev <47385491+aesarantsev@users.noreply.github.com>
2022-11-18 15:57:38 +1000

    bug(UIKIT-523,ui,DataGrid): Исправлено дублирование отмеченных строк в onSelectRow (#470)

--- packages/ui/src/DataGrid/DataGrid.tsx
+++ packages/ui/src/DataGrid/DataGrid.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, ReactNode, useCallback, useMemo } from 'react';
+import { uniqBy } from 'lodash-es';
 
 import { Table } from '../Table';
 
@@ -125,7 +126,7 @@ export function DataGrid<
     }
 
     if (event.target.checked) {
-      const mergedSelectedRows = [...selectedRows, ...rows];
+      const mergedSelectedRows = uniqBy([...selectedRows, ...rows], keyId);
 
       return onSelectRow(mergedSelectedRows);
     }
