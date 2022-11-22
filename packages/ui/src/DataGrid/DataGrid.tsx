56b66ba964eaa6234e92b10011e8fb8d3ff710bf
Andrey Sarantsev <andrrr96@yandex.ru>
2022-11-22 17:27:02 +1000

    feat(UIKIT-524,ui,DataGrid): Реализовано disabled состояние по обновленному дизайну

--- packages/ui/src/DataGrid/DataGrid.tsx
+++ packages/ui/src/DataGrid/DataGrid.tsx
@@ -6,7 +6,11 @@ import { DataGridHead } from './DataGridHead';
 import { DataGridBody } from './DataGridBody';
 import DataGridLoader from './DataGridLoader/DataGridLoader';
 import { DataGridNoData } from './DataGridNoData';
-import { DataGridContainer, StyledTableContainer } from './styles';
+import {
+  DataGridContainer,
+  DisabledTableContainer,
+  StyledTableContainer,
+} from './styles';
 import { DataGridColumns, DataGridRow, DataGridSort } from './types';
 
 export type DataGridProps<
@@ -76,6 +80,11 @@ export type DataGridProps<
    */
   loading?: boolean;
   /**
+   * @example <DataGrid  disabled={true} />
+   * Флажок блокировки таблицы
+   */
+  disabled?: boolean;
+  /**
    * @default '-'
    * @example <DataGrid  emptyCellValue{'Нет данных'} />
    * Заглушка для пустых ячеек (если отсутствует field и filter и renderCell)
@@ -110,6 +119,7 @@ export function DataGrid<
   Footer,
   noDataPlaceholder,
   loading,
+  disabled,
   onSort,
   keyId,
   emptyCellValue,
@@ -118,6 +128,9 @@ export function DataGrid<
 }: DataGridProps<Data, SortField>) {
   const selectable = Boolean(onSelectRow);
   const withFooter = Boolean(Footer);
+
+  const TableContainer =
+    loading || disabled ? DisabledTableContainer : StyledTableContainer;
 
   const handleSelectAllRows = (event: ChangeEvent<HTMLInputElement>): void => {
     if (!onSelectRow) {
@@ -174,7 +187,7 @@ export function DataGrid<
 
   return (
     <DataGridContainer maxHeight={maxHeight} className={className}>
-      <StyledTableContainer>
+      <TableContainer inert={loading || disabled ? '' : false}>
         <Table stickyHeader>
           <DataGridHead<Data, SortField>
             onSort={onSort}
@@ -198,10 +211,11 @@ export function DataGrid<
             noDataPlaceholder={renderedPlaceholder()}
           />
         </Table>
-      </StyledTableContainer>
+      </TableContainer>
       <DataGridLoader
         footerHeight={footerHeight}
         withFooter={withFooter}
+        disabled={disabled}
         loading={loading}
       />
       {Footer}
