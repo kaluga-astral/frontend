c2622b3965bffdbc7b252e53aac4183ba70d6141
Andrey Potyomkin <and_tem@icloud.com>
2022-11-28 10:35:32 +0300

    test: Поправлены зависимости для vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,5 @@
 /// <reference types="vitest" />
 import path from 'path';
-
-// eslint-disable-next-line import/no-extraneous-dependencies
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
