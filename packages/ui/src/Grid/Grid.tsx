59743e9c704fcdba136785facb4082ca019503a3
vinokurov_and@mail.ru <vinokurov_and@mail.ru>
2022-12-06 01:14:47 +0300

    bug(UIKIT-538,ui,Grid): Добавлен ref проброс

--- packages/ui/src/Grid/Grid.tsx
+++ packages/ui/src/Grid/Grid.tsx
@@ -1,43 +1,54 @@
-import { GridProps } from './types';
+import { ReactNode, forwardRef } from 'react';
+
+import { GridContainerProps, GridElementsProps } from './types';
 import { StyledGrid } from './styled';
 
-export const Grid = ({
-  container = false,
-  templateColumns,
-  templateRows,
-  templateAreas,
-  columnSpacing,
-  rowSpacing,
-  spacing,
-  autoColumns,
-  autoRows,
-  autoFlow,
-  column,
-  row,
-  area,
-  component = 'div',
-  children,
-  ...props
-}: GridProps) => {
-  return (
-    <StyledGrid
-      container={container}
-      gridTemplateColumns={templateColumns}
-      gridTemplateRows={templateRows}
-      gridTemplateAreas={templateAreas}
-      columnGap={columnSpacing}
-      rowGap={rowSpacing}
-      gap={spacing}
-      gridAutoColumns={autoColumns}
-      gridAutoRows={autoRows}
-      gridAutoFlow={autoFlow}
-      gridColumn={column}
-      gridRow={row}
-      gridArea={area}
-      component={component}
-      {...props}
-    >
-      {children}
-    </StyledGrid>
-  );
-};
+export type GridProps = GridContainerProps &
+  GridElementsProps & { children?: ReactNode };
+
+export const Grid = forwardRef<HTMLElement, GridProps>(
+  (
+    {
+      container = false,
+      templateColumns,
+      templateRows,
+      templateAreas,
+      columnSpacing,
+      rowSpacing,
+      spacing,
+      autoColumns,
+      autoRows,
+      autoFlow,
+      column,
+      row,
+      area,
+      component = 'div',
+      children,
+      ...props
+    },
+    ref,
+  ) => {
+    return (
+      <StyledGrid
+        ref={ref}
+        container={container}
+        gridTemplateColumns={templateColumns}
+        gridTemplateRows={templateRows}
+        gridTemplateAreas={templateAreas}
+        columnGap={columnSpacing}
+        rowGap={rowSpacing}
+        gap={spacing}
+        gridAutoColumns={autoColumns}
+        gridAutoRows={autoRows}
+        gridAutoFlow={autoFlow}
+        gridColumn={column}
+        gridRow={row}
+        gridArea={area}
+        component={component}
+        {...props}
+      >
+        {children}
+      </StyledGrid>
+    );
+  },
+);
