7afada44612667210624e94a20dc96697d735eed
Sedykh Ivan <konungivan@gmail.com>
2022-12-08 16:49:21 +0500

    feat(UIKIT-551,ui,AwayClickListener): добавил jsdoc для BackdropStackManager

--- packages/ui/src/BackdropStack/services/BackdropStackManager/BackdropStackManager.ts
+++ packages/ui/src/BackdropStack/services/BackdropStackManager/BackdropStackManager.ts
@@ -1,10 +1,31 @@
 import { CloseEventReason } from '../../../types';
-
+/**
+ * @description айдишка модалки/поппера/тултипа
+ */
 type PopId = string;
+/**
+ * @description айдишка клика по странице,
+ * чтобы идентифицировать клики по странице,
+ * например клик по серой зоне модалки,
+ * с открытым поппером приводит к срабатыванию закрытия сразу обоих элементов,
+ * и поппера, и тултипа,
+ * и благодаря этой айдишке, мы можем понять,
+ * что оба компонента пытаются закрыться одновременно,
+ * по одному и тому же клику, и мы будем это предотвращать
+ */
 type PointerId = string | null;
 
+/**
+ * @description тупл отображающий комбинацию причины закрытия,
+ * и айдишки клика в рамках которого происходит закрытие
+ */
 type PopTuple = [CloseEventReason | undefined, PointerId];
 
+/**
+ * @description расширяемый набор конфликтующих причин закрытия с кликом по бэкдропу модалки,
+ * т.к. некоторые способы закрытия вызываются раньше, чем вызов backdropClick,
+ * а при несоблюдении последовательности возникают конфликты
+ */
 const backdropConflictsReasons = new Set<CloseEventReason | undefined>([
   'blur',
   'awayClick',
@@ -13,10 +34,15 @@ const backdropConflictsReasons = new Set<CloseEventReason | undefined>([
 class BackdropStackManager {
   private stack: PopId[] = [];
 
-  // сохраняем информацию о том какой был предыдущий вызов pop,
-  // нужен для предотвращения синхронного закрытия при последовательности 'blur' и 'backdropClick' в рамках одного клика
+  /**
+   * @description сохраняем информацию о том какой был предыдущий вызов pop,
+   * нужен для предотвращения синхронного закрытия при последовательности 'blur' и 'backdropClick' в рамках одного клика
+   */
   private previousPopInfo: PopTuple = [null, null];
 
+  /**
+   * @description крайняя айдишка клика по странице
+   */
   private currentPointerId: PointerId = null;
 
   constructor() {
@@ -26,6 +52,9 @@ class BackdropStackManager {
     );
   }
 
+  /**
+   * @description  метод проверки на конфликт причины закрытия с предыдущим
+   */
   private checkOn = (reason?: CloseEventReason): boolean => {
     const [previousReason, previousPointerId] = this.previousPopInfo;
 
@@ -36,18 +65,33 @@ class BackdropStackManager {
     );
   };
 
+  /**
+   * @description  метод генерации случайной айдишки
+   */
   public generateID = () => {
     return String(Math.random());
   };
 
+  /**
+   * @description форс метод для удалении подписанного компонента из очереди,
+   * может быть полезно при массовом анмаунте компонентов
+   */
   public remove = (id: PopId) => {
     this.stack = this.stack.filter((item) => item !== id);
   };
 
+  /**
+   * @description метод добавления в стек
+   */
   public push = (id: PopId) => {
     this.stack.push(id);
   };
 
+  /**
+   * @description метод для запроса на выход из очереди,
+   * если все ок, то вернется true, что означает разрешение на выход и компонент будет вычеркнут из очереди,
+   * соответственно возврат false означает запрет, и компонент остается в очереди
+   */
   public pop = (id: PopId, reason?: CloseEventReason): boolean => {
     if (this.checkOn(reason)) {
       return false;
