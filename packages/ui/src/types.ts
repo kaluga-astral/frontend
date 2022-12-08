1c30f537e3a532eb08dd7b042f0cbee5b14b8a8d
Sedykh Ivan <konungivan@gmail.com>
2022-12-08 13:26:50 +0500

    feat(UIKIT-551,ui,AwayClickListener): добавил jsdoc

--- packages/ui/src/types.ts
+++ packages/ui/src/types.ts
@@ -2,6 +2,10 @@ import { AutocompleteCloseReason } from '@mui/base/AutocompleteUnstyled/useAutoc
 
 export type WithoutEmotionSpecific<Props> = Omit<Props, 'sx' | 'css'>;
 
+/**
+ * @description список возможных причин закрытия попперов, модалок, тултипов,
+ * является абстрактным способом связать сущности всплывашек с BackdropStackManager
+ */
 export type CloseEventReason =
   | 'escapeKeyDown'
   | 'backdropClick'
