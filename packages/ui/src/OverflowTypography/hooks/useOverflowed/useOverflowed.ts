0c729d3de010d3183123fad8a3c5f9074ac0d2e3
Sedykh Ivan <konungivan@gmail.com>
2022-12-09 14:47:18 +0500

    bug(UIKIT-578,ui,OverflowTypography): добавлен jsdoc

--- packages/ui/src/OverflowTypography/hooks/useOverflowed/useOverflowed.ts
+++ packages/ui/src/OverflowTypography/hooks/useOverflowed/useOverflowed.ts
@@ -19,7 +19,13 @@ export const useOverflowed = (forwardedRef?: Ref<HTMLElement>) => {
 
   const handleResize = useCallback(
     debounce(([{ target, contentRect }]: ResizeObserverEntry[]) => {
+      /**
+       * @description сверка высоты дом ноды и высоты скролл контейнера, если скролл больше, то значит компонент переполнен контентом
+       */
       const isScrollHeightBigger = contentRect.height < target.scrollHeight;
+      /**
+       * @description сверка ширины дом ноды и ширины скролл контейнера, если скролл больше, то значит компонент переполнен контентом
+       */
       const isScrollWidthBigger = target.scrollWidth > contentRect.width;
 
       setOverflow(isScrollHeightBigger || isScrollWidthBigger);
