75376549014620856ae54f297cda7d6c42562c0f
Sedykh Ivan <konungivan@gmail.com>
2022-12-09 14:29:05 +0500

    bug(UIKIT-578,ui,OverflowTypography): вынесение значений в переменные

--- packages/ui/src/OverflowTypography/hooks/useOverflowed/useOverflowed.ts
+++ packages/ui/src/OverflowTypography/hooks/useOverflowed/useOverflowed.ts
@@ -18,14 +18,12 @@ export const useOverflowed = (forwardedRef?: Ref<HTMLElement>) => {
   const [isOverflowed, setOverflow] = useState(false);
 
   const handleResize = useCallback(
-    debounce(
-      ([{ target, contentRect }]: ResizeObserverEntry[]) =>
-        setOverflow(
-          contentRect.height < target.scrollHeight ||
-            target.scrollWidth > contentRect.width,
-        ),
-      500,
-    ),
+    debounce(([{ target, contentRect }]: ResizeObserverEntry[]) => {
+      const isScrollHeightBigger = contentRect.height < target.scrollHeight;
+      const isScrollWidthBigger = target.scrollWidth > contentRect.width;
+
+      setOverflow(isScrollHeightBigger || isScrollWidthBigger);
+    }, 500),
     [],
   );
 
--- packages/ui/src/OverflowTypography/styles.ts
+++ packages/ui/src/OverflowTypography/styles.ts
@@ -3,18 +3,25 @@ import { Typography } from '../Typography';
 
 import { OverflowedProps } from './OverflowTypography';
 
+type OverflowTypographyWrapperProps = {
+  hasMultipleRows: boolean;
+} & Required<OverflowedProps>;
+
 // проверки на rowsCount в стилях нужны для того, чтобы элементы в одну строку обрезались бы по буквенно
 // но если нужно обрезание по нескольким строчкам, тогда элементы будут обрезаться по словам
 export const OverflowTypographyWrapper = styled(Typography, {
-  shouldForwardProp: (name) => name !== 'rowsCount',
-})<Required<OverflowedProps>>`
+  shouldForwardProp: (name) =>
+    name !== 'rowsCount' && name !== 'hasMultipleRows',
+})<OverflowTypographyWrapperProps>`
   /* stylelint-disable-next-line */
-  display: ${({ rowsCount }) => (rowsCount > 1 ? '-webkit-box' : '')};
+  display: ${({ hasMultipleRows }) => (hasMultipleRows ? '-webkit-box' : '')};
   max-width: 100%;
   overflow: hidden;
 
-  white-space: ${({ rowsCount }) => (rowsCount > 1 ? 'initial' : 'nowrap')};
+  white-space: ${({ hasMultipleRows }) =>
+    hasMultipleRows ? 'initial' : 'nowrap'};
   text-overflow: ellipsis;
   -webkit-line-clamp: ${({ rowsCount }) => rowsCount};
-  -webkit-box-orient: ${({ rowsCount }) => (rowsCount > 1 ? 'vertical' : '')};
+  -webkit-box-orient: ${({ hasMultipleRows }) =>
+    hasMultipleRows ? 'vertical' : ''};
 `;
