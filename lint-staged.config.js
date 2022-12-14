a4d2f2d21ac7ea1a3c76be9997bc2c3cbd9e85fe
Andrey Potyomkin <and_tem@icloud.com>
2022-12-14 17:52:18 +0300

    chore(UIKIT-431,ui): Пакет добавлен в lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  'packages/ui/**/*.{js,jsx,ts,tsx}': [
+    'npm run lint --workspace=@astral/ui',
+    'npm run lint:styles --workspace=@astral/ui',
+    () => 'npm run lint:types --workspace=@astral/ui',
+  ],
   'packages/components/**/*.{js,jsx,ts,tsx}': [
     'npm run lint --workspace=@astral/components',
     'npm run lint:styles --workspace=@astral/components',
