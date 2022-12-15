d852743d34cb4032da3264b37d3091ecb97b7944
Ivan Sedykh <59384103+Ofigelov@users.noreply.github.com>
2022-12-15 18:09:30 +0500

    bug(UIKIT-585,ui,DatePicker): фикс неправильной проверки на выбранный день (#505)

--- packages/ui/src/utils/date/isDate/isDate.ts
+++ packages/ui/src/utils/date/isDate/isDate.ts
@@ -5,4 +5,5 @@ type Unknown = null | undefined | string | number | Date;
 /**
  * @description функция проверки значения на дату
  */
-export const isDate = (value: Unknown): value is Date => dayjs(value).isValid();
+export const isDate = (value: Unknown): value is Date =>
+  Boolean(value) && dayjs(value).isValid();
