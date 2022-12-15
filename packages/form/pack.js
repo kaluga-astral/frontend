3a27a327badc85ef747b74b7b020fd5cf0858388
Andrey Potyomkin <and_tem@icloud.com>
2022-12-15 20:38:16 +0300

    chore(UIKIT-431,form): Исправлен билд пакета

--- packages/form/pack.js
+++ packages/form/pack.js
@@ -1,6 +1,12 @@
-const { buildTs, rmDist } = require('@astral/commander');
+const { buildTs, rmDist, copy } = require('@astral/commander');
 
 rmDist();
+
+copy({
+  sourcesDirPath: './src',
+  files: ['declarations.d.ts'],
+  targetPath: '.',
+});
 
 buildTs({
   releaseTag: process.env.RELEASE_TAG,
