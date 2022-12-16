f7205c95230551eff55776725b7a7a4f4c7711d7
Andrey Sarantsev <andrrr96@yandex.ru>
2022-12-16 15:08:31 +1000

    feat(UIKIT-495,validations,isEmail): Добавлен composeSome для валидаторов

---
+++ packages/validations/src/composeSome/composeSome.ts
@@ -0,0 +1,38 @@
+import { InitializedRule, ValidationResult } from '../types';
+
+/**
+ * @description Объединяет переданные правила в цепочку правил, выполняя все переданные правила, независимо от результата выполнения правил.
+ * Выполняет правила слева направо. Возвращает первую ошибку, если все правила выполнены с ошибкой
+ * @example composeSome(isIncludeDot(), isIncludeComma());
+ * @example compose(isRequired(), composeSome(isIncludeDot(), isIncludeComma()));
+ */
+export const composeSome =
+  (...rules: InitializedRule[]) =>
+  (value: unknown): ValidationResult => {
+    const errors: ValidationResult = rules.reduce<string[]>(
+      (errorsAcc, rule) => {
+        const error = rule(value);
+
+        if (Array.isArray(error) && error.length > 0) {
+          errorsAcc.push(...error);
+        }
+
+        if (typeof error === 'string') {
+          errorsAcc.push(error);
+        }
+
+        return errorsAcc;
+      },
+      [],
+    );
+
+    if (!errors.length) {
+      return undefined;
+    }
+
+    if (errors.length < rules.length) {
+      return undefined;
+    }
+
+    return errors[0];
+  };
