91a604323c64e23e7093c7786be9d5dfb90e5ca3
StanislavDa <flugegeheimenbolsen@gmail.com>
2023-02-15 19:51:43 +0500

    bug(UIKIT-656,ui,DataGridPagination): Поправил тесты

--- packages/components/src/DataGridPagination/DataGridPagination.test.tsx
+++ packages/components/src/DataGridPagination/DataGridPagination.test.tsx
@@ -2,52 +2,26 @@ import { renderWithTheme, screen } from '@astral/tests';
 
 import { DataGridPagination } from './DataGridPagination';
 
+const TEST_DATA = [
+  { page: 4, totalCount: 31, expected: '31 — 31 из 31 записей' },
+  { page: 3, totalCount: 24, expected: '21 — 24 из 24 записей' },
+  { page: 2, totalCount: 20, expected: '11 — 20 из 20 записей' },
+  { page: 2, totalCount: 19, expected: '11 — 19 из 19 записей' },
+  { page: 1, totalCount: 10, expected: '1 — 10 из 10 записей' },
+  { page: 1, totalCount: 5, expected: '1 — 5 из 5 записей' },
+];
+
 describe('DataGridPagination', () => {
-  it('page=4, totalCount=31', () => {
-    renderWithTheme(<DataGridPagination totalCount={31} page={4} />);
+  test.each(TEST_DATA)(
+    'Props:page=$page:totalCount=$totalCount: Текст пагинации "$expected"',
+    ({ totalCount, page, expected }) => {
+      renderWithTheme(
+        <DataGridPagination totalCount={totalCount} page={page} />,
+      );
 
-    const text = screen.getByText('31 — 31 из 31 записей');
+      const text = screen.getByText(expected);
 
-    expect(text).toBeInTheDocument();
-  });
-
-  it('page=3, totalCount=24', () => {
-    renderWithTheme(<DataGridPagination totalCount={24} page={3} />);
-
-    const text = screen.getByText('21 — 24 из 24 записей');
-
-    expect(text).toBeInTheDocument();
-  });
-
-  it('page=2, totalCount=20', () => {
-    renderWithTheme(<DataGridPagination totalCount={20} page={2} />);
-
-    const text = screen.getByText('11 — 20 из 20 записей');
-
-    expect(text).toBeInTheDocument();
-  });
-
-  it('page=2, totalCount=19', () => {
-    renderWithTheme(<DataGridPagination totalCount={19} page={2} />);
-
-    const text = screen.getByText('11 — 19 из 19 записей');
-
-    expect(text).toBeInTheDocument();
-  });
-
-  it('page=1, totalCount=10', () => {
-    renderWithTheme(<DataGridPagination totalCount={10} page={1} />);
-
-    const text = screen.getByText('1 — 10 из 10 записей');
-
-    expect(text).toBeInTheDocument();
-  });
-
-  it('page=1, totalCount=5', () => {
-    renderWithTheme(<DataGridPagination totalCount={5} page={1} />);
-
-    const text = screen.getByText('1 — 5 из 5 записей');
-
-    expect(text).toBeInTheDocument();
-  });
+      expect(text).toBeInTheDocument();
+    },
+  );
 });
