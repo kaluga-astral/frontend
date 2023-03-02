b16aa4da3abccc0eb133708b9adf5999f891c022
Stanislav Dayanov <93906455+StanislavDa@users.noreply.github.com>
2023-03-02 15:58:00 +0500

    bug(UIKIT-677,ui,SideDialog): Исправлена стилизация (#559)

--- packages/components/src/SideDialog/styles.tsx
+++ packages/components/src/SideDialog/styles.tsx
@@ -4,8 +4,9 @@ import { styled } from '../styles';
 import { WithoutEmotionSpecific } from '../types';
 
 export const StyledDrawer = styled(Drawer)<WithoutEmotionSpecific<DrawerProps>>`
-  .MuiPaper-root {
+  .MuiDrawer-paper {
     width: 30%;
   }
+
   height: 100vh;
 `;
