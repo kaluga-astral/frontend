143f3e90b74a2da9cbcd5144da516bc245e43036
Andrey Potyomkin <and_tem@icloud.com>
2023-03-03 10:49:57 +0300

    wip(UIKIT-672): Основа пакета features

--- packages/features/global.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="@emotion/react/types/css-prop" />
-
-declare module '*.svg' {
-  const image: string;
-
-  export default image;
-}
--- packages/features/pack.js
+++ packages/features/pack.js
@@ -1,14 +1,13 @@
-const { buildTs, copy, rmDist } = require('@astral/commander');
+const { buildTs, rmDist, copy } = require('@astral/commander');
 
 rmDist();
 
 copy({
-  sourcesDirPath: './src/declaration',
+  sourcesDirPath: './src',
+  files: ['declarations.d.ts'],
+  targetPath: '.',
 });
 
 buildTs({
   releaseTag: process.env.RELEASE_TAG,
-  packageExports: {
-    './server': './server/index.js',
-  },
 });
