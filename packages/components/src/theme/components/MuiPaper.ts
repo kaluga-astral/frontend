fad65f132d2c053ea993579ba8bf29681b311cdf
Лебаков Кирилл <bblchock.p.izdat@gmail.com>
2023-05-08 19:11:15 +0300

    feat(UIKIT-630,ui,Paper): Добавил jsdoc функции

--- packages/components/src/theme/components/MuiPaper.ts
+++ packages/components/src/theme/components/MuiPaper.ts
@@ -2,6 +2,14 @@ import { Components } from '@mui/material';
 
 import type { Theme } from '../baseTheme';
 
+/**
+ * @description Подставить нужную тень
+ * @param {number|undefined} elevation - Значение пропса "elevation"
+ * @param {Theme} theme - Объект темы
+ * @return {string} Вернёт значение "box-shadow" из "theme"
+ * @example
+ * getShadow( 2, theme)
+ */
 const getShadow = (elevation: number | undefined, theme: Theme): string => {
   if (elevation === 2) {
     return theme.elevation['200'];
