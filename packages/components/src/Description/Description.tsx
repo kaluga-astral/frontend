8104493388ad7c0d16d6c1e5ba017d5424842a9f
Кулешов Олег Игоревич <oleg-kaluga2001@mail.ru>
2023-05-16 15:12:29 +0300

    bug(UIKIT-628,ui,Description): Добавлены пропсы justifyContent и leader

--- packages/components/src/Description/Description.tsx
+++ packages/components/src/Description/Description.tsx
@@ -3,16 +3,37 @@ import { ReactNode } from 'react';
 import { Name } from './Name';
 import { Value } from './Value';
 import { DescriptionWrapper } from './styles';
+import { DescriptionProvider } from './DescriptionProvider';
 
 export type DescriptionProps = {
   /**
    * Элементы Description.Name и Description.Value
    */
   children: ReactNode;
+
+  /**
+   * пропс justifyContent
+   */
+  justifyContent?: 'space-between' | 'start';
+
+  /**
+   * флаг отвечающий за заполнение свободного пространства между Name Value
+   */
+  leader?: boolean;
 };
 
-export const Description = ({ children }: DescriptionProps) => {
-  return <DescriptionWrapper>{children}</DescriptionWrapper>;
+export const Description = ({
+  children,
+  justifyContent,
+  leader = false,
+}: DescriptionProps) => {
+  return (
+    <DescriptionProvider leader={leader}>
+      <DescriptionWrapper justifyContent={justifyContent}>
+        {children}
+      </DescriptionWrapper>
+    </DescriptionProvider>
+  );
 };
 
 Description.Name = Name;
--- packages/components/src/Description/styles.ts
+++ packages/components/src/Description/styles.ts
@@ -1,10 +1,13 @@
 import { styled } from '../styles';
 
-export const DescriptionWrapper = styled.div`
+export const DescriptionWrapper = styled.div<{
+  justifyContent?: 'space-between' | 'start';
+}>`
   display: flex;
   flex-wrap: wrap;
   align-items: baseline;
-
+  justify-content: ${({ justifyContent }) =>
+    justifyContent ? justifyContent : 'start'};
   ${({ theme }) => theme.breakpoints.down('sm')} {
     flex-direction: column;
 
@@ -12,4 +15,12 @@ export const DescriptionWrapper = styled.div`
       font-size: ${({ theme }) => theme.typography.fontSize};
     }
   }
+`;
+
+export const DashedSeparator = styled.div`
+  flex: 1;
+  height: 4px;
+  margin-right: ${({ theme }) => theme.spacing(1)};
+
+  border-bottom: 1px dashed ${({ theme }) => theme.palette.grey[400]};
 `;
