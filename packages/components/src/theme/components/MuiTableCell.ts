69a05415baa2d8397143b05d101021973d492425
StanislavDa <flugegeheimenbolsen@gmail.com>
2023-05-17 14:46:09 +0500

    feat(UIKIT-758,ui,DataGrid): Оставил комментарий

--- packages/components/src/theme/components/MuiTableCell.ts
+++ packages/components/src/theme/components/MuiTableCell.ts
@@ -8,7 +8,7 @@ export const MuiTableCell: Components<Theme>['MuiTableCell'] = {
       return {
         border: 'none',
         padding: theme.spacing(1, 4),
-        // 44px - высота, установленная в дизайн-системе
+        // 44px - минимальная высота ячейки, установленная в дизайн-системе
         height: 44,
       };
     },
