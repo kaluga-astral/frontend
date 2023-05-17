3aaa0bee75b7c722130c8a6afe039a8eab266d0a
StanislavDa <flugegeheimenbolsen@gmail.com>
2023-05-17 14:09:51 +0500

    feat(UIKIT-758,ui,DataGrid): Ячейки приведены к единой начальной высоте 44px

--- packages/components/src/theme/components/MuiTableCell.ts
+++ packages/components/src/theme/components/MuiTableCell.ts
@@ -4,9 +4,11 @@ import type { Theme } from '../baseTheme';
 
 export const MuiTableCell: Components<Theme>['MuiTableCell'] = {
   styleOverrides: {
-    root() {
+    root({ theme }) {
       return {
         border: 'none',
+        padding: theme.spacing(1, 4),
+        height: 44,
       };
     },
   },
