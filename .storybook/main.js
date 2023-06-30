b11bd6d7505f02971f56985c3ef6f09a0874df5b
Andrey Potyomkin <and_tem@icloud.com>
2023-06-30 16:39:47 +0300

    docs: Обновлен storybook, реализован mdx для Button

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,16 @@
 module.exports = {
-  stories: [
-    '../packages/**/*.stories.mdx',
-    '../packages/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  stories: ['../packages/**/*.mdx', '../packages/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['storybook-addon-swc', '@storybook/addon-links', '@storybook/addon-essentials'],
   features: {
     // https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#emotion11-quasi-compatibility
     // у storybook в 6 версии есть проблемы с 11 emotion - не работает темиация
-    emotionAlias: false,
+    emotionAlias: false
   },
-  core: {
-    builder: "webpack5"
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
   },
-}
+  docs: {
+    autodocs: true
+  }
+};
--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,33 +1,26 @@
-// emotion-theming нужен для: https://github.com/storybookjs/storybook/issues/10231#issuecomment-613394048
-
-import { ThemeProvider } from '../packages/components/src'
+import { ThemeProvider, Grid } from '../packages/components/src'
 import { getTheme, themes } from './themes'
 
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  options: {
-    storySort: (a, b) =>
-      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true })
-  }
-}
-
-export const decorators = [
-  (Story, context) => (
-    <ThemeProvider theme={getTheme(context.globals.theme)}>
-      <Story {...context} />
-    </ThemeProvider>
-  )
-]
-
-export const globalTypes = {
-  theme: {
-    name: 'Theme',
-    description: 'Global theme for components',
-    defaultValue: themes[0].name,
-    toolbar: {
-      icon: 'circlehollow',
-      items: themes.map((theme) => theme.name),
-      showName: true
+export default {
+  decorators: [
+    (Story, context) => (
+      <ThemeProvider theme={getTheme(context.globals.theme)}>
+        <Grid container justifyContent="center" alignItems="center" autoFlow="column" spacing={4}>
+          <Story {...context} />
+        </Grid>
+      </ThemeProvider>
+    )
+  ],
+  globalTypes: {
+    theme: {
+      name: 'Theme',
+      description: 'Global theme for components',
+      defaultValue: themes[0].name,
+      toolbar: {
+        icon: 'circlehollow',
+        items: themes.map((theme) => theme.name),
+        showName: true
+      }
     }
-  }
-}
+  },
+};
