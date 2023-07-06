86aa76f9a67d76639f5009eeb0a6d828a292d589
MaksFett <138494717+MaksFett@users.noreply.github.com>
2023-07-06 14:35:11 +0300

    feat: Реализован storybook для компонента Tabs в соответствии с макетом

--- packages/components/src/Tabs/Tabs.stories.tsx
+++ packages/components/src/Tabs/Tabs.stories.tsx
@@ -1,37 +1,89 @@
-import Stack from '@mui/material/Stack';
 import { Story } from '@storybook/react';
 import { SyntheticEvent, useState } from 'react';
 
 import { Typography } from '../Typography';
+import { Paper } from '../Paper';
+import { Box } from '@mui/material';
 import { Tab } from '../Tab';
-
 import { Tabs } from './Tabs';
+import { ExampleTemplate } from '../docs';
 
 export default {
   title: 'Components/Tabs',
   component: Tabs,
 };
 
-export const TabsShowcase: Story = () => {
-  const [value, setValue] = useState(0);
+interface TabPanelProps {
+  children: JSX.Element | JSX.Element[];
+  value: number;
+  index: number;
+}
 
-  const handleChange = (_event: SyntheticEvent, newValue: number) => {
-    setValue(newValue);
+function CustomTabPanel({children, value, index} : TabPanelProps) {
+  return (<div> {(value==index) && children} </div>)
+};
+
+export const TabsShowcase: Story = () => {
+  const [value1, setValue1] = useState(0);
+  const [value2, setValue2] = useState(0);
+
+  const handleChange1 = (_event: SyntheticEvent, newValue: number) => {
+    setValue1(newValue);
+  };
+
+  const handleChange2 = (_event: SyntheticEvent, newValue: number) => {
+    setValue2(newValue);
   };
 
   return (
-    <Stack gap={4}>
-      <Stack gap={4}>
-        <Typography variant="h3">Tab</Typography>
-        <Stack gap={1}>
-          <Tabs value={value} onChange={handleChange}>
-            <Tab label="Tab" />
-            <Tab label="Tab" />
-            <Tab label="Tab" disabled />
+    <ExampleTemplate>
+      <Typography variant="h3">Tabs</Typography>
+      <br />
+      <br />
+      <Typography style={{ width: 772 }} color="grey" colorIntensity="800" paragraph>
+        Вкладки упрощают просмотр и переключение между разделами в рамках одной страницы. 
+      </Typography>
+      <Typography style={{ width: 772 }} color="grey" colorIntensity="800" paragraph>
+        Данный компонент может быть использован, когда необходимо организовать и обеспечить навигацию между 
+        связанными между собой группами контента, которые находятся на одном уровне иерархии страницы. 
+      </Typography>
+      <br />
+      <ExampleTemplate.Case title="Отображение компонента" 
+      descriptionList={[
+        'Tabs',
+        'Данный компонент используется в интерфейсе для переключения между разными группами контента в рамках одной страницы.',
+       ]}
+       fullWidth={true}>
+        <Tabs style={{margin: '0px 32px'}} value={value1} onChange={handleChange1} centered>
+          <Tab label="Вкладка 1" />
+          <Tab label="Вкладка 2" />
+          <Tab label="Вкладка 3" />
+          <Tab label="Вкладка 4" />
+          <Tab label="Вкладка 5" />
+          <Tab label="Вкладка 6" disabled />
+        </Tabs>
+      </ExampleTemplate.Case>
+      <br />
+      <br />
+      <Paper style={{ width: 468 }}>
+        <Box padding='24px 24px'>
+          <Typography variant="h4">Заголовок окна</Typography>
+          <Tabs value={value2} onChange={handleChange2}>
+            <Tab label="Вкладка 1" />
+            <Tab label="Вкладка 2" />
           </Tabs>
-        </Stack>
-      </Stack>
-    </Stack>
+          <br />
+          <CustomTabPanel value={value2} index={0}>
+            <span>Заглушка примера текста страницы, который несёт очень важный смысл для пользователя 
+              и предлагает ему варианты выбора действий с контентом и в рамках работы приложения.</span>
+          </CustomTabPanel>
+          <CustomTabPanel value={value2} index={1}>
+            <span>Заглушка примера текста страницы, который тоже несёт очень важный смысл для пользователя 
+              и предлагает ему варианты выбора действий с контентом и в рамках работы приложения.</span>
+          </CustomTabPanel>
+        </Box>
+      </Paper>
+    </ExampleTemplate>
   );
 };
 
