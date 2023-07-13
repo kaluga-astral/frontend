cab9e91eaa015354d3014057b8c993254893417d
MaksFett <138494717+MaksFett@users.noreply.github.com>
2023-07-13 15:22:11 +0300

    Update Tabs.stories.tsx

--- packages/components/src/Tabs/Tabs.stories.tsx
+++ packages/components/src/Tabs/Tabs.stories.tsx
@@ -2,10 +2,10 @@ import { Story } from '@storybook/react';
 import { SyntheticEvent, useState } from 'react';
 
 import { Typography } from '../Typography';
-import { Paper } from '../Paper';
 import { Box } from '@mui/material';
 import { Tab } from '../Tab';
 import { Tabs } from './Tabs';
+import { TabsShowcaseTypography1, TabsShowcaseTypography2, TabsShowcaseTabs1, TabsShowcasePaper, TabsShowcaseTabs2} from './styles';
 import { ExampleTemplate } from '../docs';
 
 export default {
@@ -38,35 +38,35 @@ export const TabsShowcase: Story = () => {
   return (
     <ExampleTemplate>
       <Typography variant="h3">Tabs</Typography>
-      <Typography style={{ width: 772, margin: '60px 0px 16px' }} color="grey" colorIntensity="800" paragraph>
+      <TabsShowcaseTypography1 color="grey" colorIntensity="800" paragraph>
         Вкладки упрощают просмотр и переключение между разделами в рамках одной страницы. 
-      </Typography>
-      <Typography style={{ width: 772, margin: '0px 0px 60px' }} color="grey" colorIntensity="800" paragraph>
+      </TabsShowcaseTypography1>
+      <TabsShowcaseTypography2 color="grey" colorIntensity="800" paragraph>
         Данный компонент может быть использован, когда необходимо организовать и обеспечить навигацию между 
         связанными между собой группами контента, которые находятся на одном уровне иерархии страницы. 
-      </Typography>
+      </TabsShowcaseTypography2>
       <ExampleTemplate.Case title="Отображение компонента" 
       descriptionList={[
         'Tabs',
         'Данный компонент используется в интерфейсе для переключения между разными группами контента в рамках одной страницы.',
        ]}
        fullWidth={true}>
-        <Tabs style={{margin: '0px 32px'}} value={value1} onChange={handleChange1} centered>
+        <TabsShowcaseTabs1 value={value1} onChange={handleChange1} centered>
           <Tab label="Вкладка 1" />
           <Tab label="Вкладка 2" />
           <Tab label="Вкладка 3" />
           <Tab label="Вкладка 4" />
           <Tab label="Вкладка 5" />
           <Tab label="Вкладка 6" disabled />
-        </Tabs>
+        </TabsShowcaseTabs1>
       </ExampleTemplate.Case>
-      <Paper style={{ width: 468, margin: '60px 0px 0px' }}>
+      <TabsShowcasePaper>
         <Box padding='24px 24px'>
           <Typography variant="h4">Заголовок окна</Typography>
-          <Tabs value={value2} onChange={handleChange2} style={{margin: '0px 0px 16px'}}>
+          <TabsShowcaseTabs2 value={value2} onChange={handleChange2}>
             <Tab label="Вкладка 1" />
             <Tab label="Вкладка 2" />
-          </Tabs>
+          </TabsShowcaseTabs2>
           <CustomTabPanel value={value2} index={0}>
             <span>Заглушка примера текста страницы, который несёт очень важный смысл для пользователя 
               и предлагает ему варианты выбора действий с контентом и в рамках работы приложения.</span>
@@ -76,7 +76,7 @@ export const TabsShowcase: Story = () => {
               и предлагает ему варианты выбора действий с контентом и в рамках работы приложения.</span>
           </CustomTabPanel>
         </Box>
-      </Paper>
+      </TabsShowcasePaper>
     </ExampleTemplate>
   );
 };
