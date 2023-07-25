a0b1f8c3ab3bbd04bf002e7624d073572616d66e
ystudenikin <studenikin.jarik@gmail.com>
2023-07-25 11:42:04 +0300

    feat(UIKIT-764,ui,CheckboxField): Добавлен компонент CheckboxField

---
+++ packages/components/src/CheckboxField/CheckboxField.tsx
@@ -0,0 +1,63 @@
+import { ReactNode, useMemo } from 'react';
+
+import { Checkbox, CheckboxProps } from '../Checkbox';
+import { FormControl } from '../FormControl';
+import { FormControlLabel } from '../FormControlLabel';
+import { FormHelperText } from '../FormHelperText';
+import { Tooltip } from '../Tooltip';
+
+export type CheckboxFieldProps = CheckboxProps & {
+  /**
+   * Флаг отображения успешного состояния текстовой ошибки (helperText)
+   */
+  success?: boolean;
+  /**
+   * Текст-подсказка
+   */
+  helperText?: ReactNode;
+  /**
+   * Флаг принудительного скрытия блока helperText
+   */
+  hideHelperText?: boolean;
+  /**
+   * Текст или элемент около чекбокса (обычно краткое описание назначения чекбокса)
+   */
+  label: ReactNode;
+};
+
+/**
+ * @description Составной компонент чекбокса. Содержит label, tooltip, helperText и т.д.
+ */
+export const CheckboxField = ({
+  success,
+  helperText,
+  hideHelperText = false,
+  label,
+  isError,
+  ...restProps
+}: CheckboxFieldProps) => {
+  const showTooltip = useMemo(
+    () => Boolean(isError && hideHelperText),
+    [isError, hideHelperText],
+  );
+
+  return (
+    <FormControl>
+      <Tooltip
+        title={showTooltip && helperText}
+        placement={'bottom-start'}
+        withoutContainer={false}
+      >
+        <FormControlLabel
+          control={<Checkbox isError={isError} {...restProps} />}
+          label={label}
+        />
+      </Tooltip>
+      {!hideHelperText && (
+        <FormHelperText error={isError} success={success}>
+          {helperText}
+        </FormHelperText>
+      )}
+    </FormControl>
+  );
+};
