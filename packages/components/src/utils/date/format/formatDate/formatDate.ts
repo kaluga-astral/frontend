c7f912ac9cc5c45c002d86b90d3163479caee6ee
Andrey Potyomkin <and_tem@icloud.com>
2023-07-31 09:48:47 +0300

    Revert "Revert "Revert "refactor: Обновлен ts и storybook"""
    This reverts commit 19f3a148b46572b0ea4a24270b5e34c1de5cc358.

--- packages/components/src/utils/date/format/formatDate/formatDate.ts
+++ packages/components/src/utils/date/format/formatDate/formatDate.ts
@@ -22,7 +22,7 @@ export const formatDate = (
 ): string =>
   mask
     .split(separator)
-    .map((element) => elementsMap[element as DateMaskElements]?.(date))
+    .map((element) => elementsMap[element]?.(date))
     .filter(Boolean)
     .map((value) => zeroPad(value, 2))
     .join(separator);
