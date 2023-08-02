4b78ee9c92c6b89e4abe643ec0fb1a883893fc71
Andrey Potyomkin <and_tem@icloud.com>
2023-08-02 13:08:52 +0300

    doc: Обновлены конфиги storybook

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,25 +1,38 @@
-// emotion-theming нужен для: https://github.com/storybookjs/storybook/issues/10231#issuecomment-613394048
-
-import { ThemeProvider } from '../packages/components/src'
+import { ThemeProvider, styled } from '../packages/components/src'
 import { getTheme, themes } from './themes'
 
-export const decorators = [
-  (Story, context) => (
-    <ThemeProvider theme={getTheme(context.globals.theme)}>
-      <Story {...context} />
-    </ThemeProvider>
-  )
-]
-
-export const globalTypes = {
-  theme: {
-    name: 'Theme',
-    description: 'Global theme for components',
-    defaultValue: themes[0].name,
-    toolbar: {
-      icon: 'circlehollow',
-      items: themes.map((theme) => theme.name),
-      showName: true
-    }
+const StoryWrapper = styled.article`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: ${({ theme }) => theme.spacing(3)};
+  
+  ${({theme}) => theme.breakpoints.down('sm')} {
+    flex-direction: column;
   }
-}
+`;
+
+export default {
+  decorators: [
+    (Story, context) => (
+      <ThemeProvider theme={getTheme(context.globals.theme)}>
+        <StoryWrapper>
+          <Story {...context} />
+        </StoryWrapper>
+      </ThemeProvider>
+    )
+  ],
+  globalTypes: {
+    theme: {
+      name: 'Theme',
+      description: 'Global theme for components',
+      defaultValue: themes[0].name,
+      toolbar: {
+        icon: 'circlehollow',
+        items: themes.map((theme) => theme.name),
+        showName: true
+      }
+    }
+  },
+};
