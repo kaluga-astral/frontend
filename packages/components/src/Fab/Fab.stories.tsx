fdca9289e39c228108c0327acb6e021faff175ff
Igor <igorbabikov91@gmail.com>
2023-08-22 16:59:28 +0300

    doc(UIKIT-794,ui,Fab): Реализован storybook

--- packages/components/src/Fab/Fab.stories.tsx
+++ packages/components/src/Fab/Fab.stories.tsx
@@ -1,184 +1,102 @@
-import { Story } from '@storybook/react';
-import { Stack } from '@mui/material';
 import { LikeOutlineMd } from '@astral/icons';
+import { Stack } from '@mui/material';
+import { Meta, StoryObj } from '@storybook/react';
 
 import { Typography } from '../Typography';
 
 import { Fab } from './Fab';
 
-export default {
+const meta: Meta<typeof Fab> = {
   title: 'Components/Fab',
   component: Fab,
 };
 
 const svg = <LikeOutlineMd />;
 
-export const FabShowcase: Story = () => (
-  <Stack gap={4}>
-    <Stack gap={4}>
-      <Typography variant="h3">Default Type (Square)</Typography>
-      <Stack gap={1}>
-        <Typography variant="h4">Small </Typography>
-        <Stack gap={3} direction="row" alignItems="center">
-          <Fab size="small" color="default">
-            {svg}
-          </Fab>
-          <Fab size="small" color="primary">
-            {svg}
-          </Fab>
-          <Fab size="small" color="success">
-            {svg}
-          </Fab>
-          <Fab size="small" color="warning">
-            {svg}
-          </Fab>
-          <Fab size="small" color="error">
-            {svg}
-          </Fab>
-          <Fab size="small" color="error" disabled>
-            {svg}
-          </Fab>
-        </Stack>
-      </Stack>
-      <Stack gap={1}>
-        <Typography variant="h4">Medium </Typography>
-        <Stack gap={3} direction="row" alignItems="center">
-          <Fab size="medium" color="default">
-            {svg}
-          </Fab>
-          <Fab size="medium" color="primary">
-            {svg}
-          </Fab>
-          <Fab size="medium" color="success">
-            {svg}
-          </Fab>
-          <Fab size="medium" color="warning">
-            {svg}
-          </Fab>
-          <Fab size="medium" color="error">
-            {svg}
-          </Fab>
-          <Fab size="medium" color="error" disabled>
-            {svg}
-          </Fab>
-        </Stack>
-      </Stack>
-      <Stack gap={1}>
-        <Typography variant="h4">Large </Typography>
-        <Stack gap={3} direction="row" alignItems="center">
-          <Fab size="large" color="default">
-            {svg}
-          </Fab>
-          <Fab size="large" color="primary">
-            {svg}
-          </Fab>
-          <Fab size="large" color="success">
-            {svg}
-          </Fab>
-          <Fab size="large" color="warning">
-            {svg}
-          </Fab>
-          <Fab size="large" color="error">
-            {svg}
-          </Fab>
-          <Fab size="large" color="error" disabled>
-            {svg}
-          </Fab>
-        </Stack>
-      </Stack>
-    </Stack>
-    <Stack gap={4}>
-      <Typography variant="h3">Сircular Type</Typography>
-      <Stack gap={1}>
-        <Typography variant="h4">Small </Typography>
-        <Stack gap={3} direction="row" alignItems="center">
-          <Fab variant="circular" size="small" color="default">
-            {svg}
-          </Fab>
-          <Fab variant="circular" size="small" color="primary">
-            {svg}
-          </Fab>
-          <Fab variant="circular" size="small" color="success">
-            {svg}
-          </Fab>
-          <Fab variant="circular" size="small" color="warning">
-            {svg}
-          </Fab>
-          <Fab variant="circular" size="small" color="error">
-            {svg}
-          </Fab>
-          <Fab variant="circular" size="small" color="error" disabled>
-            {svg}
-          </Fab>
-        </Stack>
-      </Stack>
-      <Stack gap={1}>
-        <Typography variant="h4">Medium </Typography>
-        <Stack gap={3} direction="row" alignItems="center">
-          <Fab variant="circular" size="medium" color="default">
-            {svg}
-          </Fab>
-          <Fab variant="circular" size="medium" color="primary">
-            {svg}
-          </Fab>
+export default meta;
 
-          <Fab variant="circular" size="medium" color="success">
-            {svg}
-          </Fab>
-          <Fab variant="circular" size="medium" color="warning">
-            {svg}
-          </Fab>
-          <Fab variant="circular" size="medium" color="error">
-            {svg}
-          </Fab>
-          <Fab variant="circular" size="medium" color="error" disabled>
-            {svg}
-          </Fab>
-        </Stack>
-      </Stack>
-      <Stack gap={1}>
-        <Typography variant="h4">Large </Typography>
-        <Stack gap={3} direction="row" alignItems="center">
-          <Fab variant="circular" size="large" color="default">
-            {svg}
-          </Fab>
-          <Fab variant="circular" size="large" color="primary">
-            {svg}
-          </Fab>
-          <Fab variant="circular" size="large" color="success">
-            {svg}
-          </Fab>
-          <Fab variant="circular" size="large" color="warning">
-            {svg}
-          </Fab>
-          <Fab variant="circular" size="large" color="error">
-            {svg}
-          </Fab>
-          <Fab variant="circular" size="large" color="error" disabled>
-            {svg}
-          </Fab>
-        </Stack>
+type Story = StoryObj<typeof Fab>;
+
+export const Interaction: Story = {
+  args: {
+    children: svg,
+  },
+  parameters: {
+    docs: {
+      disable: true,
+    },
+  },
+};
+
+export const Example = () => (
+  <Fab size="small" color="default">
+    {svg}
+  </Fab>
+);
+
+export const Sizes = () => (
+  <>
+    <Stack gap={4}>
+      <Typography variant="h5">Small</Typography>
+      <Stack gap={3} direction="row" alignItems="center">
+        <Fab size="small" color="default">
+          {svg}
+        </Fab>
       </Stack>
     </Stack>
+
+    <Stack gap={4}>
+      <Typography variant="h5">Medium</Typography>
+      <Stack gap={3} direction="row" alignItems="center">
+        <Fab size="medium" color="default">
+          {svg}
+        </Fab>
+      </Stack>
+    </Stack>
+
+    <Stack gap={4}>
+      <Typography variant="h5">Large</Typography>
+      <Stack gap={3} direction="row" alignItems="center">
+        <Fab size="large" color="default">
+          {svg}
+        </Fab>
+      </Stack>
+    </Stack>
+  </>
+);
+
+export const Colors = () => (
+  <Stack gap={4} direction="row" alignItems="center">
+    <Fab size="small" color="default">
+      {svg}
+    </Fab>
+
+    <Fab size="small" color="primary">
+      {svg}
+    </Fab>
+
+    <Fab size="small" color="success">
+      {svg}
+    </Fab>
+
+    <Fab size="small" color="warning">
+      {svg}
+    </Fab>
+
+    <Fab size="small" color="error">
+      {svg}
+    </Fab>
   </Stack>
 );
 
-FabShowcase.parameters = { options: { showPanel: false } };
+export const Сircular = () => (
+  <Fab variant="circular" size="small" color="default">
+    {svg}
+  </Fab>
+);
 
-const Template: Story = (args) => <Fab {...args}>{svg}</Fab>;
-
-export const FabStory = Template.bind({});
-
-FabStory.storyName = 'Fab';
-
-FabStory.args = {
-  disabled: false,
-  color: 'primary',
-  variant: 'square',
-  size: 'medium',
-};
-
-FabStory.parameters = {
-  options: { showPanel: true },
-  controls: { expanded: true },
-};
+export const Disabled = () => (
+  <Fab size="small" color="default" disabled>
+    {svg}
+  </Fab>
+);
