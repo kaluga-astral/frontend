c3b65c5e65c49f8469be3b3096862fa445ffbe91
Igor <igorbabikov91@gmail.com>
2023-08-25 14:48:03 +0300

    doc(UIKIT-794,ui,Fab): Отцентровал элементы

--- packages/components/src/Fab/Fab.stories.tsx
+++ packages/components/src/Fab/Fab.stories.tsx
@@ -1,8 +1,8 @@
 import { LikeOutlineMd } from '@astral/icons';
 import { Meta, StoryObj } from '@storybook/react';
 
-import { Typography } from '../Typography';
 import { Grid } from '../Grid';
+import { Typography } from '../Typography';
 import { styled } from '../styles';
 
 import { Fab } from './Fab';
@@ -11,7 +11,6 @@ import { Fab } from './Fab';
  * ### [Figma]()
  * ### [Guide]()
  */
-
 const meta: Meta<typeof Fab> = {
   title: 'Components/Fab',
   component: Fab,
@@ -42,6 +41,12 @@ const ColorsContainer = styled(Grid)`
   }
 `;
 
+const Item = styled.div`
+  display: grid;
+  justify-items: center;
+  gap: ${({ theme }) => theme.spacing(2)};
+`;
+
 export const Example = () => (
   <Fab size="small" color="default">
     <LikeOutlineMd />
@@ -51,24 +56,30 @@ export const Example = () => (
 export const Sizes = () => (
   <>
     <Grid spacing={2}>
-      <Typography variant="h6">small</Typography>
-      <Fab size="small" color="default">
-        <LikeOutlineMd />
-      </Fab>
+      <Item>
+        <Typography variant="h6">small</Typography>
+        <Fab size="small" color="default">
+          <LikeOutlineMd />
+        </Fab>
+      </Item>
     </Grid>
 
     <Grid spacing={2}>
-      <Typography variant="h6">medium</Typography>
-      <Fab size="medium" color="default">
-        <LikeOutlineMd />
-      </Fab>
+      <Item>
+        <Typography variant="h6">medium</Typography>
+        <Fab size="medium" color="default">
+          <LikeOutlineMd />
+        </Fab>
+      </Item>
     </Grid>
 
     <Grid spacing={2}>
-      <Typography variant="h6">large</Typography>
-      <Fab size="large" color="default">
-        <LikeOutlineMd />
-      </Fab>
+      <Item>
+        <Typography variant="h6">large</Typography>
+        <Fab size="large" color="default">
+          <LikeOutlineMd />
+        </Fab>
+      </Item>
     </Grid>
   </>
 );
@@ -76,38 +87,48 @@ export const Sizes = () => (
 export const Colors = () => (
   <ColorsContainer>
     <Grid spacing={2}>
-      <Typography variant="h6">default</Typography>
-      <Fab size="small" color="default">
-        <LikeOutlineMd />
-      </Fab>
+      <Item>
+        <Typography variant="h6">default</Typography>
+        <Fab size="small" color="default">
+          <LikeOutlineMd />
+        </Fab>
+      </Item>
     </Grid>
 
     <Grid spacing={2}>
-      <Typography variant="h6">primary</Typography>
-      <Fab size="small" color="primary">
-        <LikeOutlineMd />
-      </Fab>
+      <Item>
+        <Typography variant="h6">primary</Typography>
+        <Fab size="small" color="primary">
+          <LikeOutlineMd />
+        </Fab>
+      </Item>
     </Grid>
 
     <Grid spacing={2}>
-      <Typography variant="h6">success</Typography>
-      <Fab size="small" color="success">
-        <LikeOutlineMd />
-      </Fab>
+      <Item>
+        <Typography variant="h6">success</Typography>
+        <Fab size="small" color="success">
+          <LikeOutlineMd />
+        </Fab>
+      </Item>
     </Grid>
 
     <Grid spacing={2}>
-      <Typography variant="h6">warning</Typography>
-      <Fab size="small" color="warning">
-        <LikeOutlineMd />
-      </Fab>
+      <Item>
+        <Typography variant="h6">warning</Typography>
+        <Fab size="small" color="warning">
+          <LikeOutlineMd />
+        </Fab>
+      </Item>
     </Grid>
 
     <Grid spacing={2}>
-      <Typography variant="h6">error</Typography>
-      <Fab size="small" color="error">
-        <LikeOutlineMd />
-      </Fab>
+      <Item>
+        <Typography variant="h6">error</Typography>
+        <Fab size="small" color="error">
+          <LikeOutlineMd />
+        </Fab>
+      </Item>
     </Grid>
   </ColorsContainer>
 );
@@ -115,17 +136,21 @@ export const Colors = () => (
 export const Variants = () => (
   <>
     <Grid spacing={2}>
-      <Typography variant="h6">square</Typography>
-      <Fab size="small" color="default">
-        <LikeOutlineMd />
-      </Fab>
+      <Item>
+        <Typography variant="h6">square</Typography>
+        <Fab size="small" color="default">
+          <LikeOutlineMd />
+        </Fab>
+      </Item>
     </Grid>
 
     <Grid spacing={2}>
-      <Typography variant="h6">circular</Typography>
-      <Fab variant="circular" size="small" color="default">
-        <LikeOutlineMd />
-      </Fab>
+      <Item>
+        <Typography variant="h6">circular</Typography>
+        <Fab variant="circular" size="small" color="default">
+          <LikeOutlineMd />
+        </Fab>
+      </Item>
     </Grid>
   </>
 );
