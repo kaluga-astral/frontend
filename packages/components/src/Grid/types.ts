3a58dc32d57ac95f3d3070070bbbca596afc65ff
Sedykh Ivan <konungivan@gmail.com>
2023-09-11 13:16:50 +0500

    feat(UIKIT-837,ui,Grid): Отказ от description в jsdoc

--- packages/components/src/Grid/types.ts
+++ packages/components/src/Grid/types.ts
@@ -2,44 +2,44 @@ import { ElementType, ReactNode } from 'react';
 
 export type GridPropsBase = {
   /**
-   * @description Задаёт display: grid; Если задан columns, spacing и тп, то включается автоматически
+   * Задаёт display: grid; Если задан columns, spacing и тп, то включается автоматически
    * @default false
    */
   container?: boolean;
   /**
-   * @description Количество равных колонок,
+   * Количество равных колонок,
    * либо текстовое значение соответствующее grid-template-columns
    * @default undefined
    */
   columns?: number | string;
   /**
-   * @description Количество равных рядов,
+   * Количество равных рядов,
    * либо текстовое значение соответствующее grid-template-rows
    * @default undefined
    */
   rows?: number | string;
   /**
-   * @description Отступы между колонками
+   * Отступы между колонками
    * @default undefined
    */
   columnSpacing?: number;
   /**
-   * @description Отступы между рядами
+   * Отступы между рядами
    * @default undefined
    */
   rowSpacing?: number;
   /**
-   * @description Отступы между колонками и рядами. Если передан массив: Первый элемент - отступ между рядами, второй - между колонками
+   * Отступы между колонками и рядами. Если передан массив: Первый элемент - отступ между рядами, второй - между колонками
    * @default undefined
    */
   spacing?: number | [number, number];
   /**
-   * @description Соответствует grid-autoflow
+   * Соответствует grid-autoflow
    * @default 	'row'
    */
   direction?: 'row' | 'column' | 'dense';
   /**
-   * @description Тип элемента
+   * Тип элемента
    * @default 	'div'
    */
   component?: ElementType;
