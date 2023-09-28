90ffa2feef9b638cec3d3b5ea67af5c3e44749d5
ayles1 <114210403+ayles1@users.noreply.github.com>
2023-09-28 13:06:03 +0300

    bug(UIKIT-955,icons,InfoFillSm): Исправлена чрезмерная оптимизация (#683)

--- packages/icons/generated-themed-icons/InfoFillSm.tsx
+++ packages/icons/generated-themed-icons/InfoFillSm.tsx
@@ -10,7 +10,7 @@ const InfoFillSm: React.FunctionComponent<SvgIconProps> = ({ ...props }) => (
     fill="currentColor"
     {...props}
   >
-    <path d="M8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12Zm0-7.3a.7.7 0 0 1 .7.6v3.4a.7.7 0 1 1-1.4 0V7.3a.7.7 0 0 1 .7-.6Zm0-2A.7.7 0 1 1 8 6a.7.7 0 0 1 0-1.3Z" />
+    <path d="M8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12zm0-7.333a.667.667 0 0 1 .667.666v3.334a.667.667 0 1 1-1.334 0V7.333A.667.667 0 0 1 8 6.667zm0-2A.667.667 0 1 1 8 6a.667.667 0 0 1 0-1.333z" />
   </SvgIcon>
 );
 
