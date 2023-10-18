291631f30b1847ad2afdbac929a0f607faab9425
StanislavDa <flugegeheimenbolsen@gmail.com>
2023-10-18 18:02:56 +0500

    feat(UIKIT-980,ui,MenuItem): Изменено состояние selected

--- packages/components/src/MenuItem/MenuItem.tsx
+++ packages/components/src/MenuItem/MenuItem.tsx
@@ -2,14 +2,9 @@ import {
   MenuItem as MuiMenuItem,
   MenuItemProps as MuiMenuItemProps,
 } from '@mui/material';
-import { forwardRef } from 'react';
 
 import { WithoutEmotionSpecific } from '../types';
 
 export type MenuItemProps = WithoutEmotionSpecific<MuiMenuItemProps>;
 
-export const MenuItem = forwardRef<HTMLLIElement, MenuItemProps>(
-  (props, ref) => {
-    return <MuiMenuItem ref={ref} {...props} />;
-  },
-);
+export const MenuItem = MuiMenuItem;
