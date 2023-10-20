9ff7883579f0c290c1d315d03b7e203ea06439dd
Sedykh Ivan <konungivan@gmail.com>
2023-10-20 11:29:11 +0500

    feat(UIKIT-799,OverflowTypography): Реализация сторибук

--- packages/components/src/OverflowTypography/OverflowTypography.stories.tsx
+++ packages/components/src/OverflowTypography/OverflowTypography.stories.tsx
@@ -1,46 +1,77 @@
-import { Story } from '@storybook/react';
-import { Stack } from '@mui/material';
+import { Meta, StoryObj } from '@storybook/react';
 
-import { Typography } from '../Typography';
+import { OverflowTypography } from './OverflowTypography';
 
-import {
-  OverflowTypography,
-  OverflowedTypographyProps,
-} from './OverflowTypography';
+/**
+ * ##Мотивация
+ * Вариация ```Typography``` элемента, с автоматической проверкой на переполнение текстового контента, и если есть переполнение, то тогда добавляется кастомизируемая обертка ```Tooltip```.
+ */
 
-export default {
+const meta: Meta<typeof OverflowTypography> = {
   title: 'Components/OverflowTypography',
   component: OverflowTypography,
 };
 
-export const ShowCase: Story = () => {
+export default meta;
+
+type Story = StoryObj<typeof OverflowTypography>;
+
+export const Interaction: Story = {
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque aut delectus dolorem ea, explicabo illo minus nostrum quae quod veniam.',
+  },
+  parameters: {
+    docs: {
+      disable: true,
+    },
+  },
+};
+
+export const Example = () => {
   return (
-    <Stack maxWidth={600} spacing={4}>
-      <Typography variant="h2">Мотивация</Typography>
-      <Typography variant="code">
-        Вариация 'Typography' элемента, с автоматической проверкой на
-        переполнение текстового контента, и если есть переполнение, то тогда
-        добавляется кастомизируемая обертка 'Tooltip'.
-        <br />
-        Важный параметр 'rowsCount', по умолчанию равен '1'. Означает число
-        строк, после которого текст будет обрезаться, и добавляться многоточие.
-        <br />
-        Так же важно учитывать, что элементы с 'rowsCount' больше 1 будут
-        обрезаться по словам, в одну строку будут обрезаться по буквенно
-      </Typography>
-      <Typography variant="h2">Примеры</Typography>
+    <div style={{ width: '600px' }}>
       <OverflowTypography>
         Not enough long text for special behavior.
       </OverflowTypography>
+      <br />
       <OverflowTypography>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque aut
         delectus dolorem ea, explicabo illo minus nostrum quae quod veniam.
       </OverflowTypography>
+    </div>
+  );
+};
+
+/**
+ * Prop ```rowsCount``` Означает число строк, после которого текст будет обрезаться, и добавляться многоточие. По умолчанию равен '1'.
+ * *
+ * Так же важно учитывать, что элементы с 'rowsCount' больше 1 будут обрезаться по словам, в одну строку будут обрезаться по буквенно
+ */
+export const RowsCount = () => {
+  return (
+    <div style={{ width: '600px' }}>
+      <OverflowTypography>
+        default props, Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Atque aut delectus dolorem ea, explicabo illo minus nostrum quae quod
+        veniam.
+      </OverflowTypography>
+      <br />
       <OverflowTypography rowsCount={2}>
         rowsCount = 2, like in default Autocomplite component. Lorem ipsum dolor
         sit amet. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui,
         tempore.
       </OverflowTypography>
+    </div>
+  );
+};
+
+/**
+ * Prop ```tooltipProps``` позволяет задавать кастомизируемые настройки для тултипа
+ */
+export const TooltipProps = () => {
+  return (
+    <div style={{ width: '600px' }}>
       <OverflowTypography
         tooltipProps={{
           title: 'custom tooltip, with separate settings',
@@ -51,25 +82,6 @@ export const ShowCase: Story = () => {
         adipisicing elit. Assumenda autem debitis eligendi inventore magni nobis
         perspiciatis quisquam ratione, unde vel?
       </OverflowTypography>
-    </Stack>
+    </div>
   );
-};
-
-ShowCase.parameters = { options: { showPanel: false } };
-
-const Template: Story<OverflowedTypographyProps> = (args) => {
-  return (
-    <OverflowTypography {...args} ref={undefined}>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
-      deserunt dolore doloremque eligendi error esse facilis harum inventore
-      labore maxime non pariatur perspiciatis quae quibusdam, recusandae
-      repudiandae saepe tempore veritatis?
-    </OverflowTypography>
-  );
-};
-
-export const Default = Template.bind({});
-
-Default.parameters = {
-  controls: { expanded: true },
 };
