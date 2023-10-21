b918aeee8ff8394af9ddd4093d222ff032f10e05
Alekseev_Roman <roma_alieksieiev.02@mail.ru>
2023-10-21 17:19:59 +0300

    test(UIKIT-991,ui,TextField): Доработан тест

--- packages/components/src/TextField/TextField.test.tsx
+++ packages/components/src/TextField/TextField.test.tsx
@@ -1,5 +1,8 @@
-import { renderWithTheme, screen } from '@astral/tests';
-import { expect } from 'vitest';
+import { fireEvent, renderWithTheme, screen } from '@astral/tests';
+import { expect, it } from 'vitest';
+import { theme } from '@astral/tests/src/theme';
+
+import { ThemeProvider } from '../ThemeProvider';
 
 import { TextField } from './TextField';
 
@@ -12,5 +15,68 @@ describe('TextField', () => {
   it('Prop:endAdornment: Отображается элемент в конце', () => {
     renderWithTheme(<TextField endAdornment="end" />);
     expect(screen.getByText('end')).toBeInTheDocument();
+  });
+
+  it('Prop:label: отображается label', () => {
+    renderWithTheme(<TextField label="TestTextField" />);
+    expect(screen.getByLabelText('TestTextField')).toBeInTheDocument();
+  });
+
+  it('Prop:disabled: блокирует TextField', () => {
+    renderWithTheme(<TextField defaultValue="TestTextField" disabled />);
+    expect(screen.getByDisplayValue('TestTextField')).toBeDisabled();
+  });
+
+  it('Prop:error, helperText: отображает helperText и состояние error', () => {
+    renderWithTheme(
+      <ThemeProvider theme={theme}>
+        <TextField
+          defaultValue="TestTextField"
+          error
+          helperText="Обязательно"
+        />
+      </ThemeProvider>,
+    );
+
+    expect(screen.getByText('Обязательно'));
+
+    expect(screen.getByText('Обязательно')).toHaveStyle({
+      color: theme.palette.error.dark,
+    });
+  });
+
+  it('Prop:success, helperText: отображает helperText и состояние success', () => {
+    renderWithTheme(
+      <ThemeProvider theme={theme}>
+        <TextField
+          defaultValue="TestTextField"
+          success
+          helperText="Удачно завершился процесс проверки"
+        />
+      </ThemeProvider>,
+    );
+
+    expect(
+      screen.getByText('Удачно завершился процесс проверки'),
+    ).toBeInTheDocument();
+
+    expect(screen.getByText('Удачно завершился процесс проверки')).toHaveStyle({
+      color: theme.palette.success.dark,
+    });
+  });
+
+  it('Prop:required: после становится required', () => {
+    renderWithTheme(<TextField defaultValue="TestTextField" required />);
+    expect(screen.getByDisplayValue('TestTextField')).toBeRequired();
+  });
+
+  it('Prop:changeValue: меняется значение input при вводе текста', () => {
+    renderWithTheme(<TextField defaultValue="TestTextField" required />);
+
+    fireEvent.change(screen.getByDisplayValue('TestTextField'), {
+      target: { value: 'newTextInTextField' },
+    });
+
+    expect(screen.getByDisplayValue('newTextInTextField')).toBeInTheDocument();
   });
 });
