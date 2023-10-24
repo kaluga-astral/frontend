a069d8ff95f1e60be5f7920e8d707d60a4f9839b
Bospur(Ivan) <bospobob@yandex.ru>
2023-10-24 10:10:21 +0300

    wip(UIKIT-984,ui,CollapsibleTypography): Добавил тест

--- packages/components/src/CollapsibleTypography/CollapsibleTypography.test.tsx
+++ packages/components/src/CollapsibleTypography/CollapsibleTypography.test.tsx
@@ -7,16 +7,17 @@ import {
 } from './CollapsibleTypography';
 
 const EXAMPLE_SHOW_TEXT = 'Какой-то текст';
-
-beforeAll(() => {
+const setScrollHeight = (value: number) => {
   Object.defineProperty(HTMLElement.prototype, 'scrollHeight', {
     configurable: true,
-    value: 200,
+    value: value,
   });
-});
+};
 
 describe('CollapsibleTypography ', () => {
   it('Prop: rowsCount=1: Появляется кнопка раскрытия ', () => {
+    setScrollHeight(200);
+
     renderWithTheme(
       <CollapsibleTypography rowsCount={1}>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
@@ -32,6 +33,8 @@ describe('CollapsibleTypography ', () => {
   });
 
   it('Prop: rowsCount={-5}: При отрицательном значении устанавливается значение по умолчанию', () => {
+    setScrollHeight(200);
+
     renderWithTheme(
       <CollapsibleTypography rowsCount={-5}>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
@@ -47,6 +50,8 @@ describe('CollapsibleTypography ', () => {
   });
 
   it('Prop: showButtonText={"Какой-то текст"}: Изменяется текст кнопки открытия', () => {
+    setScrollHeight(200);
+
     renderWithTheme(
       <CollapsibleTypography showButtonText={EXAMPLE_SHOW_TEXT}>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
@@ -62,6 +67,8 @@ describe('CollapsibleTypography ', () => {
   });
 
   it('Prop: hideButtonText={"Какой-то текст"}: Изменяется текст кнопки сокрытия', async () => {
+    setScrollHeight(200);
+
     renderWithTheme(
       <CollapsibleTypography hideButtonText={EXAMPLE_SHOW_TEXT}>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
@@ -78,6 +85,8 @@ describe('CollapsibleTypography ', () => {
   });
 
   it('Click control button: При нажатии на кнопку "Показать" меняется состояние', async () => {
+    setScrollHeight(200);
+
     renderWithTheme(
       <CollapsibleTypography rowsCount={1}>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
@@ -91,5 +100,22 @@ describe('CollapsibleTypography ', () => {
 
     await userEvents.click(controlButton);
     expect(screen.getByText(HIDE_BUTTON_TEXT)).toBeInTheDocument();
+  });
+
+  it('если контент не переполнен', () => {
+    setScrollHeight(0);
+
+    renderWithTheme(
+      <CollapsibleTypography>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+        asperiores a, aliquam nam nihil maxime eaque aliquid illo hic
+        architecto, aperiam repellendus quaerat nulla esse debitis repudiandae,
+        suscipit ipsa officia.
+      </CollapsibleTypography>,
+    );
+
+    const controlButton = screen.queryByRole('button');
+
+    expect(controlButton).not.toBeInTheDocument();
   });
 });
