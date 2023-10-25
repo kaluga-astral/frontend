c57230de791c50b315639ecfa1615a63b9f49ac6
StanislavDa <flugegeheimenbolsen@gmail.com>
2023-10-25 12:52:03 +0500

    feat(UIKIT-325,ui,MenuOrganization): Исправлены отступы

--- packages/components/src/MenuOrganization/styles.ts
+++ packages/components/src/MenuOrganization/styles.ts
@@ -13,7 +13,7 @@ export const OrganizationGroupTitle = styled.div`
     top: 0;
     z-index: 1;
 
-    padding: ${({ theme }) => theme.spacing(2, 3)};
+    padding: ${({ theme }) => theme.spacing(3, 2, 1, 2)};
 
     background: ${({ theme }) => theme.palette.background.paper};
   }
