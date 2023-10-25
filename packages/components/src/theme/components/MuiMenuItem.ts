caed66dff4fda4c935681d4fa3d2e62afe312df4
Sedykh Ivan <konungivan@gmail.com>
2023-10-25 08:49:16 +0500

    bug(UIKIT-982,ui): Устранение циклической зависимости Theme

--- packages/components/src/theme/components/MuiMenuItem.ts
+++ packages/components/src/theme/components/MuiMenuItem.ts
@@ -1,6 +1,6 @@
 import { Components, menuItemClasses } from '@mui/material';
 
-import type { Theme } from '../baseTheme';
+import { Theme } from '../types';
 
 export const MuiMenuItem: Components<Theme>['MuiMenuItem'] = {
   styleOverrides: {
---
+++ packages/components/src/theme/types.ts
@@ -0,0 +1,3 @@
+import { Theme as MuiTheme } from '@mui/material/styles';
+
+export type Theme = MuiTheme;
