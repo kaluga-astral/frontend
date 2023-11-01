aa4ea9320ea1ce1ef3ecb62bfaef14a89c3bb143
JetBlackCodes <melnikova_ia@astral.ru>
2023-11-01 17:04:55 +0300

    test(UIKIT-946,form,CodeField): Написан тест для FormCodeField

---
+++ packages/form/src/FormCodeField/FormCodeField.test.tsx
@@ -0,0 +1,40 @@
+import { renderWithTheme, screen, userEvents, waitFor } from '@astral/tests';
+import { object, string } from '@astral/validations';
+import { resolver } from '@astral/validations-react-hook-form-resolver';
+
+import { Form } from '../Form';
+import { useForm } from '../hooks';
+
+import { FormCodeField } from './FormCodeField';
+
+type FormValues = { code: string };
+
+const validationSchema = object<FormValues>({
+  code: string(),
+});
+
+describe('FormTextField', () => {
+  it('Отображается ошибка валидации', async () => {
+    const TestComponent = () => {
+      const form = useForm({
+        resolver: resolver<FormValues>(validationSchema),
+      });
+
+      return (
+        <Form form={form} onSubmit={form.handleSubmit(() => undefined)}>
+          <FormCodeField name="code" control={form.control} />
+          <button type="submit">submit</button>
+        </Form>
+      );
+    };
+
+    renderWithTheme(<TestComponent />);
+    await userEvents.click(screen.getByText('submit'));
+
+    await waitFor(() => {
+      const errorText = screen.getByText('Обязательно');
+
+      expect(errorText).toBeVisible();
+    });
+  });
+});
