77d6e9f0bbd275a9ad775b2dd44429a2feb4ef7a
cryzer12 <feoktistov.ilya@yandex.ru>
2023-11-10 17:42:53 +0300

    feat(UIKIT-1016,features,ProductSwitcher): Добавлены виджеты продуктов (#735)
    Co-authored-by: cryzerok <cryzer1271@gmail.com>

---
+++ packages/features/src/productSwitcher/utils/formatIdentityProducts/formatIdentityProducts.test.ts
@@ -0,0 +1,71 @@
+import { WidgetProduct } from '@astral/ui';
+
+import { IdentityProductsWidgetDTO } from '../../types';
+
+import { formatIdentityProducts } from './formatIdentityProducts';
+
+const IDENTITY_URL = 'testIdentity';
+
+describe('formatIdentityProducts', () => {
+  it('Форматирует список продуктов identity в формат для виджета продуктов', () => {
+    const identityInitialProducts: IdentityProductsWidgetDTO[] = [
+      {
+        id: 'testId',
+        name: 'testName',
+        productUrl: 'testUrl',
+        iconFileId: 'testIconFile',
+        backgroundHexColor: 'testColor',
+      },
+    ];
+
+    const formattedProducts: WidgetProduct[] = [
+      {
+        id: 'testId',
+        url: 'testUrl',
+        name: 'testName',
+        logoUrl: 'testIdentity/api/files/testIconFile',
+        color: 'testColor',
+      },
+    ];
+
+    const result = formatIdentityProducts(
+      IDENTITY_URL,
+      identityInitialProducts,
+    );
+
+    expect(result).toStrictEqual(formattedProducts);
+  });
+
+  it('Форматирует продукты с незаполненными необязательными полями', () => {
+    const identityInitialProducts: IdentityProductsWidgetDTO[] = [
+      {
+        id: 'testId',
+        iconFileId: 'testIconFile',
+        logoUrl: 'testIdentity/api/files/testIconFile',
+      },
+    ];
+
+    const formattedProducts: WidgetProduct[] = [
+      {
+        id: 'testId',
+        url: '',
+        name: '',
+        logoUrl: 'testIdentity/api/files/testIconFile',
+        color: '',
+      },
+    ];
+
+    const result = formatIdentityProducts(
+      IDENTITY_URL,
+      identityInitialProducts,
+    );
+
+    expect(result).toStrictEqual(formattedProducts);
+  });
+
+  it('Возвращает пустой массив, если нет продуктов', () => {
+    const result = formatIdentityProducts(IDENTITY_URL);
+
+    expect(result).toStrictEqual([]);
+  });
+});
