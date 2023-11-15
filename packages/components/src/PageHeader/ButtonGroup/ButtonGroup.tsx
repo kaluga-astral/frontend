be90e05591c9852ac77114c18e486e1cc4b37873
ystudenikin <studenikin.jarik@gmail.com>
2023-11-15 14:36:03 +0300

    feat(UIKIT-1038,ui,PageHeader/ButtonGroup): Добавлена поддержка DropdownButton

--- packages/components/src/PageHeader/ButtonGroup/ButtonGroup.tsx
+++ packages/components/src/PageHeader/ButtonGroup/ButtonGroup.tsx
@@ -1,41 +1,97 @@
 import { DotsOutlineMd } from '@astral/icons';
+import { useCallback, useMemo } from 'react';
 
 import { Button, ButtonProps } from '../../Button';
 import { ListItemButton, ListItemButtonProps } from '../../ListItemButton';
 import { ListItemText } from '../../ListItemText';
 import { IconDropdownButton } from '../../IconDropdownButton';
+import { MenuItem, MenuItemProps } from '../../MenuItem';
+import { DropdownButton, DropdownButtonProps } from '../../DropdownButton';
 
 import { ButtonGroupWrapper } from './styles';
 
-export type ButtonGroupProps = {
-  main?: (Omit<ButtonProps, 'children'> & {
-    text: string;
-  })[];
-  secondary?: (Omit<ListItemButtonProps, 'children'> & {
-    text: string;
-  })[];
+type NestedAction = MenuItemProps & {
+  /** Название действия */
+  text: string;
 };
 
-export const ButtonGroup = (props: ButtonGroupProps) => {
-  const { main, secondary } = props;
+type SingleAction = Omit<ButtonProps, 'children'> & {
+  /** Флаг показа выпадающего списка при клике */
+  nested?: false;
+  /** Название действия */
+  text: string;
+};
+
+type MultipleAction = Omit<DropdownButtonProps, 'children' | 'name'> & {
+  /** Флаг показа выпадающего списка при клике */
+  nested: true;
+  /** Список действий для выпадающего списка */
+  actions: NestedAction[];
+  /** Название действия */
+  text: string;
+};
+
+type MainAction = SingleAction | MultipleAction;
+
+type SecondaryAction = Omit<ListItemButtonProps, 'children'> & {
+  text: string;
+};
+
+export type ButtonGroupProps = {
+  /** Основные действия */
+  main?: MainAction[];
+  /** Второстепенные действия */
+  secondary?: SecondaryAction[];
+};
+
+export const ButtonGroup = ({
+  main = [],
+  secondary = [],
+}: ButtonGroupProps) => {
+  const renderMainAction = useCallback((action: MainAction) => {
+    if (action.nested) {
+      const { text, actions, ...buttonProps } = action;
+
+      return (
+        <DropdownButton name={text} {...buttonProps}>
+          {actions.map(({ text: nestedActionText, ...nestedProps }) => (
+            <MenuItem key={nestedActionText} {...nestedProps}>
+              {nestedActionText}
+            </MenuItem>
+          ))}
+        </DropdownButton>
+      );
+    }
+
+    const { text, ...mainProps } = action;
+
+    return (
+      <Button key={text} {...mainProps}>
+        {text}
+      </Button>
+    );
+  }, []);
+
+  const renderSecondaryActions = useMemo(() => {
+    if (!Boolean(secondary?.length)) {
+      return null;
+    }
+
+    return (
+      <IconDropdownButton icon={<DotsOutlineMd />} variant="light">
+        {secondary.map(({ text, ...secondaryProps }) => (
+          <ListItemButton key={text} {...secondaryProps}>
+            <ListItemText primary={text} />
+          </ListItemButton>
+        ))}
+      </IconDropdownButton>
+    );
+  }, [secondary]);
 
   return (
     <ButtonGroupWrapper>
-      {secondary && (
-        <IconDropdownButton icon={<DotsOutlineMd />} variant="light">
-          {secondary.map(({ text, ...secondaryProps }) => (
-            <ListItemButton key={text} {...secondaryProps}>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          ))}
-        </IconDropdownButton>
-      )}
-      {main &&
-        main.map(({ text, ...mainProps }) => (
-          <Button key={text} {...mainProps}>
-            {text}
-          </Button>
-        ))}
+      {renderSecondaryActions}
+      {main && main.map(renderMainAction)}
     </ButtonGroupWrapper>
   );
 };
