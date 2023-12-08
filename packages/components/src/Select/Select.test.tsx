85545703ad91a1892db9a23ec09820a1d2d371bc
Andrey Potyomkin <and_tem@icloud.com>
2023-12-08 12:02:40 +0300

    test: Изменен формат части тест-кейсов (#759)

--- packages/components/src/Select/Select.test.tsx
+++ packages/components/src/Select/Select.test.tsx
@@ -7,7 +7,7 @@ import { MenuItem } from '../MenuItem';
 import { Select } from './Select';
 
 describe('Select', () => {
-  it('Prop:ref: is present', () => {
+  it('Ref доступен', () => {
     const resultRef = { current: null };
 
     const SelectWithRef = () => {
@@ -28,32 +28,34 @@ describe('Select', () => {
     expect(resultRef?.current).not.toBeNull();
   });
 
-  it('Props:hideHelperText=true: helperText не отображается', () => {
-    renderWithTheme(<Select helperText="helper text" hideHelperText />);
-    expect(screen.queryByText('helper text')).toBeNull();
+  describe('HelperText', () => {
+    it('Скрывается, если hideHelperText=true', () => {
+      renderWithTheme(<Select helperText="helper text" hideHelperText />);
+      expect(screen.queryByText('helper text')).toBeNull();
+    });
+
+    it('Отображается в состоянии ошибки', () => {
+      renderWithTheme(<Select helperText="helper text" error />);
+      expect(screen.getByText('helper text')).toBeInTheDocument();
+    });
+
+    it('Отображается в состоянии success', () => {
+      renderWithTheme(<Select helperText="helper text" success />);
+      expect(screen.getByText('helper text')).toBeInTheDocument();
+    });
   });
 
-  it('Props:error, helperText: отображается статус error и helperText', () => {
-    renderWithTheme(<Select helperText="helper text" error />);
-    expect(screen.getByText('helper text')).toBeInTheDocument();
-  });
-
-  it('Props:success, helperText: отображается статус success и helperText', () => {
-    renderWithTheme(<Select helperText="helper text" success />);
-    expect(screen.getByText('helper text')).toBeInTheDocument();
-  });
-
-  it('Props:label: отображается label', () => {
+  it('Label отображается', () => {
     renderWithTheme(<Select label="label" />);
     expect(screen.getByText('label')).toBeInTheDocument();
   });
 
-  it('Props:placeholder: отображается placeholder', () => {
+  it('Placeholder отображается', () => {
     renderWithTheme(<Select value="" placeholder="placeholder" />);
     expect(screen.getByText('placeholder')).toBeInTheDocument();
   });
 
-  it('Props:isNoData: отображается isNoData', () => {
+  it('NoData placeholder отображается, если нет ни одного option', () => {
     renderWithTheme(
       <Select value="" defaultOpen>
         {[]}
@@ -63,7 +65,7 @@ describe('Select', () => {
     expect(screen.getByText('Нет данных')).toBeInTheDocument();
   });
 
-  it('Props:loading: отображается loading', () => {
+  it('Отображается лоадер в состоянии загрузки', () => {
     renderWithTheme(
       <Select loading value="" defaultOpen>
         {[]}
@@ -73,7 +75,7 @@ describe('Select', () => {
     expect(screen.getByRole('progressbar')).toBeInTheDocument();
   });
 
-  it('Props:renderValue: value не пустое и select отображает выбранные значения', () => {
+  it('Выбранные значения отображаются, если  value не пустое', () => {
     const children = [
       { value: '1', name: 'Name 1' },
       { value: '2', name: 'Name 2' },
@@ -107,7 +109,7 @@ describe('Select', () => {
     );
   });
 
-  it('Props:renderValue: value пустое и select не отображает значения', () => {
+  it('Рендерится пустой инпут, если значения не выбраны', () => {
     const children = [
       { value: '1', name: 'Name 1' },
       { value: '2', name: 'Name 2' },
