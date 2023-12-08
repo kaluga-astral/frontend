b1762979cad2ef1d6dc3eeebeaf826d85c08738d
Ilya <p9067700518@gmail.com>
2023-12-08 10:19:00 +0300

    feat(UIKIT-621,ui,EmailField): Правит тест для EmailField

--- packages/components/src/EmailField/EmailField.test.tsx
+++ packages/components/src/EmailField/EmailField.test.tsx
@@ -1,93 +1,14 @@
-import { fireEvent, renderWithTheme, screen } from '@astral/tests';
-import { vi } from 'vitest';
-import { useEffect, useRef } from 'react';
+import { renderWithTheme, screen } from '@astral/tests';
 
 import { EmailField } from './EmailField';
 
 describe('EmailField', () => {
-  it('Prop:startAdornment: Отображается элемент в начале', () => {
-    renderWithTheme(<EmailField startAdornment="start" />);
-    expect(screen.getByText('start')).toBeInTheDocument();
-  });
-
-  it('Prop:endAdornment: Отображается элемент в конце', () => {
-    renderWithTheme(<EmailField endAdornment="end" />);
-    expect(screen.getByText('end')).toBeInTheDocument();
-  });
-
-  it('Prop:label: отображается label', () => {
+  it('Prop:label: есть аттрибут type="email" ', () => {
     renderWithTheme(<EmailField label="TestEmailField" />);
-    expect(screen.getByLabelText('TestEmailField')).toBeInTheDocument();
-  });
 
-  it('Prop:disabled: блокирует TextField', () => {
-    renderWithTheme(<EmailField defaultValue="TestEmailField" disabled />);
-    expect(screen.getByDisplayValue('TestEmailField')).toBeDisabled();
-  });
-
-  it('Prop:required: после становится required', () => {
-    renderWithTheme(<EmailField defaultValue="TestEmailField" required />);
-    expect(screen.getByDisplayValue('TestEmailField')).toBeRequired();
-  });
-
-  it('Prop:onChange: корректно передаётся значение в onChange', () => {
-    const onChangeMock = vi.fn();
-
-    renderWithTheme(
-      <EmailField
-        onChange={(e) => onChangeMock(e.target.value)}
-        defaultValue="TestEmailField"
-      />,
+    expect(screen.getByLabelText('TestEmailField')).toHaveAttribute(
+      'type',
+      'email',
     );
-
-    fireEvent.change(screen.getByDisplayValue('TestEmailField'), {
-      target: { value: 'newEmailInEmailField' },
-    });
-
-    expect(onChangeMock.mock.lastCall).toEqual(['newEmailInEmailField']);
-    vi.clearAllMocks();
-  });
-
-  it('Prop:ref: ref из props заменяется на оригинальный', () => {
-    const resultRef = { current: null };
-
-    const TextFieldRef = () => {
-      const ref = useRef(null);
-
-      useEffect(() => {
-        resultRef.current = ref.current;
-      }, []);
-
-      return <EmailField ref={ref} defaultValue="TestEmailField" />;
-    };
-
-    renderWithTheme(<TextFieldRef />);
-    expect(resultRef?.current).not.toBeNull();
-  });
-
-  it('Prop:error: отображает состояние error', () => {
-    renderWithTheme(
-      <EmailField
-        defaultValue="TestEmailField"
-        error
-        helperText="Обязательно"
-      />,
-    );
-
-    expect(screen.getByText('Обязательно')).toBeInTheDocument();
-  });
-
-  it('Prop:success: отображает состояние success', () => {
-    renderWithTheme(
-      <EmailField
-        defaultValue="TestEmailField"
-        success
-        helperText="Удачно завершился процесс проверки"
-      />,
-    );
-
-    expect(
-      screen.getByText('Удачно завершился процесс проверки'),
-    ).toBeInTheDocument();
   });
 });
