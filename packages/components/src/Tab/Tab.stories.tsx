ed4ca8d6ffa3d526bd826c707af4fe6371e055ab
korablin-ivan <ikorablin@tehzor.ru>
2023-12-25 08:11:10 +0300

    refactor(UIKIT-443,components,Tabs): Изменение storybook

--- packages/components/src/Tab/Tab.stories.tsx
+++ packages/components/src/Tab/Tab.stories.tsx
@@ -1,18 +1,39 @@
-import { type Story } from '@storybook/react';
+import { type Meta, type StoryObj } from '@storybook/react';
 import { type SyntheticEvent, useState } from 'react';
 import { Stack } from '@mui/material';
 
-import { Tabs } from '../Tabs';
+import { Grid } from '../Grid';
 import { Typography } from '../Typography';
+import { Tabs } from '../Tabs';
 
 import { Tab } from './Tab';
 
-export default {
-  title: 'Components/Tabs/Tab',
+/**
+ * ### [Figma](https://www.figma.com/file/3ghN4WjSgkKx5rETR64jqh/Sirius-Design-System-(%D0%90%D0%9A%D0%A2%D0%A3%D0%90%D0%9B%D0%AC%D0%9D%D0%9E)?type=design&node-id=376-5801&mode=design&t=6DlItWfeBP8xOP7n-0)
+ * ### [Guide]()
+ */
+const meta: Meta<typeof Tab> = {
+  title: 'Components/Tab',
   component: Tab,
 };
 
-export const TabShowcase: Story = () => {
+export default meta;
+
+type Story = StoryObj<typeof Tab>;
+
+export const Interaction: Story = {
+  args: {
+    label: 'Tab',
+  },
+  parameters: {
+    options: { showPanel: false },
+    docs: {
+      disable: true,
+    },
+  },
+};
+
+export const TabsWithTab = () => {
   const [value, setValue] = useState(0);
 
   const handleChange = (_event: SyntheticEvent, newValue: number) => {
@@ -20,19 +41,23 @@ export const TabShowcase: Story = () => {
   };
 
   return (
-    <Stack gap={4}>
+    <Grid container spacing={2} rows={2}>
+      <Typography>
+        Пример Tab с компонентом Tabs, который является оберткой для размещения
+        прочих компонентов в заголовке.
+      </Typography>
       <Stack gap={4}>
-        <Typography variant="h3">Tab</Typography>
-        <Stack gap={1}>
-          <Tabs value={value} onChange={handleChange}>
-            <Tab label="Tab" />
-            <Tab label="Tab" />
-            <Tab label="Tab" disabled />
-          </Tabs>
+        <Stack gap={4}>
+          <Typography variant="h3">Tab</Typography>
+          <Stack gap={1}>
+            <Tabs value={value} onChange={handleChange}>
+              <Tab label="Tab" />
+              <Tab label="Tab" />
+              <Tab label="Tab" disabled />
+            </Tabs>
+          </Stack>
         </Stack>
       </Stack>
-    </Stack>
+    </Grid>
   );
 };
-
-TabShowcase.parameters = { options: { showPanel: false } };
