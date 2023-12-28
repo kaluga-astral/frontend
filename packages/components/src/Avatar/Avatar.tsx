190abbba778909a08529183fb91767273fb8e454
Andrey Zaitsev <ScootalooRainbow@yandex.ru>
2023-12-28 17:31:45 +0300

    bug(UIKIT-1125, ui, Avatar): Исправлен размер шрифта

--- packages/components/src/Avatar/Avatar.tsx
+++ packages/components/src/Avatar/Avatar.tsx
@@ -1,1 +1,12 @@
-export { Avatar } from '@mui/material';
+import { type AvatarProps as MuiAvatarProps } from '@mui/material';
+import { forwardRef } from 'react';
+
+import { type WithoutEmotionSpecific } from '../types';
+
+import { AvatarRoot } from './styles';
+
+export type AvatarProps = WithoutEmotionSpecific<MuiAvatarProps>;
+
+export const Avatar = forwardRef<HTMLDivElement, AvatarProps>((props, ref) => {
+  return <AvatarRoot {...props} ref={ref} />;
+});
