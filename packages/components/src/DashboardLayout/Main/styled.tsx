4db67fbac9a1c86da15cd601648874cbe5dd2185
flugegeheimen <flugegeheimenbolsen@gmail.com>
2024-02-06 14:13:28 +0500

    bug(UIKIT-1205,ui,DashboardLayout): Возвращены стили гридов

--- packages/components/src/DashboardLayout/Main/styled.tsx
+++ packages/components/src/DashboardLayout/Main/styled.tsx
@@ -3,4 +3,8 @@ import { styled } from '../../styles';
 export const MainRoot = styled.main`
   display: flex;
   flex-direction: column;
+  grid-column: 2;
+  grid-row: 2;
+  overflow: hidden;
+  grid-area: main;
 `;
