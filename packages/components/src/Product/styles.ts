0458a1db8bbb22cbf23c227ada617fe914c19efb
Maxim Frolov <mfrolov@gmail.com>
2024-02-09 14:47:55 +0300

    feat(UIKIT-1217,ui,Product): Реализован адаптив под мобильные устройства

--- packages/components/src/Product/styles.ts
+++ packages/components/src/Product/styles.ts
@@ -3,4 +3,9 @@ import { Button } from '../Button';
 
 export const ProductButton = styled(Button)`
   font-size: ${({ theme }) => theme.typography.h5.fontSize};
+
+  ${({ theme }) => theme.breakpoints.down('sm')} {
+    font-size: ${({ theme }) => theme.typography.body1.fontSize};
+    font-weight: ${({ theme }) => theme.typography.body1.fontWeight};
+  }
 `;
