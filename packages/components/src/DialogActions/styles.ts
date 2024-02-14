722d125b72617514c42a3b0861fc6cb1d8425bc0
iskorostinskiinikita <nikita11042000@gmail.com>
2024-02-14 14:05:11 +0300

    feat(UIKIT-1179): Исправлены стили в DialogActions

--- packages/components/src/DialogActions/styles.ts
+++ packages/components/src/DialogActions/styles.ts
@@ -1,11 +1,12 @@
 import { Grid, type GridProps } from '../Grid';
 import { styled } from '../styles/styled';
 
-type DialogActionsGridProps = GridProps & { isMobile: boolean };
-
-export const DialogActionsGrid = styled(Grid, {
-  shouldForwardProp: (prop) => prop !== 'isMobile',
-})<DialogActionsGridProps>`
-  ${({ isMobile }) => !isMobile && 'justify-content: end'};
-  grid-auto-flow: ${({ isMobile }) => (isMobile ? 'row' : 'column')};
+export const DialogActionsGrid = styled(Grid)<GridProps>`
+  ${({ theme }) => theme.breakpoints.up('sm')} {
+    grid-auto-flow: column;
+    justify-content: end;
+  }
 `;
+
+// ${({ isMobile }) => !isMobile && 'justify-content: end'};
+// grid-auto-flow: ${({ isMobile }) => (isMobile ? 'row' : 'column')};
