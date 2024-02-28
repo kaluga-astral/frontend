f4d4edd25936b47e43357c3c1f68586f5f46b2b2
ddanilov-it <ddanya03@yandex.ru>
2024-02-28 15:03:37 +0300

    feat(UIKIT-629,ui,FlowButton): Реализована стори Colors

--- packages/components/src/FlowButton/FlowButton.stories.tsx
+++ packages/components/src/FlowButton/FlowButton.stories.tsx
@@ -40,8 +40,25 @@ export const Example = () => (
   </FlowButton>
 );
 
-export const Skip = () => (
+export const TargetText = () => (
   <FlowButton targetText="Пропустить" color="success">
     Пропустить этот шаг
   </FlowButton>
+);
+
+export const Colors = () => (
+  <>
+    <FlowButton targetText="Начальный" color="primary">
+      Синий цвет
+    </FlowButton>
+    <FlowButton targetText="Успех" color="success">
+      Зелёный цвет
+    </FlowButton>
+    <FlowButton targetText="Ошибка" color="error">
+      Красный цвет
+    </FlowButton>
+    <FlowButton targetText="Предупреждение" color="warning">
+      Оранжевый цвет
+    </FlowButton>
+  </>
 );
