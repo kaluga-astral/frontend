54ce82f5bf828a566276a3201566e588a1612834
aesarantsev <47385491+aesarantsev@users.noreply.github.com>
2024-02-29 23:59:52 +1000

    feat(UIKIT-352,ui,Iframe): Реализован компонент Iframe (#856)
    Co-authored-by: Andrey Potyomkin <and_tem@icloud.com>

---
+++ packages/components/src/Iframe/Iframe.stories.tsx
@@ -0,0 +1,94 @@
+import { type Meta } from '@storybook/react';
+import { type PropsWithChildren, useRef, useState } from 'react';
+
+import { Grid } from '../Grid';
+import { ConfigProvider } from '../ConfigProvider';
+import errorIllustration from '../../../ui/illustrations/error.svg';
+import { Button } from '../Button';
+
+import { Iframe } from './Iframe';
+
+/**
+ * Iframe позволяет отображать контент из указанного URL.
+ *
+ * ### [Figma]()
+ * ### [Guide]()
+ */
+const meta: Meta<typeof Iframe> = {
+  title: 'Components/Iframe',
+  component: Iframe,
+};
+
+export default meta;
+
+const Container = ({ children }: PropsWithChildren) => {
+  return (
+    <ConfigProvider
+      imagesMap={{
+        defaultErrorImgSrc: errorIllustration,
+        noDataImgSrc: '',
+        outdatedReleaseErrorImgSrc: '',
+      }}
+    >
+      <div style={{ width: '430px', height: '250px' }}>{children}</div>
+    </ConfigProvider>
+  );
+};
+
+const ReloadContainer = ({ children }: PropsWithChildren) => {
+  const [key, setKey] = useState(1);
+
+  const reload = () => {
+    setKey((prev) => prev + 1);
+  };
+
+  return (
+    <Grid container spacing={2}>
+      <Button onClick={reload}>Перезагрузить</Button>
+
+      <Container key={key}>{children}</Container>
+    </Grid>
+  );
+};
+
+export const Example = () => {
+  return (
+    <ReloadContainer>
+      <Iframe src="https://www.youtube.com/embed/k85mRPqvMbE" />
+    </ReloadContainer>
+  );
+};
+
+export const LoadingContent = () => {
+  return (
+    <ReloadContainer>
+      <Iframe
+        loadingContent="loading..."
+        src="https://www.youtube.com/embed/k85mRPqvMbE"
+      />
+    </ReloadContainer>
+  );
+};
+
+export const Error = () => {
+  const ref = useRef<HTMLIFrameElement>(null);
+
+  const triggerIframeError = () => {
+    ref.current?.dispatchEvent(
+      new ErrorEvent('error', {
+        message: 'Ошибка при нажатии на кнопку',
+      }),
+    );
+  };
+
+  return (
+    <Grid container spacing={2}>
+      <Button fullWidth onClick={triggerIframeError}>
+        Вызвать ошибку
+      </Button>
+      <Container>
+        <Iframe ref={ref} src="https://www.youtube.com/embed/k85mRPqvMbE" />
+      </Container>
+    </Grid>
+  );
+};
---
+++ packages/components/src/Iframe/Iframe.test.tsx
@@ -0,0 +1,52 @@
+import { describe, expect, it, vi } from 'vitest';
+import { act, renderWithTheme, screen } from '@astral/tests';
+import { useEffect, useRef } from 'react';
+
+import { Iframe } from './Iframe';
+
+describe('Iframe', () => {
+  it('Ref доступен', () => {
+    const resultRef = { current: null };
+
+    const IframeWithRef = () => {
+      const ref = useRef(null);
+
+      useEffect(() => {
+        resultRef.current = ref.current;
+      }, []);
+
+      return <Iframe ref={ref} src="https://example.com" />;
+    };
+
+    renderWithTheme(<IframeWithRef />);
+    expect(resultRef?.current).not.toBeNull();
+  });
+
+  it('Атрибут src соответствует переданному', () => {
+    renderWithTheme(<Iframe title="test iframe" src="https://example.com" />);
+
+    const iframe = screen.getByTitle('test iframe');
+
+    expect(iframe).toHaveAttribute('src', 'https://example.com');
+  });
+
+  it('onError вызывается при ошибке', async () => {
+    const onErrorSpy = vi.fn();
+
+    renderWithTheme(
+      <Iframe
+        title="test iframe"
+        src="https://example.com"
+        onError={onErrorSpy}
+      />,
+    );
+
+    const iframe: HTMLIFrameElement = screen.getByTitle('test iframe');
+
+    act(() => {
+      iframe.dispatchEvent(new ErrorEvent('error'));
+    });
+
+    expect(onErrorSpy).toHaveBeenCalled();
+  });
+});
