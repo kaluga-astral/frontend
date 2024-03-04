3ba0be5fee994052a71cd8aa33e855d8ea64b788
Uvoi <iovu_dmtr@yahoo.com>
2024-03-04 15:14:37 +0300

    feat(UIKIT-983): Исправлены стили

--- packages/components/src/Link/styles.ts
+++ packages/components/src/Link/styles.ts
@@ -1,37 +1,40 @@
 import { Link } from '@mui/material';
+import { theme } from '@astral/tests/src/theme';
 
 import { styled } from '../styles';
 
-const DEFAULT = '#1874FF';
-const HOVER = '#268FF9';
-const ACTIVE = '#072D57';
-const VISITED = '#762376';
-const OUTLINE = '#70AEFF';
-
 export const StyledLink = styled(Link)`
-  font-size: 14px;
-  color: ${DEFAULT};
+  padding: ${theme.spacing(0.5, 0.25, 0.5, 0)};
+
+  font-size: ${theme.typography.fontSize}px;
+  color: ${theme.palette.components.link.main};
 
   &:active {
-    color: ${ACTIVE};
+    color: ${theme.palette.components.link.active};
   }
 
   &:visited {
-    color: ${VISITED};
+    color: ${theme.palette.components.link.visited};
   }
 
   &:hover {
-    color: ${HOVER};
+    color: ${theme.palette.components.link.hover};
 
-    span {
+    * {
       text-decoration: underline;
     }
   }
 
   &:focus {
     border-radius: 4px;
-    outline: 2px solid ${OUTLINE};
-    outline-offset: 1.5px;
+    outline: 2px solid ${theme.palette.primary[400]};
+  }
+
+  & div {
+    display: inline-flex;
+    gap: ${theme.spacing(1)};
+    align-items: baseline;
+    justify-content: flex-start;
   }
 
   .MuiSvgIcon-root {
