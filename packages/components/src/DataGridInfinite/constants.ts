02bf7067113e3cc6a865b27b9b32e55df52a0c32
Andrey Andreev <agro_06@mail.ru>
2024-03-05 11:15:21 +0300

    feat(UIKIT-265,ui,DataGridInfinite): Изменен текст в конце скролла

--- packages/components/src/DataGridInfinite/constants.ts
+++ packages/components/src/DataGridInfinite/constants.ts
@@ -2,4 +2,4 @@ export const OVERSCAN_COUNT = 30;
 
 export const DEFAULT_ROW_HEIGHT = 44;
 
-export const END_OF_SCROLL_MESSAGE = 'Вы достигли конца списка';
+export const END_OF_SCROLL_MESSAGE = 'Конец списка';
