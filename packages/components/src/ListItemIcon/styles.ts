b55a240665ff47c3d03596bf5f8f7fa49bcad5ce
Andrey Andreev <agro_06@mail.ru>
2024-03-12 15:57:32 +0300

    bug(UIKIT-1311,ui,ListItemIcon): Поправлен цвет иконки при hover и selected (#871)

--- packages/components/src/ListItemIcon/styles.ts
+++ packages/components/src/ListItemIcon/styles.ts
@@ -3,5 +3,5 @@ import { ListItemIcon } from '@mui/material';
 import { styled } from '../styles';
 
 export const StyledListItemIcon = styled(ListItemIcon)`
-  color: ${({ theme }) => theme.palette.grey[900]};
+  color: inherit;
 `;
