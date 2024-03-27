0160fff26a5cba4156dd380bfdce33c5fee32d06
Kostayne <fuckogg@yandex.ru>
2024-03-27 18:04:36 +0300

    bug(UIKIT-1343,ui,Avatar): Изменен font-size инициалов для мобилки

--- packages/components/src/Avatar/styles.tsx
+++ packages/components/src/Avatar/styles.tsx
@@ -3,6 +3,9 @@ import { Avatar } from '@mui/material';
 import { styled } from '../styles/styled';
 
 export const StyledAvatar = styled(Avatar)`
-  font-size: ${({ theme }) => theme.typography.h5.fontSize};
+  font-size: ${({ theme }) =>
+    theme.breakpoints.down('sm')
+      ? theme.typography.h7.fontSize
+      : theme.typography.h5.fontSize};
   font-weight: ${({ theme }) => theme.typography.h5.fontWeight};
 `;
