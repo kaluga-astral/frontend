308426a98ef31f02bdc12bb7ed4fbadd76b235fb
Kostayne <fuckogg@yandex.ru>
2024-03-28 10:04:25 +0300

    bug(UIKIT-1343,ui,Avatar): Изменена конструкция media query

--- packages/components/src/Avatar/styles.tsx
+++ packages/components/src/Avatar/styles.tsx
@@ -3,9 +3,10 @@ import { Avatar } from '@mui/material';
 import { styled } from '../styles/styled';
 
 export const StyledAvatar = styled(Avatar)`
-  font-size: ${({ theme }) =>
-    theme.breakpoints.down('sm')
-      ? theme.typography.h7.fontSize
-      : theme.typography.h5.fontSize};
+  font-size: ${({ theme }) => theme.typography.h5.fontSize};
   font-weight: ${({ theme }) => theme.typography.h5.fontWeight};
+
+  ${({ theme }) => theme.breakpoints.down('sm')} {
+    font-size: ${({ theme }) => theme.typography.h7.fontSize};
+  }
 `;
