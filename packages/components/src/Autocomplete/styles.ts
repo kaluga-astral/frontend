48fe25f513fb1cb304a5f65e307ac320bed103c9
Kostayne <fuckogg@yandex.ru>
2024-04-02 10:59:20 +0300

    bug(UIKIT-1317,ui,Autocomplete): Установлена высота Option в 32px

--- packages/components/src/Autocomplete/styles.ts
+++ packages/components/src/Autocomplete/styles.ts
@@ -1,7 +1,12 @@
-import { styled } from '../styles/styled';
+import { styled } from '../styles';
 import { Paper } from '../Paper';
+import { MenuItem } from '../MenuItem';
 
 export const PopperWrapper = styled(Paper)`
   margin-top: ${({ theme }) => theme.spacing(2)};
   padding: ${({ theme }) => theme.spacing(6, 0, 6, 3)};
+`;
+
+export const StyledMenuItem = styled(MenuItem)`
+  height: ${({ theme }) => theme.spacing(8)};
 `;
