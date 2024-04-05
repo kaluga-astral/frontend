2526f17d94630bad19ad491a3b17dc4dcaa909cc
Kostayne <fuckogg@yandex.ru>
2024-04-05 12:50:50 +0300

    bug(UIKIT-1317,ui,MenuItem): Убран файл styles.ts

--- packages/components/src/MenuItem/MenuItem.tsx
+++ packages/components/src/MenuItem/MenuItem.tsx
@@ -1,13 +1,8 @@
 import { type MenuItemProps as MuiMenuItemProps } from '@mui/material';
-import { type MenuItem as MuiMenuItem } from '@mui/material';
+import { MenuItem as MuiMenuItem } from '@mui/material';
 
 import { type WithoutEmotionSpecific } from '../types';
 
-import { StyledMenuItem } from './styles';
-
-// нужно для избежания ошибок при проверке типов в существующем коде
-type MuiMenuItemT = typeof MuiMenuItem;
-
 export type MenuItemProps = WithoutEmotionSpecific<MuiMenuItemProps>;
 
-export const MenuItem: MuiMenuItemT = StyledMenuItem as MuiMenuItemT;
+export const MenuItem = MuiMenuItem;
--- packages/components/src/MenuItem/styles.ts
+++
@@ -1,7 +0,0 @@
-import { MenuItem as MuiMenuItem } from '@mui/material';
-
-import { styled } from '../styles';
-
-export const StyledMenuItem = styled(MuiMenuItem)`
-  max-height: ${({ theme }) => theme.spacing(13)};
-`;
