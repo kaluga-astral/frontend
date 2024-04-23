af4db79a64b5ea35b2d188f6db576a09c3a4f57e
ooops_o_O <47597207+mfrolov89@users.noreply.github.com>
2024-04-23 15:41:10 +0300

    chore(UIKIT-1421): Реализован сервис для отправки новости с чейнжлгом в telegram канал (#930)

--- changelog-generator/src/utils/index.ts
+++ changelog-generator/src/utils/index.ts
@@ -1,4 +1,5 @@
 export { dateToView } from './dateToView';
+export { generateLinkToChangelog } from './generateLinkToChangelog';
 export { generateLinkToJira } from './generateLinkToJira';
 export { generateLinkToStorybook } from './generateLinkToStorybook';
 export { subtractDays } from './subtractDays';
