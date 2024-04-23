5ee5fe8133812dd89e5e3c1f2f33c2d09cc751c2
cryzerok <cryzer1271@gmail.com>
2024-04-23 13:51:28 +0300

    wip(UIKIT-1029,utils): Убраны теги descriptions из JSDOC

--- packages/utils/src/date/isDate/isDate.ts
+++ packages/utils/src/date/isDate/isDate.ts
@@ -3,7 +3,7 @@ import dayjs from 'dayjs';
 type Unknown = null | undefined | string | number | Date;
 
 /**
- * @description Утилита проверки значения на валидность даты
+ * Утилита проверки значения на валидность даты
  * @param {Unknown} value Значение, которое нужно проверить на валидность к дате
  * @return {Boolean} Возвращает boolean проверки валидности
  */
