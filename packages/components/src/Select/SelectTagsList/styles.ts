0cf4302b8ba96ac3ebd0610ac70cb66c275c4065
Kostayne <fuckogg@yandex.ru>
2024-04-26 17:27:50 +0300

    feat(UIKIT-1166,Select): Убраны старые стили из SelectTagsList

--- packages/components/src/Select/SelectTagsList/styles.ts
+++ packages/components/src/Select/SelectTagsList/styles.ts
@@ -8,16 +8,10 @@ export const TagsWrapper = styled.div`
   margin-bottom: -${({ theme }) => theme.spacing(1)};
 
   .MuiChip-root {
-    margin-bottom: ${({ theme }) => theme.spacing(1)};
-
     background-color: ${({ theme }) => theme.palette.grey['300']};
   }
 
   .MuiChip-root:hover {
     background-color: ${({ theme }) => theme.palette.grey['300']};
-  }
-
-  .MuiChip-root:not(:last-of-type) {
-    margin-right: ${({ theme }) => theme.spacing(1)};
   }
 `;
