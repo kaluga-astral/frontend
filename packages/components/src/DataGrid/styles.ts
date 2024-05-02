7d9793d679da5f4dade9f4a951152ec1430dd68f
Kostayne <fuckogg@yandex.ru>
2024-05-02 18:00:07 +0300

    feat(UIKIT-1273,DataGrid): Перенос padding'ов в DataGridCell

--- packages/components/src/DataGrid/styles.ts
+++ packages/components/src/DataGrid/styles.ts
@@ -18,14 +18,6 @@ export const Container = styled.div<StyledTableContainerProps>`
   width: 100%;
   height: 100%;
   max-height: ${({ maxHeight }) => (maxHeight ? `${maxHeight}px` : 'initial')};
-
-  & {
-    td,
-    th {
-      padding-right: ${({ theme }) => theme.spacing(2)};
-      padding-left: ${({ theme }) => theme.spacing(2)};
-    }
-  }
 `;
 
 export const StyledTableContainer = styled(TableContainer, {
