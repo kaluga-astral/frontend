64d9cd8973160ea8cf93c3e4dd65b86fb945a897
Maxim Frolov <mfrolov@gmail.com>
2024-05-02 17:31:47 +0300

    feat(UIKIT-546): Убраны description в описании типов

--- packages/components/src/NotificationNext/types.ts
+++ packages/components/src/NotificationNext/types.ts
@@ -32,39 +32,45 @@ export type NotificationProps = Omit<
   | 'theme'
 > & {
   /**
-   * @description Элемент заголовка, может быть контролируемой реакт нодой
+   * Элемент заголовка, может быть контролируемой реакт нодой
    * @example notify.success('title')
    * */
   title: ReactNode;
+
   /**
-   * @description флаг управления цветовым фоном уведомления, если true то фон, например success будет зеленым
+   * Флаг управления цветовым фоном уведомления, если true то фон, например success будет зеленым
    * @default true
    * @example notify.success('title', { filled: false })
    * */
   filled?: boolean;
+
   /**
-   * @description опциональный контент нотификации, может быть управляемой реакт нодой
+   * Опциональный контент нотификации, может быть управляемой реакт нодой
    * @example notify.success('title', { content: <div>Hello notify</div> })
    * */
   content?: ReactNode;
+
   /**
-   * @description реакт элемент предполагающий наличие кнопок для кастомных взаимодействий с пользователем
+   * React-элемент предполагающий наличие кнопок для кастомных взаимодействий с пользователем
    * @example notify.success('title', { actions: <button>lorem ipsum</button> })
    * */
   actions?: JSX.Element;
+
   /**
-   * @description местоположение кастомных кнопок активностей, слева или справа
+   * Местоположение кастомных кнопок активностей, слева или справа
    * @default right
    * @example notify.success('title', { actionsDirection: 'left' })
    * */
   actionsDirection?: ActionsDirection;
+
   /**
-   * @description опциональная иконка, которая будет подменять стандартную
+   * Опциональная иконка, которая будет подменять стандартную
    * @default зависит от Variant нотификации, например Variant="success" иконка "Зеленая галочка"
    */
   icon?: ReactNode;
+
   /**
-   * @description опциональный пропс позволяющий отключить кнопку рендера крестика закрытия нотификации
+   * Опциональный пропс позволяющий отключить кнопку рендера крестика закрытия нотификации
    * @default true
    */
   showCloseButton?: boolean;
@@ -79,20 +85,23 @@ type NotificationFunction = (
 
 type Controllable = {
   /**
-   * @description метод создания полностью кастомной нотификации
+   * Метод создания полностью кастомной нотификации
    */
   custom: (content: ToastContent, options?: ToastOptions) => Id;
+
   /**
-   * @description force метод для закрытия всех нотификаций, либо конкретной по айдишке
+   * force метод для закрытия всех нотификаций, либо конкретной по айдишке
    */
   dismiss: (id?: Id) => void;
+
   /**
-   * @description метод обновления конкретной нотификации,
+   * Метод обновления конкретной нотификации,
    * пригодится когда имеется контролируемый прогресс бар
    */
   update: (id: Id, options?: UpdateOptions) => void;
+
   /**
-   * @description метод закрывающий нотификацию с контролируемым прогресс баром
+   * Метод закрывающий нотификацию с контролируемым прогресс баром
    */
   done: (id: Id) => void;
 };
