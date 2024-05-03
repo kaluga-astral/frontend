cc424b6434cd3b4829b4209da075a9a210eb66f6
Kostayne <fuckogg@yandex.ru>
2024-05-03 08:52:45 +0300

    feat(UIKIT-1166,Select): Убраны стили цвета для тега

--- packages/components/src/Select/SelectTagsList/styles.ts
+++ packages/components/src/Select/SelectTagsList/styles.ts
@@ -6,12 +6,4 @@ export const TagsWrapper = styled.div`
   column-gap: ${({ theme }) => theme.spacing(1)};
 
   margin-bottom: -${({ theme }) => theme.spacing(1)};
-
-  .MuiChip-root {
-    background-color: ${({ theme }) => theme.palette.grey['300']};
-  }
-
-  .MuiChip-root:hover {
-    background-color: ${({ theme }) => theme.palette.grey['300']};
-  }
 `;
