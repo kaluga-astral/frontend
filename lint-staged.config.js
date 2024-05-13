44dfeead63f662e2020ffcaf3680f68f68cb51af
Kostayne <fuckogg@yandex.ru>
2024-05-13 14:58:35 +0300

    chore(UIKIT-903): Удален плагин cspeller для eslint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   'packages/ui/**/*.{js,jsx,ts,tsx}': [
-    'npm run lint --workspace=@astral/ui',
+    (fileNames) => `npm run spellcheck ${fileNames.join(' ')}`,
+    (fileNames) => `npm run lint --workspace=@astral/ui -- ${fileNames}`,
     () => 'npm run lint:types --workspace=@astral/ui',
   ],
 
   'packages/components/**/*.{js,jsx,ts,tsx}': [
+    (fileNames) => `npm run spellcheck ${fileNames.join(' ')}`,
     (fileNames) =>
       `npm run lint --workspace=@astral/components -- ${fileNames.join(' ')}`,
     () => 'npm run lint:types --workspace=@astral/components',
@@ -17,17 +19,20 @@ module.exports = {
   ],
 
   'packages/icons/**/*.{js,jsx,ts,tsx}': [
-    'npm run lint --workspace=@astral/icons',
+    (fileNames) => `npm run spellcheck ${fileNames.join(' ')}`,
+    (fileNames) => `npm run lint --workspace=@astral/icons -- ${fileNames.join(' ')}`,
     () => 'npm run lint:types --workspace=@astral/icons',
   ],
 
   'packages/form/**/*.{js,jsx,ts,tsx}': [
+    (fileNames) => `npm run spellcheck ${fileNames.join(' ')}`,
     (fileNames) =>
       `npm run lint --workspace=@astral/form -- ${fileNames.join(' ')}`,
     () => 'npm run lint:types --workspace=@astral/form',
   ],
 
   'packages/features/**/*.{js,jsx,ts,tsx}': [
+    (fileNames) => `npm run spellcheck ${fileNames.join(' ')}`,
     (fileNames) =>
       `npm run lint --workspace=@astral/features -- ${fileNames.join(' ')}`,
     () => 'npm run lint:types --workspace=@astral/features',
