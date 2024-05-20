17b9e01c3bd0b0189eb66830b91d6453681452ba
Sedykh Ivan <konungivan@gmail.com>
2024-05-20 15:57:01 +0500

    feat(UIKIT-1414,ui): Адаптация styled для работы с приватными пропсами стилей

--- packages/components/src/styles/styled.ts
+++ packages/components/src/styles/styled.ts
@@ -1,3 +1,12 @@
-export { default as styled } from '@emotion/styled';
+import { default as emotionStyled } from '@emotion/styled';
 
 export { keyframes, Global } from '@emotion/react';
+
+export const styled = new Proxy(emotionStyled, {
+  apply: (target, _thisArg, argArray) =>
+    target(argArray[0], {
+      ...argArray[1],
+      shouldForwardProp: (prop) =>
+        !prop.match(/^\$/) || argArray[1]?.shouldForwardProp?.(prop),
+    }),
+});
