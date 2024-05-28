ac8ff7bfef5a868da635203bb8d8e23d7bb27611
Maxim Frolov <mfrolov@gmail.com>
2024-05-28 12:06:28 +0300

    bug(UIKIT-1479,Tab): Поправлена типизация, в типы добавлена поддержка пропса component

--- packages/components/src/Tab/Tab.tsx
+++ packages/components/src/Tab/Tab.tsx
@@ -1,9 +1,20 @@
+import { type ElementType } from 'react';
 import { type TabProps as MuiTabProps } from '@mui/material';
 
 import { type WithoutEmotionSpecific } from '../types';
 
 import { StyledTab } from './styles';
 
-export type TabProps = WithoutEmotionSpecific<MuiTabProps>;
+export type TabProps<
+  TComponent extends ElementType,
+  TComponentProps = {},
+> = WithoutEmotionSpecific<MuiTabProps<TComponent, TComponentProps>> & {
+  component?: TComponent;
+};
 
-export const Tab = (props: TabProps) => <StyledTab {...props} />;
+export const Tab = <
+  TComponent extends ElementType = 'div',
+  TComponentProps = {},
+>(
+  props: TabProps<TComponent, TComponentProps>,
+) => <StyledTab {...props} />;
