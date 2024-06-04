33562a5bbae162326ce673809ec7968084c622bc
ooops_o_O <47597207+mfrolov89@users.noreply.github.com>
2024-06-04 11:21:03 +0300

    bug(UIKIT-1502): Заменена реализация хука useId для RadioCard, RadioGroup, StepSlider (#991)

--- packages/components/src/StepSlider/StepSlider.tsx
+++ packages/components/src/StepSlider/StepSlider.tsx
@@ -1,5 +1,6 @@
-import { type ReactNode, useEffect, useId, useRef, useState } from 'react';
+import { type ReactNode, useEffect, useRef, useState } from 'react';
 
+import { useId } from '../hooks/useId';
 import { Slide, type SlideProps } from '../Slide';
 
 import { Item, Wrapper } from './styles';
