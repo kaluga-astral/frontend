9b18e01c63202f8729d57a0b323a726053c76747
Королев Данил <105650840+pan1caisreal@users.noreply.github.com>
2024-06-27 17:08:59 +0300

    feat(UIKIT-1228,Product): Правки по PR

--- packages/components/src/Product/styles.ts
+++ packages/components/src/Product/styles.ts
@@ -3,17 +3,14 @@ import { Button } from '../Button';
 
 export const StyledButton = styled(Button)`
   font-size: ${({ theme }) => theme.typography.h5.fontSize};
-  font-weight: ${({ theme }) => theme.typography.link.fontWeight};
+  font-weight: ${({ theme }) => theme.typography.body1.fontWeight};
 
   ${({ theme }) => theme.breakpoints.down('sm')} {
     font-size: ${({ theme }) => theme.typography.body1.fontSize};
-    font-weight: ${({ theme }) => theme.typography.link.fontWeight};
+    font-weight: ${({ theme }) => theme.typography.body1.fontWeight};
   }
 
-  &:hover {
-    background-color: unset;
-  }
-
+  &:hover,
   &:active {
     color: unset;
 
