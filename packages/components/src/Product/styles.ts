9f8b98daaa7e96f6cba395648f1c73bdb59aeacd
Королев Данил <105650840+pan1caisreal@users.noreply.github.com>
2024-06-27 16:18:21 +0300

    feat(UIKIT-1228,Product): Убраны состояния, изменен fotWeight

--- packages/components/src/Product/styles.ts
+++ packages/components/src/Product/styles.ts
@@ -3,9 +3,20 @@ import { Button } from '../Button';
 
 export const StyledButton = styled(Button)`
   font-size: ${({ theme }) => theme.typography.h5.fontSize};
+  font-weight: ${({ theme }) => theme.typography.link.fontWeight};
 
   ${({ theme }) => theme.breakpoints.down('sm')} {
     font-size: ${({ theme }) => theme.typography.body1.fontSize};
-    font-weight: ${({ theme }) => theme.typography.body1.fontWeight};
+    font-weight: ${({ theme }) => theme.typography.link.fontWeight};
+  }
+
+  &:hover {
+    background-color: unset;
+  }
+
+  &:active {
+    color: unset;
+
+    background-color: unset;
   }
 `;
