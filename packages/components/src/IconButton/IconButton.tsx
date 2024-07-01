ee34dba2f4ed9613d9050f19a1a821fe669829c9
Королев Данил <105650840+pan1caisreal@users.noreply.github.com>
2024-07-01 09:44:42 +0300

    bug(UIKIT-1581,IconButton): Исправлена ошибка с атрибутами элементов

--- packages/components/src/IconButton/IconButton.tsx
+++ packages/components/src/IconButton/IconButton.tsx
@@ -1,45 +1,59 @@
-import { forwardRef, useMemo } from 'react';
+import { type ElementType, type ForwardedRef, useMemo } from 'react';
 
 import { type ButtonProps, ButtonVariants } from '../Button';
 import {
   CircularProgress,
   type CircularProgressProps,
 } from '../CircularProgress';
+import { forwardRefWithGeneric } from '../forwardRefWithGeneric';
 
 import { StyledButton } from './styles';
 
-export type IconButtonProps = ButtonProps & {
-  /**
-   * @description Флаг отображения индикатора загрузки
-   * @example <IconButton loading />
-   */
-  loading?: boolean;
+export type IconButtonProps<TComponent extends ElementType> =
+  ButtonProps<TComponent> & {
+    /**
+     * @description Флаг отображения индикатора загрузки
+     * @example <IconButton loading />
+     */
+    loading?: boolean;
+  };
+
+const UnwrappedIconButton = <TComponent extends ElementType>(
+  props: IconButtonProps<TComponent>,
+  ref: ForwardedRef<HTMLButtonElement>,
+) => {
+  const {
+    variant = ButtonVariants.Contained,
+    loading,
+    children,
+    ...restProps
+  } = props;
+
+  const progressColor = useMemo((): CircularProgressProps['color'] => {
+    if (variant === ButtonVariants.Contained) {
+      return 'inverted';
+    }
+
+    return 'primary';
+  }, [variant]);
+
+  return (
+    <StyledButton
+      loading={loading}
+      variant={variant}
+      tabIndex={loading ? -1 : 0}
+      {...restProps}
+      ref={ref}
+    >
+      {loading ? (
+        <CircularProgress size="small" color={progressColor} />
+      ) : (
+        children
+      )}
+    </StyledButton>
+  );
 };
 
-export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
-  ({ children, loading, variant, ...props }, ref) => {
-    const progressColor = useMemo((): CircularProgressProps['color'] => {
-      if (variant === ButtonVariants.Contained) {
-        return 'inverted';
-      }
+export const IconButton = forwardRefWithGeneric(UnwrappedIconButton);
 
-      return 'primary';
-    }, [variant]);
-
-    return (
-      <StyledButton
-        loading={loading}
-        variant={variant}
-        tabIndex={loading ? -1 : 0}
-        {...props}
-        ref={ref}
-      >
-        {loading ? (
-          <CircularProgress size="small" color={progressColor} />
-        ) : (
-          children
-        )}
-      </StyledButton>
-    );
-  },
-);
+export default IconButton;
