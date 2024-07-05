b66ad16b20334bfa58ac00280561c3d3b3b190ef
Uvoi <iovu_dmtr@yahoo.com>
2024-07-05 19:33:18 +0300

    doc(UIKIT-1480, Slide): Переформатирован storybook в новый формат

--- packages/components/src/Slide/Slide.stories.tsx
+++ packages/components/src/Slide/Slide.stories.tsx
@@ -1,16 +1,25 @@
-import { useState } from 'react';
+import { type ChangeEvent, Children, useState } from 'react';
 import { Box, Paper, Switch } from '@mui/material';
 import { type Theme } from '@mui/material/styles';
-import { type StoryFn } from '@storybook/react';
+import { type Meta, type StoryFn, type StoryObj } from '@storybook/react';
 
 import { FormControlLabel } from '../FormControlLabel';
+import { Grid } from '../Grid';
 
 import { Slide } from './Slide';
 
-export default {
+/**
+ * ### [Figma]()
+ * ### [Guide]()
+ */
+const meta: Meta<typeof Slide> = {
   title: 'Components/Slide',
   component: Slide,
 };
+
+export default meta;
+
+type Story = StoryObj<typeof Slide>;
 
 const icon = (
   <Paper sx={{ m: 1 }} elevation={4}>
@@ -28,7 +37,22 @@ const icon = (
   </Paper>
 );
 
-const Template: StoryFn = () => {
+export const Interaction: Story = {
+  args: {
+    direction: 'up',
+    in: true,
+    mountOnEnter: true,
+    unmountOnExit: true,
+    children: icon,
+  },
+  parameters: {
+    docs: {
+      disable: true,
+    },
+  },
+};
+
+export const Example = () => {
   const [checked, setChecked] = useState(false);
 
   const handleChange = () => {
@@ -36,7 +60,7 @@ const Template: StoryFn = () => {
   };
 
   return (
-    <Box sx={{ height: 180 }}>
+    <>
       <Box sx={{ width: 'calc(100px + 16px)' }}>
         <FormControlLabel
           control={<Switch checked={checked} onChange={handleChange} />}
@@ -46,13 +70,125 @@ const Template: StoryFn = () => {
           {icon}
         </Slide>
       </Box>
-    </Box>
+    </>
   );
 };
 
-export const Default = Template.bind({});
+export const Direction = () => {
+  const [checkedUp, setCheckedUp] = useState(false);
+  const [checkedRight, setCheckedRight] = useState(false);
+  const [checkedDown, setCheckedDown] = useState(false);
+  const [checkedLeft, setCheckedLeft] = useState(false);
+  const [slideDirection, setSlideDirection] = useState<
+    'left' | 'right' | 'up' | 'down'
+  >('up');
 
-Default.parameters = {
-  options: { showPanel: true },
-  controls: { expanded: true },
+  const handleChange = (
+    event: ChangeEvent<HTMLInputElement>,
+    direction?: 'left' | 'right' | 'up' | 'down',
+  ) => {
+    switch (direction) {
+      case 'up':
+        setCheckedUp(event.target.checked);
+
+        break;
+      case 'right':
+        setCheckedRight(event.target.checked);
+
+        break;
+      case 'down':
+        setCheckedDown(event.target.checked);
+
+        break;
+      case 'left':
+        setCheckedLeft(event.target.checked);
+
+        break;
+      default:
+        break;
+    }
+
+    direction && setSlideDirection(direction);
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Box sx={{ width: 'calc(100px + 16px)' }}>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={checkedUp}
+              onChange={(e) => handleChange(e, 'up')}
+            />
+          }
+          label="Up"
+        />
+        <Slide
+          direction={slideDirection}
+          in={checkedUp}
+          mountOnEnter
+          unmountOnExit
+        >
+          {icon}
+        </Slide>
+      </Box>
+      <Box sx={{ width: 'calc(100px + 16px)' }}>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={checkedRight}
+              onChange={(e) => handleChange(e, 'right')}
+            />
+          }
+          label="Right"
+        />
+        <Slide
+          direction={slideDirection}
+          in={checkedRight}
+          mountOnEnter
+          unmountOnExit
+        >
+          {icon}
+        </Slide>
+      </Box>
+      <Box sx={{ width: 'calc(100px + 16px)' }}>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={checkedDown}
+              onChange={(e) => handleChange(e, 'down')}
+            />
+          }
+          label="Down"
+        />
+        <Slide
+          direction={slideDirection}
+          in={checkedDown}
+          mountOnEnter
+          unmountOnExit
+        >
+          {icon}
+        </Slide>
+      </Box>
+      <Box sx={{ width: 'calc(100px + 16px)' }}>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={checkedLeft}
+              onChange={(e) => handleChange(e, 'left')}
+            />
+          }
+          label="Left"
+        />
+        <Slide
+          direction={slideDirection}
+          in={checkedLeft}
+          mountOnEnter
+          unmountOnExit
+        >
+          {icon}
+        </Slide>
+      </Box>
+    </Box>
+  );
 };
