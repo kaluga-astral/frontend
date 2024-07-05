5e7d994b68e9700c1dcd2656da2eff3ae7720379
Uvoi <iovu_dmtr@yahoo.com>
2024-07-05 14:52:03 +0300

    doc(UIKIT-1480, Switch): Переформатирован storybook в новый формат

--- packages/components/src/Switch/Switch.stories.tsx
+++ packages/components/src/Switch/Switch.stories.tsx
@@ -1,5 +1,5 @@
-import { type StoryFn } from '@storybook/react';
-import { Paper, useMediaQuery, useTheme } from '@mui/material';
+import { type Meta, type StoryObj } from '@storybook/react';
+import { Paper } from '@mui/material';
 
 import { Typography } from '../Typography';
 import { LegacyGrid } from '../LegacyGrid';
@@ -7,136 +7,40 @@ import { DialogContent } from '../DialogContent';
 import { DialogActions } from '../DialogActions';
 import { DialogTitle } from '../DialogTitle';
 import { FormControlLabel } from '../FormControlLabel';
-import { ExampleTemplate } from '../docs/ExampleTemplate';
+import { Grid } from '../Grid';
 
 import { Switch } from './Switch';
 
-export default {
+/**
+ * Switch — переключатель — включает или выключает состояние отдельной настройки.
+ * Данный компонент можно использовать при необходимости включения/отключения какой-либо функции в сервисе.
+ *
+ * ### [Figma](https://www.figma.com/design/3ghN4WjSgkKx5rETR64jqh/Sirius-Design-System-(%D0%90%D0%9A%D0%A2%D0%A3%D0%90%D0%9B%D0%AC%D0%9D%D0%9E)?node-id=574-76885)
+ * ### [Guide]()
+ */
+const meta: Meta<typeof Switch> = {
   title: 'Components/Switch',
   component: Switch,
 };
 
-export const SwitchShowcase: StoryFn = () => {
-  const theme = useTheme();
+export default meta;
 
-  const matches = useMediaQuery(theme.breakpoints.down('sm'));
-  const autoFlow = matches ? 'row' : 'column';
+type Story = StoryObj<typeof Switch>;
 
+export const Interaction: Story = {
+  args: {
+    color: 'primary',
+  },
+  parameters: {
+    docs: {
+      disable: true,
+    },
+  },
+};
+
+export const Example = () => {
   return (
-    <ExampleTemplate>
-      <Typography paragraph variant="h3">
-        Switch
-      </Typography>
-      <Typography paragraph>
-        Switch — переключатель — включает или выключает состояние отдельной
-        настройки.
-      </Typography>
-      <Typography>
-        Данный компонент можно использовать при необходимости
-        включения/отключения какой-либо функции в сервисе.
-      </Typography>
-      <br />
-      <br />
-      <Typography variant="h4" paragraph>
-        Типы компонента
-      </Typography>
-
-      <ExampleTemplate.Case
-        title="Switch"
-        descriptionList={[
-          'Переключатель имеет два состояния: выключен и включен.',
-        ]}
-      >
-        <LegacyGrid
-          container
-          justifyContent="center"
-          autoFlow={autoFlow}
-          spacing={4}
-          templateColumns="auto auto"
-          templateRows="auto auto"
-        >
-          <Switch />
-          <Switch checked />
-          <Switch disabled />
-          <Switch checked disabled />
-        </LegacyGrid>
-      </ExampleTemplate.Case>
-
-      <ExampleTemplate.Case
-        title="Switch+text"
-        descriptionList={[
-          'Компонент переключателя используется в интерфейсе с поясняющим текстом.',
-          'Переключатель может располагаться слева и справа от текста. Отступ между текстом и переключателем по умолчанию 8px, но может и отличаться от этого значения в зависимости от сетки страницы.',
-          'Пример использования: включить отображение сервисных  уведомлений.',
-        ]}
-      >
-        <LegacyGrid
-          container
-          justifyContent="center"
-          autoFlow={autoFlow}
-          spacing={4}
-          templateRows="auto auto"
-        >
-          <FormControlLabel control={<Switch />} label="Text" />
-          <FormControlLabel
-            control={<Switch />}
-            label="Text"
-            labelPlacement="start"
-          />
-
-          <FormControlLabel control={<Switch />} disabled label="Text" />
-          <FormControlLabel
-            control={<Switch />}
-            disabled
-            label="Text"
-            labelPlacement="start"
-          />
-
-          <FormControlLabel control={<Switch checked />} label="Text" />
-          <FormControlLabel
-            control={<Switch checked />}
-            label="Text"
-            labelPlacement="start"
-          />
-
-          <FormControlLabel
-            control={<Switch checked />}
-            label="Text"
-            disabled
-          />
-          <FormControlLabel
-            control={<Switch checked />}
-            label="Text"
-            labelPlacement="start"
-            disabled
-          />
-        </LegacyGrid>
-      </ExampleTemplate.Case>
-
-      <ExampleTemplate.Case
-        title="Size"
-        descriptionList={[
-          'Переключатель имеет несколько размеров: small, medium.',
-        ]}
-      >
-        <LegacyGrid
-          container
-          justifyContent="center"
-          autoFlow={autoFlow}
-          spacing={4}
-          templateRows="auto auto"
-        >
-          <Switch size="small" />
-          <FormControlLabel control={<Switch size="small" />} label="Text" />
-
-          <Switch />
-          <FormControlLabel control={<Switch />} label="Text" />
-        </LegacyGrid>
-      </ExampleTemplate.Case>
-
-      <Typography variant="h5" paragraph>
-        Пример использования
-      </Typography>
+    <>
       <Paper style={{ width: 500 }}>
         <DialogTitle variant="h5">Настройки</DialogTitle>
         <DialogContent>
@@ -151,19 +55,67 @@ export const SwitchShowcase: StoryFn = () => {
         </DialogContent>
         <DialogActions></DialogActions>
       </Paper>
-    </ExampleTemplate>
+    </>
   );
 };
 
-SwitchShowcase.parameters = { options: { showPanel: false } };
+/**
+ * Переключатель имеет два состояния: выключен и включен.
+ */
+export const Types = () => (
+  <Grid container columns={2} rows={2} spacing={6}>
+    <Switch />
+    <Switch checked />
+    <Switch disabled />
+    <Switch checked disabled />
+  </Grid>
+);
 
-const Template: StoryFn = (args) => <Switch {...args} />;
+/**
+ * Компонент переключателя используется в интерфейсе с поясняющим текстом.
+ */
+export const SwitchWithText = () => (
+  <Grid container spacing={4} columns={4}>
+    <FormControlLabel control={<Switch />} label="Text" />
+    <FormControlLabel
+      control={<Switch />}
+      label="Text"
+      labelPlacement="start"
+    />
 
-export const SwitchStory = Template.bind({});
+    <FormControlLabel control={<Switch />} disabled label="Text" />
+    <FormControlLabel
+      control={<Switch />}
+      disabled
+      label="Text"
+      labelPlacement="start"
+    />
 
-SwitchStory.storyName = 'Switch';
+    <FormControlLabel control={<Switch checked />} label="Text" />
+    <FormControlLabel
+      control={<Switch checked />}
+      label="Text"
+      labelPlacement="start"
+    />
 
-SwitchStory.parameters = {
-  options: { showPanel: true },
-  controls: { expanded: true },
-};
+    <FormControlLabel control={<Switch checked />} label="Text" disabled />
+    <FormControlLabel
+      control={<Switch checked />}
+      label="Text"
+      labelPlacement="start"
+      disabled
+    />
+  </Grid>
+);
+
+/**
+ * Переключатель имеет несколько размеров: small, medium.
+ */
+export const Sizes = () => (
+  <Grid container spacing={4} columns={2}>
+    <Switch size="small" />
+    <FormControlLabel control={<Switch size="small" />} label="Text" />
+    <Switch />
+    <FormControlLabel control={<Switch />} label="Text" />
+  </Grid>
+);
