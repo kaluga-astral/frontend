3ecaa661ac5ce20ccdf73c1c2c6e40f813029c77
Королев Данил <105650840+pan1caisreal@users.noreply.github.com>
2024-07-09 11:10:06 +0300

    wip(UIKIT-1439,DataGridPagination): Добавлены тесты

--- packages/components/src/DataGridPagination/DataGridPagination.test.tsx
+++ packages/components/src/DataGridPagination/DataGridPagination.test.tsx
@@ -15,7 +15,11 @@ describe('DataGridPagination', () => {
     'Props:page=$page:totalCount=$totalCount: текст пагинации "$expected"',
     ({ totalCount, page, expected }) => {
       renderWithTheme(
-        <DataGridPagination totalCount={totalCount} page={page} />,
+        <DataGridPagination
+          totalCount={totalCount}
+          page={page}
+          rowsPerPage={10}
+        />,
       );
 
       const text = screen.getByText(expected);
@@ -30,5 +34,57 @@ describe('DataGridPagination', () => {
     );
 
     expect(container).toBeEmptyDOMElement();
+  });
+
+  it('Select не отображается, при отсутствии onSetCountPerPage', () => {
+    renderWithTheme(
+      <DataGridPagination
+        totalCount={30}
+        onChange={() => {}}
+        rowsPerPage={10}
+        page={1}
+      />,
+    );
+
+    const buttons = screen.getAllByRole('button');
+    const selectButton = buttons.find((button) => button.textContent === '10');
+
+    expect(selectButton).toBeUndefined();
+  });
+
+  it('Select отображается, при onSetCountPerPage', () => {
+    renderWithTheme(
+      <DataGridPagination
+        totalCount={30}
+        onChange={() => {}}
+        onSetCountPerPage={() => {}}
+        page={1}
+      />,
+    );
+
+    const buttons = screen.getAllByRole('button');
+    const selectButton = buttons.find((button) => button.textContent === '20');
+
+    expect(selectButton).toBeInTheDocument();
+  });
+
+  it('Кастомные опции отображаются, если задан rowsPerPageOptions', async () => {
+    const ROWS_PER_PAGE_OPTION = [5, 10, 15, 20];
+
+    renderWithTheme(
+      <DataGridPagination
+        totalCount={30}
+        onChange={() => {}}
+        onSetCountPerPage={() => {}}
+        rowsPerPage={ROWS_PER_PAGE_OPTION[2]}
+        rowsPerPageOptions={ROWS_PER_PAGE_OPTION}
+        page={3}
+      />,
+    );
+
+    const buttons = screen.getAllByRole('button');
+    const selectButton = buttons.find((button) => button.textContent === '15');
+
+    expect(selectButton).toBeInTheDocument();
   });
 });
