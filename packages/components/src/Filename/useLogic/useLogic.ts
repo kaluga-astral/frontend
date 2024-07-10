b86d8011569e03665421d9f1168333bb2cf0fd46
Королев Данил <105650840+pan1caisreal@users.noreply.github.com>
2024-07-10 22:35:48 +0300

    wip(UIKIT-1565,PageHeader): Правки по PR

--- packages/components/src/Filename/useLogic/useLogic.ts
+++ packages/components/src/Filename/useLogic/useLogic.ts
@@ -1,7 +1,7 @@
-import { type Ref, useLayoutEffect, useRef, useState } from 'react';
-import { debounce } from 'remeda';
+import { type Ref } from 'react';
 
 import { type FileNameProps } from '../Filename';
+import { useOverflowed } from '../../OverflowTypography/hooks';
 
 import { truncateString } from './utils';
 
@@ -10,38 +10,7 @@ export const useLogic = (
   forwardedRef: Ref<HTMLElement>,
   props: Omit<FileNameProps, 'children' | 'tooltipProps'>,
 ) => {
-  const [isOverflowed, setIsOverflowed] = useState<boolean>(false);
-  const localRef = useRef<HTMLElement>(null);
-  const ref =
-    forwardedRef && typeof forwardedRef !== 'function'
-      ? forwardedRef
-      : localRef;
-
-  const handleResize = debounce(
-    ([{ target, contentRect }]: ResizeObserverEntry[]) => {
-      // сверка ширины дом ноды и ширины скролл контейнера, если скролл больше, то значит компонент переполнен контентом
-      const isScrollWidthBigger =
-        target.scrollWidth > Math.round(contentRect.width);
-      const isScrollHeightBigger =
-        target.scrollHeight > Math.round(contentRect.height);
-
-      setIsOverflowed(isScrollWidthBigger || isScrollHeightBigger);
-    },
-    { waitMs: 500 },
-  );
-
-  useLayoutEffect(() => {
-    if (ref?.current) {
-      const resizeObserver = new ResizeObserver(handleResize.call);
-      const node = ref.current;
-
-      resizeObserver.observe(node);
-
-      return () => resizeObserver.unobserve(node);
-    }
-
-    return;
-  }, [children, ref.current]);
+  const { ref, isOverflowed } = useOverflowed(forwardedRef);
 
   const { baseName, ext } = truncateString(children);
 
