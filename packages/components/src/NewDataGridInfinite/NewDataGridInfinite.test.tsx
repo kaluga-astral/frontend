2312306a51e4efb4c0e0fc96165a8e103b7326b6
ooops_o_O <47597207+mfrolov89@users.noreply.github.com>
2024-07-12 11:28:47 +0300

    feat(UIKIT-1555,NewDataGridInfinite): Реализован новый компонент NewDataGridInfinite (#1039)

---
+++ packages/components/src/NewDataGridInfinite/NewDataGridInfinite.test.tsx
@@ -0,0 +1,310 @@
+import { describe, expect, it, vi } from 'vitest';
+import { render, renderWithTheme, screen, userEvents } from '@astral/tests';
+import { VirtuosoMockContext } from 'react-virtuoso';
+import { theme } from '@astral/tests/src/theme';
+import { BinOutlineMd } from '@astral/icons';
+
+import { styled } from '../styles';
+import { ThemeProvider } from '../ThemeProvider';
+import { ActionCell } from '../ActionCell';
+
+import { END_OF_SCROLL_MESSAGE } from './constants';
+import { NewDataGridInfinite } from './NewDataGridInfinite';
+
+const DataGridInfiniteWrapper = styled.div`
+  width: 100%;
+  height: 400px;
+`;
+
+describe('NewDataGridInfinite', () => {
+  it('Названия колонок отображаются', () => {
+    const columns = [
+      {
+        field: 'name',
+        label: 'Наименование',
+      },
+    ];
+
+    renderWithTheme(
+      <NewDataGridInfinite
+        keyId="id"
+        rows={[]}
+        columns={columns}
+        onRetry={() => {}}
+      />,
+    );
+
+    const title = screen.getByText('Наименование');
+
+    expect(title).toBeVisible();
+  });
+
+  it('Данные в строке отображаются', async () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <DataGridInfiniteWrapper>
+          <NewDataGridInfinite
+            keyId="name"
+            rows={[{ name: 'Vasya' }]}
+            columns={[
+              {
+                field: 'name',
+                label: 'Наименование',
+              },
+            ]}
+            onRetry={() => {}}
+          />
+        </DataGridInfiniteWrapper>
+      </ThemeProvider>,
+      {
+        wrapper: ({ children }) => (
+          <VirtuosoMockContext.Provider
+            value={{ viewportHeight: 300, itemHeight: 100 }}
+          >
+            {children}
+          </VirtuosoMockContext.Provider>
+        ),
+      },
+    );
+
+    const title = screen.getByText('Vasya');
+
+    expect(title).toBeVisible();
+  });
+
+  it('OnRowClick получает данные row по клику на строку', async () => {
+    const onClickSpy = vi.fn();
+
+    render(
+      <ThemeProvider theme={theme}>
+        <DataGridInfiniteWrapper>
+          <NewDataGridInfinite
+            keyId="name"
+            rows={[{ name: 'Vasya' }]}
+            columns={[
+              {
+                field: 'name',
+                label: 'Наименование',
+              },
+            ]}
+            onRowClick={onClickSpy}
+            onRetry={() => {}}
+          />
+        </DataGridInfiniteWrapper>
+      </ThemeProvider>,
+      {
+        wrapper: ({ children }) => (
+          <VirtuosoMockContext.Provider
+            value={{ viewportHeight: 300, itemHeight: 100 }}
+          >
+            {children}
+          </VirtuosoMockContext.Provider>
+        ),
+      },
+    );
+
+    await userEvents.click(screen.getByText('Vasya'));
+    expect(onClickSpy.mock.calls[0][0]).toEqual({ name: 'Vasya' });
+  });
+
+  it('Placeholder отображается при отсутствии данных', () => {
+    renderWithTheme(
+      <NewDataGridInfinite
+        keyId="id"
+        rows={[]}
+        columns={[
+          {
+            field: 'name',
+            label: 'Наименование',
+          },
+        ]}
+        onRetry={() => {}}
+      />,
+    );
+
+    const title = screen.getByText('Нет данных');
+
+    expect(title).toBeVisible();
+  });
+
+  it('Placeholder c ошибкой отображается при isError=true', () => {
+    renderWithTheme(
+      <NewDataGridInfinite
+        keyId="id"
+        isError
+        rows={[]}
+        columns={[
+          {
+            field: 'name',
+            label: 'Наименование',
+          },
+        ]}
+        onRetry={() => {}}
+      />,
+    );
+
+    const title = screen.getByText('Произошла ошибка');
+
+    expect(title).toBeVisible();
+  });
+
+  it('Лоадер отображается при первичной загрузке данных', () => {
+    renderWithTheme(
+      <NewDataGridInfinite
+        keyId="id"
+        isLoading
+        rows={[]}
+        columns={[
+          {
+            field: 'name',
+            label: 'Наименование',
+          },
+        ]}
+        onRetry={() => {}}
+      />,
+    );
+
+    const loader = screen.getByRole('progressbar');
+
+    expect(loader).toBeVisible();
+  });
+
+  it('Сообщение отображается при достижении конца списка', () => {
+    renderWithTheme(
+      <NewDataGridInfinite
+        keyId="id"
+        isEndReached
+        rows={[]}
+        columns={[
+          {
+            field: 'name',
+            label: 'Наименование',
+          },
+        ]}
+        onRetry={() => {}}
+      />,
+    );
+
+    const title = screen.getByText(END_OF_SCROLL_MESSAGE);
+
+    expect(title).toBeVisible();
+  });
+
+  it('OnRetry вызывается при нажатии на кнопку "Попробовать снова"', async () => {
+    const onRetrySpy = vi.fn();
+
+    renderWithTheme(
+      <NewDataGridInfinite
+        keyId="id"
+        isError
+        rows={[]}
+        columns={[
+          {
+            field: 'name',
+            label: 'Наименование',
+          },
+        ]}
+        onRetry={onRetrySpy}
+      />,
+    );
+
+    await userEvents.click(screen.getByRole('button'));
+    expect(onRetrySpy).toHaveBeenCalled();
+  });
+
+  it('Строка не доступна для взаимодействия, если isDisabled=true', async () => {
+    const onClickSpy = vi.fn();
+
+    render(
+      <ThemeProvider theme={theme}>
+        <DataGridInfiniteWrapper>
+          <NewDataGridInfinite
+            keyId="name"
+            rows={[{ name: 'Vasya', options: { isDisabled: true } }]}
+            onRowClick={onClickSpy}
+            columns={[
+              {
+                field: 'name',
+                label: 'Наименование',
+              },
+            ]}
+            onRetry={() => {}}
+          />
+        </DataGridInfiniteWrapper>
+      </ThemeProvider>,
+      {
+        wrapper: ({ children }) => (
+          <VirtuosoMockContext.Provider
+            value={{ viewportHeight: 300, itemHeight: 100 }}
+          >
+            {children}
+          </VirtuosoMockContext.Provider>
+        ),
+      },
+    );
+
+    await userEvents.click(screen.getByText('Vasya'));
+    expect(onClickSpy).not.toHaveBeenCalled();
+  });
+
+  it('Последняя ячейка доступна для взаимодействия, если isDisabled=true и isDisabledLastCell=false', async () => {
+    const onDeleteSpy = vi.fn();
+
+    const ACTIONS = {
+      main: [
+        {
+          icon: <BinOutlineMd />,
+          name: 'Удалить',
+          onClick: onDeleteSpy,
+        },
+      ],
+    };
+
+    render(
+      <ThemeProvider theme={theme}>
+        <DataGridInfiniteWrapper>
+          {' '}
+          <NewDataGridInfinite
+            keyId="name"
+            rows={[
+              {
+                name: 'Vasya',
+                options: { isDisabled: true, isDisabledLastCell: false },
+              },
+            ]}
+            columns={[
+              {
+                field: 'name',
+                label: 'Наименование',
+              },
+              {
+                label: 'Действия',
+                sortable: false,
+                align: 'center',
+                width: '100px',
+                renderCell: (row) => {
+                  return <ActionCell actions={ACTIONS} row={row} />;
+                },
+              },
+            ]}
+            onRetry={() => {}}
+          />
+        </DataGridInfiniteWrapper>
+      </ThemeProvider>,
+      {
+        wrapper: ({ children }) => (
+          <VirtuosoMockContext.Provider
+            value={{ viewportHeight: 300, itemHeight: 100 }}
+          >
+            {children}
+          </VirtuosoMockContext.Provider>
+        ),
+      },
+    );
+
+    const button = screen.getByRole('button');
+
+    await userEvents.click(button);
+    expect(onDeleteSpy).toHaveBeenCalled();
+  });
+});
---
+++ packages/components/src/NewDataGridInfinite/useLogic/useLogic.ts
@@ -0,0 +1,137 @@
+import { type ChangeEvent, useCallback, useMemo, useRef } from 'react';
+import type { ListRange, VirtuosoHandle } from 'react-virtuoso';
+
+import { prop, uniqBy } from '../../utils';
+import { useToggle } from '../../hooks';
+import { getGridTemplateColumns } from '../../NewDataGrid/useLogic/utils';
+import type { DataGridRow } from '../../NewDataGrid';
+import { type NewDataGridInfiniteProps } from '../NewDataGridInfinite';
+
+type UseLogicParams<
+  TData extends Record<string, unknown> = DataGridRow,
+  TSortField extends keyof TData = keyof TData,
+> = NewDataGridInfiniteProps<TData, TSortField>;
+
+export const useLogic = <
+  TData extends Record<string, unknown> = DataGridRow,
+  TSortField extends keyof TData = keyof TData,
+>({
+  keyId,
+  columns,
+  rows,
+  selectedRows = [],
+  isLoading,
+  isDisabled,
+  isEndReached,
+  onEndReached,
+  onSelectRow,
+}: UseLogicParams<TData, TSortField>) => {
+  const virtuoso = useRef<VirtuosoHandle>(null);
+
+  const [isStickyButtonActive, showStickyButton, hideStickyButton] =
+    useToggle();
+
+  const isNoData = Boolean(rows?.length);
+  const isSelectable = Boolean(onSelectRow);
+  const isDataGridDisabled = isDisabled || (!isNoData && isLoading);
+
+  const availableRows = rows.filter((row) => !row.options?.isDisabled);
+
+  const gridColumns = getGridTemplateColumns(columns, isSelectable);
+
+  const handleEndReach = useCallback(() => {
+    if (!isEndReached && onEndReached) {
+      onEndReached();
+    }
+  }, [isEndReached, onEndReached]);
+
+  const uncheckedRowsCount = useMemo(() => {
+    return availableRows.filter(
+      (row) =>
+        !selectedRows.find((selectedRow) => selectedRow[keyId] === row[keyId]),
+    ).length;
+  }, [availableRows, selectedRows, keyId]);
+
+  const handleRangeChanged = useCallback(
+    (range: ListRange) => {
+      if (range.startIndex > 2) {
+        showStickyButton();
+
+        return;
+      }
+
+      hideStickyButton();
+    },
+    [hideStickyButton, showStickyButton],
+  );
+
+  const handleSelectAllRows = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (!onSelectRow) {
+      return;
+    }
+
+    if (event.target.checked) {
+      const mergedSelectedRows = uniqBy(
+        [...selectedRows, ...availableRows],
+        prop(keyId),
+      );
+
+      onSelectRow(mergedSelectedRows);
+    }
+
+    const filteredRows = selectedRows.filter(
+      (selectedRow) => !rows.find((row) => row[keyId] === selectedRow[keyId]),
+    );
+
+    onSelectRow(filteredRows);
+  };
+
+  const handleSelectRow = useCallback(
+    (row: TData) =>
+      (event: ChangeEvent<HTMLInputElement>): void => {
+        if (!onSelectRow) {
+          return;
+        }
+
+        if (event.target.checked) {
+          return onSelectRow([...selectedRows, row]);
+        }
+
+        onSelectRow(
+          selectedRows.filter(
+            (selectedRow) => selectedRow[keyId] !== row[keyId],
+          ),
+        );
+      },
+    [selectedRows, onSelectRow, keyId],
+  );
+
+  const handleScrollToTop = () => {
+    virtuoso.current?.scrollIntoView({ index: 0, behavior: 'smooth' });
+  };
+
+  return {
+    isNoData,
+    isDataGridDisabled,
+    isStickyButtonActive,
+    virtuosoProps: {
+      ref: virtuoso,
+      endReached: handleEndReach,
+      rangeChanged: handleRangeChanged,
+    },
+    headProps: {
+      isSelectable,
+      uncheckedRowsCount: uncheckedRowsCount,
+      gridColumns,
+      onSelectAllRows: handleSelectAllRows,
+    },
+    rowProps: {
+      isSelectable,
+      gridColumns,
+      onSelectRow: handleSelectRow,
+    },
+    scrollToTopButtonProps: {
+      onClick: handleScrollToTop,
+    },
+  };
+};
