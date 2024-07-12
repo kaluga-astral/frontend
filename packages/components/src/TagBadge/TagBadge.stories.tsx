a5955e14c5674b8db202ae6949ebe281d66ad2b8
Uvoi <iovu_dmtr@yahoo.com>
2024-07-12 16:14:11 +0300

    doc(UIKIT-1480, TagBadge): Изменен порядок story, добавлена Variant и изменена Color

--- packages/components/src/TagBadge/TagBadge.stories.tsx
+++ packages/components/src/TagBadge/TagBadge.stories.tsx
@@ -3,7 +3,7 @@ import { type Meta, type StoryObj } from '@storybook/react';
 
 import { Tag } from '../Tag';
 
-import { TagBadge, type TagBadgeProps } from '.';
+import { TagBadge } from '.';
 
 /**
  * ### [Figma](https://www.figma.com/design/3ghN4WjSgkKx5rETR64jqh/Sirius-Design-System-(%D0%90%D0%9A%D0%A2%D0%A3%D0%90%D0%9B%D0%AC%D0%9D%D0%9E)?node-id=284-7165)
@@ -33,25 +33,240 @@ export const Interaction: Story = {
 
 const baseProps = {
   label: 'Тэг',
-  endAddon: (props: TagBadgeProps) => (
-    <TagBadge {...props} badgeContent={'12'} />
-  ),
 };
 
 export const Example = () => (
-  <Tag variant="contained" color="primary" {...baseProps} />
+  <Tag
+    {...baseProps}
+    endAddon={(props) => <TagBadge {...props} badgeContent="12" />}
+  />
 );
+
+export const Default = () => <TagBadge color="primary" badgeContent="12" />;
 
 export const Color = () => (
   <Stack direction="column" gap={2}>
     <Stack direction="row" gap={2}>
-      <Tag color="warning" variant="contained" {...baseProps} />
-      <Tag color="grey" variant="contained" {...baseProps} />
-      <Tag color="warning" variant="light" {...baseProps} />
-      <Tag color="error" variant="light" {...baseProps} />
-      <Tag color="success" variant="light" {...baseProps} />
+      <Tag
+        {...baseProps}
+        endAddon={(props) => (
+          <TagBadge {...props} color="primary" badgeContent="12" />
+        )}
+      />
+      <Tag
+        {...baseProps}
+        endAddon={(props) => (
+          <TagBadge {...props} color="error" badgeContent="12" />
+        )}
+      />
+      <Tag
+        {...baseProps}
+        endAddon={(props) => (
+          <TagBadge {...props} color="success" badgeContent="12" />
+        )}
+      />
+      <Tag
+        {...baseProps}
+        endAddon={(props) => (
+          <TagBadge {...props} color="warning" badgeContent="12" />
+        )}
+      />
+      <Tag
+        {...baseProps}
+        endAddon={(props) => (
+          <TagBadge {...props} color="grey" badgeContent="12" />
+        )}
+      />
+      <Tag
+        {...baseProps}
+        endAddon={(props) => (
+          <TagBadge {...props} color="white" badgeContent="12" />
+        )}
+      />
+      <Tag
+        {...baseProps}
+        endAddon={(props) => (
+          <TagBadge {...props} color="errorLight" badgeContent="12" />
+        )}
+      />
+    </Stack>
+    <Stack direction="row" gap={2}>
+      <Tag
+        {...baseProps}
+        color="primary"
+        endAddon={(props) => (
+          <TagBadge {...props} color="primary" badgeContent="12" />
+        )}
+      />
+      <Tag
+        {...baseProps}
+        color="error"
+        endAddon={(props) => (
+          <TagBadge {...props} color="error" badgeContent="12" />
+        )}
+      />
+      <Tag
+        {...baseProps}
+        color="success"
+        endAddon={(props) => (
+          <TagBadge {...props} color="success" badgeContent="12" />
+        )}
+      />
+      <Tag
+        {...baseProps}
+        color="warning"
+        endAddon={(props) => (
+          <TagBadge {...props} color="warning" badgeContent="12" />
+        )}
+      />
+      <Tag
+        {...baseProps}
+        color="grey"
+        endAddon={(props) => (
+          <TagBadge {...props} color="grey" badgeContent="12" />
+        )}
+      />
+      <Tag
+        {...baseProps}
+        color="default"
+        endAddon={(props) => (
+          <TagBadge {...props} color="white" badgeContent="12" />
+        )}
+      />
+      <Tag
+        {...baseProps}
+        color="error"
+        endAddon={(props) => (
+          <TagBadge {...props} color="errorLight" badgeContent="12" />
+        )}
+      />
     </Stack>
   </Stack>
 );
 
-export const Default = () => <TagBadge color="primary" badgeContent="12" />;
+export const Variant = () => (
+  <Stack direction="column" gap={2}>
+    <Stack direction="row" justifyContent={'center'} gap={2}>
+      <Tag
+        label="contained"
+        variant="contained"
+        endAddon={(props) => (
+          <TagBadge {...props} color="primary" badgeContent="12" />
+        )}
+      />
+      <Tag
+        label="text"
+        variant="text"
+        endAddon={(props) => (
+          <TagBadge {...props} color="error" badgeContent="12" />
+        )}
+      />
+      <Tag
+        label="light"
+        variant="light"
+        endAddon={(props) => (
+          <TagBadge {...props} color="success" badgeContent="12" />
+        )}
+      />
+      <Tag
+        label="contained"
+        variant="contained"
+        endAddon={(props) => (
+          <TagBadge {...props} color="warning" badgeContent="12" />
+        )}
+      />
+      <Tag
+        label="text"
+        variant="text"
+        endAddon={(props) => (
+          <TagBadge {...props} color="grey" badgeContent="12" />
+        )}
+      />
+      <Tag
+        label="light"
+        variant="light"
+        endAddon={(props) => (
+          <TagBadge {...props} color="white" badgeContent="12" />
+        )}
+      />
+      <Tag
+        label="contained"
+        variant="contained"
+        endAddon={(props) => (
+          <TagBadge {...props} color="errorLight" badgeContent="12" />
+        )}
+      />
+    </Stack>
+    <Stack direction="row" justifyContent={'center'} gap={2}>
+      <Tag
+        label="contained"
+        variant="contained"
+        endAddon={(props) => (
+          <TagBadge
+            {...props}
+            variant="dot"
+            color="primary"
+            badgeContent="12"
+          />
+        )}
+      />
+      <Tag
+        label="text"
+        variant="text"
+        endAddon={(props) => (
+          <TagBadge {...props} variant="dot" color="error" badgeContent="12" />
+        )}
+      />
+      <Tag
+        label="light"
+        variant="light"
+        endAddon={(props) => (
+          <TagBadge
+            {...props}
+            variant="dot"
+            color="success"
+            badgeContent="12"
+          />
+        )}
+      />
+      <Tag
+        label="contained"
+        variant="contained"
+        endAddon={(props) => (
+          <TagBadge
+            {...props}
+            variant="dot"
+            color="warning"
+            badgeContent="12"
+          />
+        )}
+      />
+      <Tag
+        label="text"
+        variant="text"
+        endAddon={(props) => (
+          <TagBadge {...props} variant="dot" color="grey" badgeContent="12" />
+        )}
+      />
+      <Tag
+        label="light"
+        variant="light"
+        endAddon={(props) => (
+          <TagBadge {...props} variant="dot" color="white" badgeContent="12" />
+        )}
+      />
+      <Tag
+        label="contained"
+        variant="contained"
+        endAddon={(props) => (
+          <TagBadge
+            {...props}
+            variant="dot"
+            color="errorLight"
+            badgeContent="12"
+          />
+        )}
+      />
+    </Stack>
+  </Stack>
+);
