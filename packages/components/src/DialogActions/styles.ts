ed3a5c46ec593e33eb590eebc599a03d9c2f8458
ooops_o_O <47597207+mfrolov89@users.noreply.github.com>
2024-08-01 14:19:12 +0300

    feat(UIKIT-1672,Dialog): Добавлен отступ для DialogContent (#1070)

--- packages/components/src/DialogActions/styles.ts
+++ packages/components/src/DialogActions/styles.ts
@@ -1,5 +1,11 @@
+import { DialogActions } from '@mui/material';
+
 import { Grid, type GridProps } from '../Grid';
 import { styled } from '../styles';
+
+export const StyledDialogActions = styled(DialogActions)`
+  padding-top: 0;
+`;
 
 export const StyledGrid = styled(Grid)<GridProps>`
   ${({ theme }) => theme.breakpoints.up('sm')} {
