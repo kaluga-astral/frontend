dff9399e187259a1d1107ba8108117cd9e184d2e
Королев Данил <105650840+pan1caisreal@users.noreply.github.com>
2024-08-09 12:49:10 +0300

    wip(UIKIT-1163,PageHeader): Добавлены тесты

--- packages/components/src/PageHeader/PageHeader.test.tsx
+++ packages/components/src/PageHeader/PageHeader.test.tsx
@@ -4,7 +4,7 @@ import { renderWithTheme, screen, userEvents } from '@astral/tests';
 import { PageHeader } from './PageHeader';
 
 describe('PageHeader', () => {
-  it('Tooltip отображается при note и disabled=false', async () => {
+  it('Tooltip отображается при note и disabled=false основных действий', async () => {
     renderWithTheme(
       <PageHeader
         title="Черновик"
@@ -29,7 +29,7 @@ describe('PageHeader', () => {
     expect(tooltip).toHaveTextContent('Кнопка');
   });
 
-  it('Tooltip отображается при disabledReason и disabled=true', async () => {
+  it('Tooltip отображается при disabledReason и disabled=true основных действий', async () => {
     renderWithTheme(
       <PageHeader
         title="Черновик"
@@ -53,5 +53,116 @@ describe('PageHeader', () => {
 
     expect(tooltip).toBeVisible();
     expect(tooltip).toHaveTextContent('Заблокировано');
+  });
+
+  it('Tooltip отображается при disabledReason и disabled=true вторичных действий', async () => {
+    renderWithTheme(
+      <PageHeader
+        title="Черновик"
+        actions={{
+          secondary: [
+            {
+              text: 'Отправка по маршруту',
+              disabled: true,
+              disabledReason: 'Заблокировано',
+            },
+          ],
+        }}
+      />,
+    );
+
+    const secondaryActionButton = screen.getByRole('button');
+
+    await userEvents.click(secondaryActionButton);
+
+    const item = await screen.findByLabelText('Заблокировано');
+
+    await userEvents.hover(item);
+
+    const tooltip = await screen.findByRole('tooltip');
+
+    expect(tooltip).toBeVisible();
+    expect(tooltip).toHaveTextContent('Заблокировано');
+  });
+
+  it('Tooltip отображается при note и disabled=false вторичных действий', async () => {
+    renderWithTheme(
+      <PageHeader
+        title="Черновик"
+        actions={{
+          secondary: [
+            {
+              text: 'Отправка по маршруту',
+              note: 'Кнопка',
+            },
+          ],
+        }}
+      />,
+    );
+
+    const secondaryActionButton = screen.getByRole('button');
+
+    await userEvents.click(secondaryActionButton);
+
+    const item = await screen.findByLabelText('Кнопка');
+
+    await userEvents.hover(item);
+
+    const tooltip = await screen.findByRole('tooltip');
+
+    expect(tooltip).toBeVisible();
+    expect(tooltip).toHaveTextContent('Кнопка');
+  });
+
+  it('Tooltip отображается при disableReason и disabled=true вторичных видимых действий', async () => {
+    renderWithTheme(
+      <PageHeader
+        title="Черновик"
+        actions={{
+          secondaryVisible: [
+            {
+              name: 'Отправка по маршруту',
+              icon: <svg />,
+              disabled: true,
+              disableReason: 'Заблокировано',
+            },
+          ],
+        }}
+      />,
+    );
+
+    const item = await screen.findByLabelText('Заблокировано');
+
+    await userEvents.hover(item);
+
+    const tooltip = await screen.findByRole('tooltip');
+
+    expect(tooltip).toBeVisible();
+    expect(tooltip).toHaveTextContent('Заблокировано');
+  });
+
+  it('Tooltip отображается для вторичных видимых действий при disabled=false', async () => {
+    renderWithTheme(
+      <PageHeader
+        title="Черновик"
+        actions={{
+          secondaryVisible: [
+            {
+              name: 'Отправка по маршруту',
+              icon: <svg />,
+            },
+          ],
+        }}
+      />,
+    );
+
+    const button = screen.getByRole('button');
+
+    await userEvents.hover(button);
+
+    const tooltip = await screen.findByRole('tooltip');
+
+    expect(tooltip).toBeVisible();
+    expect(tooltip).toHaveTextContent('Отправка по маршруту');
   });
 });
