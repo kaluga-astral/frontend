e53273832fa21149c2cd7575ff5777a476b64923
Королев Данил <105650840+pan1caisreal@users.noreply.github.com>
2024-09-11 14:51:13 +0300

    wip(UIKIT-1726,DatePicker): Поправлен ci

--- packages/components/src/DatePicker/styles.ts
+++ packages/components/src/DatePicker/styles.ts
@@ -9,5 +9,5 @@ export const StyledButton = styled(Button)`
 export const ButtonWrapper = styled.div`
   padding: ${({ theme }) => theme.spacing(4)};
 
-  box-shadow: 0 -1px 10px 0 ${({ theme }) => rgb(theme.palette.grey[900] 0.11)};
+  box-shadow: 0 -1px 10px 0 ${({ theme }) => rgb(theme.palette.grey[900] 0.1)};
 `;
