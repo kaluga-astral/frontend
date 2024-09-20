1274e15cc97bf1dc737cd1cd11318a488d492baf
Королев Данил <105650840+pan1caisreal@users.noreply.github.com>
2024-09-20 15:07:21 +0300

    feat(UIKIT-1784,Description): Заменена логика копирования (#1155)

--- packages/components/src/Description/Value/Value.tsx
+++ packages/components/src/Description/Value/Value.tsx
@@ -1,17 +1,10 @@
-import {
-  type ReactNode,
-  type SyntheticEvent,
-  useContext,
-  useState,
-} from 'react';
+import { type ReactNode, useContext } from 'react';
 
 import { type TypographyProps } from '../../Typography';
 import { ConfigContext } from '../../ConfigProvider';
-import { Tooltip } from '../../Tooltip';
 import { DescriptionContext } from '../DescriptionContext';
 
-import { StyledCopyIcon, StyledTypography, Wrapper } from './styles';
-import { CopyStatus } from './enums';
+import { StyledCopyTypography, StyledTypography, Wrapper } from './styles';
 
 export type ValueProps = Pick<
   TypographyProps,
@@ -32,18 +25,22 @@ export type ValueProps = Pick<
    * Позиционирует иконку "копировать" (слева / справа от текста)
    */
   copyPosition?: 'left' | 'right';
+  /**
+   * Текст, который будет скопирован. Перекрывает обычное копирование если children является строкой
+   */
+  copyText?: string;
 };
 
 export const Value = ({
   canCopy,
   copyPosition = 'right',
+  copyText,
   children,
   stub,
   ...props
 }: ValueProps) => {
   const { leader, direction } = useContext(DescriptionContext);
 
-  const [status, setStatus] = useState<CopyStatus>(CopyStatus.CanCopy);
   const { emptySymbol } = useContext(ConfigContext);
 
   const resultChildren = children || stub || emptySymbol;
@@ -60,52 +57,18 @@ export const Value = ({
     );
   }
 
-  const handleClick = (event: SyntheticEvent<HTMLElement>) => {
-    event.stopPropagation();
-
-    navigator.clipboard
-      .writeText(children?.toString() || '')
-      .then(() => setStatus(CopyStatus.Copied))
-      .catch(() => setStatus(CopyStatus.Error));
-  };
-
-  const handleMouseLeave = () => {
-    if (status !== CopyStatus.CanCopy) {
-      // Ставим таймер чтобы тултип успел скрыться
-      // В ином случае пользователь увидит изменение текста
-      setTimeout(() => {
-        setStatus(CopyStatus.CanCopy);
-      }, 100);
-    }
-  };
-
   return (
-    <Wrapper onMouseLeave={handleMouseLeave}>
-      <Tooltip placement="bottom" title={status}>
-        <StyledTypography
-          $direction={direction}
-          $canCopy={canCopy}
-          $leader={leader}
-          {...props}
-          onClick={handleClick}
-        >
-          {copyPosition === 'left' && (
-            <StyledCopyIcon
-              $copyPosition={copyPosition}
-              color={props.color as 'secondary'}
-            />
-          )}
-
-          {resultChildren}
-
-          {copyPosition === 'right' && (
-            <StyledCopyIcon
-              $copyPosition={copyPosition}
-              color={props.color as 'secondary'}
-            />
-          )}
-        </StyledTypography>
-      </Tooltip>
+    <Wrapper>
+      <StyledCopyTypography
+        copyPosition={copyPosition}
+        copyText={copyText}
+        $direction={direction}
+        $leader={leader}
+        component="p"
+        {...props}
+      >
+        {resultChildren}
+      </StyledCopyTypography>
     </Wrapper>
   );
 };
